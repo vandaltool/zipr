78f3a46b3021f8bdc221469376f6f7b0c87c238a
jdh8d <jdh8d@git.zephyr-software.com>
2010-10-29 16:08:44 +0000

    Created testcase for handshaking with the controller.

---
+++ test_controller.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+whoami=`whoami`
+
+echo Building files...
+gcc -w concolic_test_handshake.c -o test_controller.exe 
+gcc -w hanoi_overrun.c -o hanoi_overrun.ncexe
+$STRATAFIER/do_stratafy.sh hanoi_overrun.ncexe >/dev/null 2>&1 
+mv new.exe hanoi_overrun.stratafied
+
+echo  Removing all ipc queues.
+for i in `ipcs -q|grep $whoami |cut -d" " -f 2`; 
+do
+	ipcrm -q $i
+done
+STRATA_GRACE=1 STRATA_LOG=ipc ./test_controller.exe ./hanoi_overrun.stratafied
+
+echo cleaning up
+killall hanoi_overrun.stratafied
+killall test_controller.exe
+echo  Removing all ipc queues.
+for i in `ipcs -q|grep $whoami |cut -d" " -f 2`; 
+do
+	ipcrm -q $i
+done
