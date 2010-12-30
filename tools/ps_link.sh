9cfadd2124f86dd92b39c42582ea1e4da8728ce5
jdh8d <jdh8d@git.zephyr-software.com>
2010-12-30 17:22:55 +0000

    changes to makefile to use ps_*.sh commands, and bug fixes to ps_* scripts now that they've been tested

--- tools/ps_link.sh
+++ tools/ps_link.sh
@@ -2,17 +2,29 @@
 
 #
 # don't pass me flags or i'll smack you.
+# exceptions: -o, -l, -L, is OK
 #
 for i in $*
 do
-	echo $i|egrep "^-";
+	echo $i|egrep "^-";	# check for starting with a - 
 	if [ 0 -eq  $? ] ; then 
-		echo SMACK\! No flags to this script, only files to link
-		exit 1
-	fi
-	if [ ! -f $i ]; then
-		echo File $i not found
-		exit 2	
+		echo $i|egrep "^-o" > /dev/null;	# check for starting with a -o 
+		dasho=$?
+		echo $i|egrep "^-l" > /dev/null;	# check for starting with a -l
+		dashl=$?
+		echo $i|egrep "^-L" > /dev/null;	# check for starting with a -L 
+		dashL=$?
+
+		if [ 0 -eq  $dasho ] ; then 
+			echo -n; # blank on purpose
+		elif [ 0 -eq  $dashl ] ; then 
+			echo -n; # blank on purpose
+		elif [ 0 -eq  $dashL ] ; then 
+			echo -n; # blank on purpose
+		else
+			echo SMACK\! No flags to this script, only files to link
+			exit 1
+		fi
 	fi
 done
 
