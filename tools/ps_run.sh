5967f3a19dea561edb1f3cc9c25a081f8c8b6e83
mc2zk <mc2zk@git.zephyr-software.com>
2011-01-11 22:59:01 +0000

    Adding demos directory to peasoup_examples. badA.txt and badB.txt are example bad inputs generated from running the attack generators: attack-gradeA.no_strata and attack-gradeB.no_strata

--- tools/ps_run.sh
+++ tools/ps_run.sh
@@ -3,6 +3,6 @@
 datapath=$1
 shift;
 
-STRATA_ANNOT_FILE=$datapath/a.ncexe.annot STRATA_PC_CONFINE=0 $datapath/a.stratafied $*
+STRATA_ANNOT_FILE=$datapath/a.ncexe.annot STRATA_PC_CONFINE=1 $datapath/a.stratafied $*
 
 
