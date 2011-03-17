274c1b702bd7c1913ad47063ecbd7e51f6955f1b
an7s <an7s@git.zephyr-software.com>
2011-03-17 19:57:37 +0000

    backup

--- tools/p1xform.sh
+++ tools/p1xform.sh
@@ -13,7 +13,7 @@ $STRATA_REWRITE/tools/transforms/p1transform a.ncexe a.ncexe.annot
 cd p1.xform
 for i in `ls *p1*bad*.aspri`
 do
-base=`basename $i`
+base=`basename $i .aspri`
 $STRATA_REWRITE/tools/spasm/spasm $i "$base".bspri  
 done
 
@@ -31,4 +31,23 @@ do
 done
 
 mv grace_replay grace_replay.baseline
+
+P1_DIR=p1.xform
+# remove any candidate functions not covered
+CANDIDATE_FNS=$P1_DIR/a.ncexe.p1.candidates
+while read fn;
+do
+  echo "Evaluating candidate fn: $fn"
+  BSPRI_BAD=$P1_DIR/a.ncexe.xform.p1.bad.$fn.bspri
+  for i in `ls $CONCOLIC/input*.json`
+  do
+    # run with bad SPRI transform
+    STRATA_SPRI_FILE=$BSPRI_BAD $GRACE_HOME/concolic/bin/replayer --stdout --stderr --engine=sdt ./a.ncexe $i
+  done
+
+  mv grace_replay grace_replay.$fn
+
+  # here we need to do a whole bunch of diffs to see if we've detected the bad xform
+  # if none of the inputs detect the bad xform, remove from candidate set of fns to P1 transform
+done < $CANDIDATE_FNS
 
