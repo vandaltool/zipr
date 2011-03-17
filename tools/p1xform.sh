6e5df7f36bfbe4a2a3d40ed8eed48c8afc3e511e
an7s <an7s@git.zephyr-software.com>
2011-03-17 19:30:51 +0000

    Generate ASPRI & BSPRI file for bad xforms

--- tools/p1xform.sh
+++ tools/p1xform.sh
@@ -7,3 +7,28 @@ cd $1
 
 $STRATA_REWRITE/tools/transforms/p1transform a.ncexe a.ncexe.annot
 
+# 
+# Create binary SPRI files for bad transformation
+#
+cd p1.xform
+for i in `ls *p1*bad*.aspri`
+do
+base=`basename $i`
+$STRATA_REWRITE/tools/spasm/spasm $i "$base".bspri  
+done
+
+cd -
+
+echo Current dir:
+pwd
+
+# get output for all inputs on the normal program
+CONCOLIC=concolic.files_a.stratafied_0001
+
+for i in `ls $CONCOLIC/input*.json`
+do
+  $GRACE_HOME/concolic/bin/replayer --stdout --stderr --engine=ptrace ./a.ncexe $i
+done
+
+mv grace_replay grace_replay.baseline
+
