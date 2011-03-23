96730a73670dca2892aa033b23605db50a4c47b9
an7s <an7s@git.zephyr-software.com>
2011-03-23 18:13:54 +0000

    Validate startup/shutdown sequence

---
+++ tools/ps_validate_ss.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+#
+# Validate startup/shutdown sequence by comparing 2 programs
+# and feeding them no inputs
+#
+
+ORIG_BINARY=$1           # original binary
+STRATAFIED_BINARY=$2     # stratafied binary
+BSPRI=$3                 # transformation specificiation SPRI file
+
+echo "=========================================="
+echo "Running ps_validate_ss.sh"
+echo "           ORIG_BINARY: $ORIG_BINARY"
+echo "     STRATAFIED_BINARY: $STRATAFIED_BINARY"
+echo "                 BSPRI: $BSPRI"
+echo "=========================================="
+
+$ORIG_BINARY > a0.orig.out 2> a0.orig.err
+ORIG_VAL=$?
+
+STRATA_SPRI_FILE="$BSPRI" $STRATAFIED_BINARY > a0.strata.out 2> a0.strata.err
+STRATA_VAL=$?
+
+if [ ! "$ORIG_VAL" = "$STRATA_VAL" ];
+  echo "ps_validate_ss.sh: BSPRI=$BSPRI: does not validate" 
+  exit 1
+then
+  exit 0
+fi
