2d72e63d21c7c2eb3bf589c20cda3de840a3331f
an7s <an7s@git.zephyr-software.com>
2011-04-06 14:56:37 +0000

    turn on p1

--- tools/ps_analyze.sh
+++ tools/ps_analyze.sh
@@ -15,6 +15,7 @@ if [ ! -f  $PEASOUP_HOME/tools/getsyms.sh ]; then echo PEASOUP_HOME is set poorl
 if [ "$SMPSA_HOME"X = X ]; then echo Please set SMPSA_HOME; exit 1; fi
 if [ ! -f  $SMPSA_HOME/SMP-analyze.sh ]; then echo SMPSA_HOME is set poorly, please fix.; exit 1; fi
 if [ "$STRATA_HOME"X = X ]; then echo Please set STRATA_HOME; exit 1; fi
+if [ "$STRATA_REWRITE"X = X ]; then echo Please set STRATA_REWRITE; exit 1; fi
 if [ ! -f  $STRATA_HOME/tools/pc_confinement/stratafy_with_pc_confine.sh ]; then echo STRATA_HOME is set poorly, please fix.; exit 1; fi
 
 if [ -z $2 ]; then
@@ -31,6 +32,7 @@ else
 stratafied_exe=$2
 fi
 
+date
 echo "Original program: $orig_exe   New program: $stratafied_exe"
 
 name=`basename $orig_exe`
@@ -69,13 +71,26 @@ $SMPSA_HOME/SMP-analyze.sh a.ncexe
 echo Done.
 
 echo Running concolic testing to generate inputs ...
-$PEASOUP_HOME/tools/do_concolic.sh a  --iterations 50 --logging tracer
+$PEASOUP_HOME/tools/do_concolic.sh a  --iterations 25 --logging tracer,instance_times,trace
 # 2>&1 |egrep -e "INPUT VECTOR:" -e "1: argc ="
 # >/dev/null 2>&1 
 echo Done.
 
 
+#
+# P1 transform 
+#
 
+echo Starting the P1 transform
+date
+
+$PEASOUP_HOME/tools/p1xform.sh $newdir > p1xform.out 2> p1xform.err
+
+date
+echo Done with the P1 transform
+
+# go back to original directory
 cd - > /dev/null 2>&1
 
 cp $newdir/$name.sh $stratafied_exe
+
