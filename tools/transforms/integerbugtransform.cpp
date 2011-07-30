a77337ecc108d57204007a3468c8f11b56b76e04
an7s <an7s@git.zephyr-software.com>
2011-07-30 18:42:09 +0000

    Working version: - adds overflow check to MUL and IMUL - transforms uses the new () callback operator defined on SPASM & SPRI - assumption: callback implemented in Strata via JMP, i.e., we are responsible for setting up context (arguments, return address) - uses address starting at 0x0F000000 for the transform - workaround oversight in spasm ( <address> -> <label> was not implemented )

--- tools/transforms/integerbugtransform.cpp
+++ tools/transforms/integerbugtransform.cpp
@@ -11,6 +11,44 @@ using namespace libIRDB;
 using namespace std;
 
 
+//
+// return available offset 
+//
+virtual_offset_t getAvailableAddress(VariantIR_t *p_virp)
+{
+	// traverse all instructions
+	// grab address
+
+	virtual_offset_t availableAddressOffset = 0;
+	for(
+		set<Instruction_t*>::const_iterator it=p_virp->GetInstructions().begin();
+		it!=p_virp->GetInstructions().end(); 
+		++it
+	   )
+	{
+		Instruction_t* insn=*it;
+		if (!insn) continue;
+
+		AddressID_t* addr = insn->GetAddress();
+		virtual_offset_t offset = addr->GetVirtualOffset();
+                
+		if (offset > availableAddressOffset)
+		{
+			availableAddressOffset = offset;
+		}
+	}
+
+	// @todo: lookup instruction size so that we don't waste any space
+	// for some reason the max available address is incorrect! was ist los?
+static int counter = -16;
+        counter += 16;
+	return 0xf0000000 + counter;
+// availableAddressOffset + 16;
+}
+
+//
+// Given an instruction, returns its disassembly
+//
 string getAssembly(Instruction_t *p_instruction)
 {
 	DISASM disasm;
@@ -26,7 +64,7 @@ string getAssembly(Instruction_t *p_instruction)
 }
 
 //
-// returns true iff instruction is mul or imul
+// Returns true iff instruction is mul or imul
 //
 bool isMultiplyInstruction32(Instruction_t *p_instruction)
 {
@@ -49,80 +87,61 @@ bool isMultiplyInstruction32(Instruction_t *p_instruction)
         return (found_pos == 0 || found_pos == 1); // we expect mul or imul to be the first word in the string
 }
 
-void addOverflowCheck(VariantIR_t *p_virp, Instruction_t *p_instruction/*, Instruction_t *p_overflowHandler*/)
+//
+//      jno <originalFallthroughInstruction>
+//      pusha
+//      pushf
+//      push L1
+//      ... setup detector ...
+//  L1: popf
+//      popa
+//
+void addOverflowCheck(VariantIR_t *p_virp, Instruction_t *p_instruction)
 {
-/*
-insert after the instruction, a check for overflow:
-     imul ...
-     jno I0
-     pusha
-     pushf
-     call L0
-L0:  add dword[esp], 8     ; 8 is 7 (size of this instruction) + 1 (size of the jmp)
-     jmp detector     ; this will be done by strata, we need to emit . () detector
-     popf
-     popa
-I0:  <original next instruction>
-
-From spasm, to get the right hex values:
-
-ff000000 ** 2 71 15     #jno I0
-ff000002 ** 1 60        #pusha
-ff000003 ** 1 9c        #pushf
-ff000004 ** 5 e8 00 00 00 00    #call L0
-FF000009 ** 7 81 04 24 08 00 00 00      #src addr = <L0> ; add dword [esp], 0x08
-ff000015 ** 1 9d        #popf + callback detector (reserved 1 byte for strata's jmp/callback
-ff000016 ** 1 61        #popa
-FF000017 ** 1 f4        #src addr = <I0> ; hlt
-FF000018 ** 1 90        #src addr = <foobar> ; nop
-*/
-
+	assert(p_virp && p_instruction);
+	
         string dataBits;
 
 	AddressID_t *jno_a =new AddressID_t;
 	AddressID_t *pusha_a =new AddressID_t;
 	AddressID_t *pushf_a =new AddressID_t;
-	AddressID_t *pusharg1_a =new AddressID_t;
-	AddressID_t *call_a =new AddressID_t;
-	AddressID_t *add_a =new AddressID_t;
+	AddressID_t *pusharg_a =new AddressID_t;
+	AddressID_t *pushret_a =new AddressID_t;
 	AddressID_t *popf_a =new AddressID_t;
+	AddressID_t *poparg_a =new AddressID_t;
 	AddressID_t *popa_a =new AddressID_t;
 
-	Instruction_t* jno_i = new Instruction_t();
-	Instruction_t* pusha_i = new Instruction_t();
-	Instruction_t* pushf_i = new Instruction_t();
-	Instruction_t* pusharg1_i = new Instruction_t();
-	Instruction_t* call_i = new Instruction_t();
-	Instruction_t* add_i = new Instruction_t();
-	Instruction_t* popf_i = new Instruction_t();
-	Instruction_t* popa_i = new Instruction_t();
+	jno_a->SetFileID(p_instruction->GetAddress()->GetFileID());
+	pusha_a->SetFileID(p_instruction->GetAddress()->GetFileID());
+	pushf_a->SetFileID(p_instruction->GetAddress()->GetFileID());
+	pusharg_a->SetFileID(p_instruction->GetAddress()->GetFileID());
+	pushret_a->SetFileID(p_instruction->GetAddress()->GetFileID());
+	popf_a->SetFileID(p_instruction->GetAddress()->GetFileID());
+	poparg_a->SetFileID(p_instruction->GetAddress()->GetFileID());
+	popa_a->SetFileID(p_instruction->GetAddress()->GetFileID());
 
+	Instruction_t* jno_i = new Instruction_t;
+	Instruction_t* pusha_i = new Instruction_t;
+	Instruction_t* pushf_i = new Instruction_t;
+	Instruction_t* pusharg_i = new Instruction_t;
+	Instruction_t* pushret_i = new Instruction_t;
+	Instruction_t* popf_i = new Instruction_t;
+	Instruction_t* poparg_i = new Instruction_t;
+	Instruction_t* popa_i = new Instruction_t;
+
+	// pin the popf instruction to a free address
+	virtual_offset_t postDetectorReturn = getAvailableAddress(p_virp);
+fprintf(stderr,"post detector return set to: 0x%x\n", postDetectorReturn);
+	popf_a->SetVirtualOffset(postDetectorReturn);
 
         jno_i->SetAddress(jno_a);
         pusha_i->SetAddress(pusha_a);
         pushf_i->SetAddress(pushf_a);
-        call_i->SetAddress(call_a);
-        add_i->SetAddress(add_a);
+        pusharg_i->SetAddress(pusharg_a);
+        pushret_i->SetAddress(pushret_a);
         popf_i->SetAddress(popf_a);
+        poparg_i->SetAddress(poparg_a);
         popa_i->SetAddress(popa_a);
-
-        // add new address to IR
-	p_virp->GetAddresses().insert(jno_a);
-	p_virp->GetAddresses().insert(pusha_a);
-	p_virp->GetAddresses().insert(pushf_a);
-	p_virp->GetAddresses().insert(call_a);
-	p_virp->GetAddresses().insert(add_a);
-	p_virp->GetAddresses().insert(popf_a);
-	p_virp->GetAddresses().insert(popa_a);
-
-        // add new instructions to IR
-	p_virp->GetInstructions().insert(jno_i);
-	p_virp->GetInstructions().insert(pusha_i);
-	p_virp->GetInstructions().insert(pushf_i);
-	p_virp->GetInstructions().insert(call_i);
-	p_virp->GetInstructions().insert(add_i);
-	p_virp->GetInstructions().insert(popf_i);
-	p_virp->GetInstructions().insert(popa_i);
 
         // handle the original mul or imul instruction
 	Instruction_t* nextOrig_i = p_instruction->GetFallthrough();
@@ -142,49 +161,48 @@ FF000018 ** 1 90        #src addr = <foobar> ; nop
         dataBits[0] = 0x60;
         pusha_i->SetDataBits(dataBits);
         pusha_i->SetComment(getAssembly(pusha_i));
-	pusha_i->SetFallthrough(pushf_i); 
+	pusha_i->SetFallthrough(pusharg_i); 
 
         // pushf   
         dataBits.resize(1);
         dataBits[0] = 0x9c;
         pushf_i->SetDataBits(dataBits);
         pushf_i->SetComment(getAssembly(pushf_i));
-	pushf_i->SetFallthrough(call_i); 
+	pushf_i->SetFallthrough(pusharg_i); 
 
-        // call ff000004 ** 5 e8 00 00 00 00    #call L0
+        // push arg
         dataBits.resize(5);
-        dataBits[0] = 0xe8;
-        dataBits[1] = 0x01;
-        dataBits[2] = 0x00;
-        dataBits[3] = 0x00;
-        dataBits[4] = 0x00;
-        call_i->SetDataBits(dataBits);
-        call_i->SetTarget(add_i);
-        call_i->SetFallthrough(add_i);
-        call_i->SetComment(getAssembly(call_i));
+        dataBits[0] = 0x68;
+	virtual_offset_t *tmp = (virtual_offset_t *) &dataBits[1];
+	*tmp = p_instruction->GetAddress()->GetVirtualOffset();
+        pusharg_i->SetDataBits(dataBits);
+        pusharg_i->SetComment(getAssembly(pusharg_i));
+	pusharg_i->SetFallthrough(pushret_i); 
 
-        // add FF000009 ** 8 36 81 04 24 09 00 00 00      #src addr = <L0> ; add dword [esp], 0x0A
-        dataBits.resize(8);
-        dataBits[0] = 0x36;
-        dataBits[1] = 0x81;
-        dataBits[2] = 0x04;
-        dataBits[3] = 0x24;
-        dataBits[4] = 0x0A; // 8 + 1 + 1?
-        dataBits[5] = 0x00;
-        dataBits[6] = 0x00;
-        dataBits[7] = 0x00;
-        add_i->SetDataBits(dataBits);
-        add_i->SetFallthrough(popf_i);
-        add_i->SetComment(getAssembly(add_i));
-        cout << "Sanity check for add: " << getAssembly(add_i) << endl;
+        // pushret   
+        dataBits.resize(5);
+        dataBits[0] = 0x68;
+	tmp = (virtual_offset_t *) &dataBits[1];
+	*tmp = postDetectorReturn;
+        pushret_i->SetDataBits(dataBits);
+        pushret_i->SetComment(getAssembly(pushret_i));
+	pushret_i->SetFallthrough(poparg_i); 
+
+	// poparg
+        dataBits.resize(1);
+        dataBits[0] = 0x58;
+        poparg_i->SetDataBits(dataBits);
+        poparg_i->SetComment(getAssembly(poparg_i) + " -- with callback to integer_overflow_detector()");
+	poparg_i->SetFallthrough(popf_i); 
+	poparg_i->SetIsIndirectTarget(true);  
 
         // popf   
         dataBits.resize(1);
         dataBits[0] = 0x9d;
         popf_i->SetDataBits(dataBits);
-        popf_i->SetComment(getAssembly(popf_i) + string(" -- with callback to test_detector()"));
+        popf_i->SetComment(getAssembly(popf_i));
 	popf_i->SetFallthrough(popa_i); 
-	popf_i->SetCallback("test_detector"); 
+	popf_i->SetCallback("integer_overflow_detector"); 
 
         // popa   
         dataBits.resize(1);
@@ -192,6 +210,30 @@ FF000018 ** 1 90        #src addr = <foobar> ; nop
         popa_i->SetDataBits(dataBits);
         popa_i->SetComment(getAssembly(popa_i));
 	popa_i->SetFallthrough(nextOrig_i); 
+
+        // add new address to IR
+	p_virp->GetAddresses().insert(jno_a);
+	p_virp->GetAddresses().insert(pusha_a);
+	p_virp->GetAddresses().insert(pusharg_a);
+	p_virp->GetAddresses().insert(pushf_a);
+	p_virp->GetAddresses().insert(pushret_a);
+	p_virp->GetAddresses().insert(popf_a);
+	p_virp->GetAddresses().insert(poparg_a);
+	p_virp->GetAddresses().insert(popa_a);
+
+        // add new instructions to IR
+	p_virp->GetInstructions().insert(jno_i);
+	p_virp->GetInstructions().insert(pusha_i);
+	p_virp->GetInstructions().insert(pusharg_i);
+	p_virp->GetInstructions().insert(pushf_i);
+	p_virp->GetInstructions().insert(pushret_i);
+	p_virp->GetInstructions().insert(popf_i);
+	p_virp->GetInstructions().insert(poparg_i);
+	p_virp->GetInstructions().insert(popa_i);
+
+	fprintf(stderr,"sanity check address of popf: 0x%x %u   fileID:%d\n", popf_a->GetVirtualOffset(), popf_a->GetVirtualOffset(), popf_a->GetFileID());
+	fprintf(stderr,"sanity check address of pushf: 0x%x %u   fileID:%d\n", pushf_a->GetVirtualOffset(), pushf_a->GetVirtualOffset(), pushf_a->GetFileID());
+
 }
 
 main(int argc, char* argv[])
@@ -241,7 +283,7 @@ main(int argc, char* argv[])
 		Instruction_t* insn=*it;
                 
 		if (isMultiplyInstruction32(insn) && insn->GetFunction() 
-			&& insn->GetFunction()->GetName().find("test_") != string::npos )
+		/*	&& insn->GetFunction()->GetName().find("test_") != string::npos */ )
 		{
 			numberMul++;
 			cout << "found MUL: address: " << insn->GetAddress()
@@ -249,7 +291,6 @@ main(int argc, char* argv[])
 	                     << " in function: " << insn->GetFunction()->GetName() << endl;
 			// for now, insert overflow check to all IMUL instructions
 			// later, we'll want to be more judicious about where to insert overflow checks
-//			addOverflowCheck(virp, insn, overflowHandler);
 			addOverflowCheck(virp, insn);
 		}
 	}
