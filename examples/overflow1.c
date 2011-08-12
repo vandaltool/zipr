f549f949361de42d38c3580f08c6692df25d6139
an7s <an7s@git.zephyr-software.com>
2011-08-12 13:57:57 +0000

    commit examples

--- examples/overflow1.c
+++ examples/overflow1.c
@@ -6,6 +6,14 @@ int test_mul32_overflow(int value)
   return result;
 }
 
+int test_addsub_overflow(int value)
+{
+  printf("tests_addsub_overflow(%d): enter\n", value);
+  int result = value + 2000000000; 
+  printf("tests_addsub_overflow -- about to exit\n");
+  return result;
+}
+
 char *test_sign_unsign(int size)
 {
   return malloc(size * 1024); 
@@ -13,12 +21,19 @@ char *test_sign_unsign(int size)
 
 int main(int argc, char **argv)
 {
+  printf("\n\n----------------------------");
+
   printf("main(): enter\n");
 
   int value = atoi(argv[1]);
-  printf("value_overflow=%d\n", test_mul32_overflow(value));
+
+  printf("mul: value_overflow=%d\n", test_mul32_overflow(value));
+
+  printf("addsub: value_overflow=%d\n", test_addsub_overflow(value));
 
   char *x = test_sign_unsign(value);
   sprintf(x,"x");
   printf("x=%s\n", x);
+
+  printf("----------------------------\n\n");
 }
