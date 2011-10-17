3ddb08a72ba2f1938040a43adce8da0cf7062cf5
an7s <an7s@git.zephyr-software.com>
2011-10-17 18:37:14 +0000

    ...
    Former-commit-id: 360a46993fcf81be3068608cd8ebe0d4b1733ed9

--- tests/coreutils/cat_manual_tests.sh
+++ tests/coreutils/cat_manual_tests.sh
@@ -7,19 +7,19 @@ echo "hello" > inputfile1
 
 # test lots of options
 cat -Abetvsnu inputfile1 > outputfile1
-$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./cat -Abetvsnu inputfile1 > outputfile1" --prog cat  --infile inputfile1 --outfile outputfile1 --name shload_options
+$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./cat -Abetvsnu inputfile1 > outputfile1" --prog cat  --infile inputfile1 --outfile outputfile1 --name cat.shload_options
 
 # test basic
 cat inputfile1 > outputfile1
-$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./cat inputfile1 > outputfile1" --prog cat  --infile inputfile1 --outfile outputfile1 --name basic
+$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./cat inputfile1 > outputfile1" --prog cat  --infile inputfile1 --outfile outputfile1 --name cat.basic
 
 # test invalid options
 cat -MX inputfile1 2>&1 | grep -vi cat > outputfile1
-$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./cat -MX inputfile1 2>&1 | grep -vi stratafied | grep -vi cat > outputfile1" --prog cat --infile inputfile1 --outfile outputfile1 --name invalid_options
+$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./cat -MX inputfile1 2>&1 | grep -vi stratafied | grep -vi cat > outputfile1" --prog cat --infile inputfile1 --outfile outputfile1 --name cat.invalid_options
 
 # test help 
 cat --help --version 2>&1 | grep -vi cat > outputfile1
-$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./cat --help --version 2>&1 | grep -vi stratafied | grep -vi cat > outputfile1" --prog cat --outfile outputfile1 --name usage
+$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./cat --help --version 2>&1 | grep -vi stratafied | grep -vi cat > outputfile1" --prog cat --outfile outputfile1 --name cat.usage
 
 # cleanup
 rm inputfile1 outputfile1
