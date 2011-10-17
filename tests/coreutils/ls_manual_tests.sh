3255befbaab8495b407324a64502e7f6b636d0e1
an7s <an7s@git.zephyr-software.com>
2011-10-17 19:20:25 +0000

    Fixed tests
    Former-commit-id: ff44adfca0104a26c55c88337b5cbfcc9e5fa274

--- tests/coreutils/ls_manual_tests.sh
+++ tests/coreutils/ls_manual_tests.sh
@@ -19,31 +19,22 @@ echo "hello" > inputfile1
 # test whole bunch of options -- output is non-deterministic so we just make sure we have the same number of lines
 # also when something goes wrong during testing, the program usually just crashes
 ls -ltarHksbBiXR inputfile1 | wc -l > outputfile1
-$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls -ltarHksbBiXR inputfile1 | wc -l > outputfile1" --prog foobar --infile inputfile1 --outfile outputfile1 --name shload_flags
-
-# basic functionality -- don't bother with comparing outputs
-$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls" --prog ls 
-
-# test invalid options
-ls -MX inputfile1 | grep -vi invalid | grep -vi usage > outputfile1
-$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls -MX inputfile1 | grep -vi invalid | grep -vi usage > outputfile1" --prog ls --infile inputfile1 --outfile outputfile1 --name invalid_options
-
-# test help 
-ls --help | grep -vi report | grep -vi usage > outputfile1
-$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls --help | grep -vi report | grep -vi usage > outputfile1" --prog ls --outfile outputfile1 --name usage
+$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls -ltarHksbBiXR inputfile1 | wc -l > outputfile1" --prog ls --infile inputfile1 --outfile outputfile1 --name ls.shload_flags
 
 # test some option flags
 ls -kfsZqp inputfile1 > outputfile1
-$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls -kfsZqp inputfile1 > outputfile1" --prog ls --infile inputfile1 --outfile outputfile1 --name flag_combo1
+$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls -kfsZqp inputfile1 > outputfile1" --prog ls --infile inputfile1 --outfile outputfile1 --name ls.flag_combo
 
 # test --ignore
 ls --ignore=hello inputfile1 > outputfile1
-$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls --ignore=hello inputfile1 > outputfile1" --prog ls --infile inputfile1 --outfile outputfile1 --name ignore
+$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls --ignore=hello inputfile1 > outputfile1" --prog ls --infile inputfile1 --outfile outputfile1 --name ls.ignore_option
+# test invalid options
+ls -MX inputfile1 | grep -vi invalid | grep -vi usage > outputfile1
+$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls -MX inputfile1 | grep -vi invalid | grep -vi usage > outputfile1" --prog ls --infile inputfile1 --outfile outputfile1 --name ls.invalid_options
 
-# test whole bunch of options -- output is non-deterministic so we just make sure we have the same number of lines
-# also when something goes wrong during testing, the program usually just crashes
-ls -ltarHksbBiXR inputfile1 | wc -l > outputfile1
-$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls -ltarHksbBiXR inputfile1 | wc -l > outputfile1" --prog ls --infile inputfile1 --outfile outputfile1 --name shload_flags
+# test help 
+ls --help | grep -vi report | grep -vi usage > outputfile1
+$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls --help | grep -vi report | grep -vi usage > outputfile1" --prog ls --outfile outputfile1 --name ls.usage
 
 # cleanup
 rm inputfile1 outputfile1
