8396f0d0a80b76ef3819c0b5325e36a8d75eba7a
an7s <an7s@git.zephyr-software.com>
2011-10-19 15:04:34 +0000

    Updated ls test
    Former-commit-id: 7e544f13763e7c8b3e569ea3a111f96513db8d53

--- tests/coreutils/ls_manual_tests.sh
+++ tests/coreutils/ls_manual_tests.sh
@@ -19,22 +19,28 @@ echo "hello" > inputfile1
 # test whole bunch of options -- output is non-deterministic so we just make sure we have the same number of lines
 # also when something goes wrong during testing, the program usually just crashes
 ls -ltarHksbBiXR inputfile1 | wc -l > outputfile1
-$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls -ltarHksbBiXR inputfile1 | wc -l > outputfile1" --prog ls --infile inputfile1 --outfile outputfile1 --name ls.shload_flags
+exitcode=$?
+$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls -ltarHksbBiXR inputfile1 | wc -l > outputfile1" --prog ls --infile inputfile1 --outfile outputfile1 --name ls.shload_flags --exitcode $exitcode
 
 # test some option flags
 ls -kfsZqp inputfile1 > outputfile1
-$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls -kfsZqp inputfile1 > outputfile1" --prog ls --infile inputfile1 --outfile outputfile1 --name ls.flag_combo
+exitcode=$?
+$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls -kfsZqp inputfile1 > outputfile1" --prog ls --infile inputfile1 --outfile outputfile1 --name ls.flag_combo --exitcode $exitcode
 
 # test --ignore
 ls --ignore=hello inputfile1 > outputfile1
-$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls --ignore=hello inputfile1 > outputfile1" --prog ls --infile inputfile1 --outfile outputfile1 --name ls.ignore_option
+exitcode=$?
+$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls --ignore=hello inputfile1 > outputfile1" --prog ls --infile inputfile1 --outfile outputfile1 --name ls.ignore_option --exitcode $exitcode
+
 # test invalid options
-ls -MX inputfile1 | grep -vi invalid | grep -vi usage > outputfile1
-$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls -MX inputfile1 | grep -vi invalid | grep -vi usage > outputfile1" --prog ls --infile inputfile1 --outfile outputfile1 --name ls.invalid_options
+ls -MX inputfile1 
+exitcode=$?
+$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls inputfile1" --prog ls --infile inputfile1 --name ls.invalid_options --exitcode $exitcode
 
 # test help 
-ls --help | grep -vi report | grep -vi usage > outputfile1
-$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls --help | grep -vi report | grep -vi usage > outputfile1" --prog ls --outfile outputfile1 --name ls.usage
+exitcode=$?
+ls --help | grep sort | grep -v ls | grep -v strata > outputfile1
+$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls --help | grep sort | grep -v ls | grep -v strata > outputfile1" --prog ls --outfile outputfile1 --name ls.usage --exitcode $exitcode
 
 # cleanup
 rm inputfile1 outputfile1
