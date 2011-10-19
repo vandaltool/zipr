213e0ec52ded426fcbd79df37a8a8e8d30878a00
an7s <an7s@git.zephyr-software.com>
2011-10-19 02:08:32 +0000

    Added annotation file strata en
    Former-commit-id: d1b00d9b628fed3a5a3bf758fe11aa1f9f76a2cd

--- tools/run_stratafied.tmpl.sh
+++ tools/run_stratafied.tmpl.sh
@@ -3,7 +3,7 @@
 # Don't use this script directly
 
 # need to get these from ps_analyze instead of hardwiring them
-STRATA_DOUBLE_FREE=1 STRATA_HEAPRAND=1 STRATA_PC_CONFINE=1 STRATA_PC_CONFINE_XOR=0 STRATA_SIEVE=1 STRATA_RC=1 STRATA_PARTIAL_INLINING=0 ../../../a.stratafied "$@"
+STRATA_DOUBLE_FREE=1 STRATA_HEAPRAND=1 STRATA_PC_CONFINE=1 STRATA_PC_CONFINE_XOR=0 STRATA_SIEVE=1 STRATA_RC=1 STRATA_PARTIAL_INLINING=0 STRATA_ANNOT_FILE=../../../a.ncexe.annot ../../../a.stratafied "$@"
 status=$?
 
 exit $status
