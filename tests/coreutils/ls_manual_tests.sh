90228f829343c5b18c5e63081fcaba3bd6de67a0
an7s <an7s@git.zephyr-software.com>
2011-10-21 19:52:35 +0000

    Updated tests
    Former-commit-id: 08511c30d5dc711eed8ea64d6e1d8e9f402ea02f

--- tests/coreutils/ls_manual_tests.sh
+++ tests/coreutils/ls_manual_tests.sh
@@ -16,6 +16,10 @@
 
 echo "hello" > inputfile1
 
+# test basic usage - does it crash?
+ls
+$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls" --prog ls --name ls.basic --exitcode 0
+
 # test whole bunch of options -- output is non-deterministic so we just make sure we have the same number of lines
 # also when something goes wrong during testing, the program usually just crashes
 ls -ltarHksbBiXR inputfile1 | wc -l > outputfile1
@@ -38,9 +42,31 @@ exitcode=$?
 $PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls -MX inputfile1" --prog ls --infile inputfile1 --name ls.invalid_options --exitcode $exitcode
 
 # test help 
-exitcode=$?
 ls --help | grep sort | grep -v ls | grep -v strata > outputfile1
+exitcode=$?
 $PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls --help | grep sort | grep -v ls | grep -v strata > outputfile1" --prog ls --outfile outputfile1 --name ls.usage --exitcode $exitcode
+
+# test recursive
+$PEASOUP_HOME/tools/manual_test_import.sh --cmd "mkdir /tmp/ls.tmp 2>/dev/null; ./ls -R /tmp/ls.tmp" --prog ls --name ls.recursive --exitcode 0
+
+# test recursive 2
+$PEASOUP_HOME/tools/manual_test_import.sh --cmd "mkdir -p /tmp/ls.tmp.foobar 2>/dev/null; ./ls -R /tmp/ls.tmp" --prog ls --name ls.recursive2 --exitcode 0
+
+# test basic usage - does it crash?
+ls -t
+$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls -t" --prog ls --name ls.timestamp --exitcode 0
+
+# test basic usage - does it crash?
+ls -u
+$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls -u" --prog ls --name ls.u --exitcode 0
+
+# test basic usage - does it crash?
+ls -Z
+$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls -Z" --prog ls --name ls.Z --exitcode 0
+
+# test basic usage - does it crash?
+ls --full-time
+$PEASOUP_HOME/tools/manual_test_import.sh --cmd "./ls --full-time" --prog ls --name ls.fulltime --exitcode 0
 
 # cleanup
 rm inputfile1 outputfile1
