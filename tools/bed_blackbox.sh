b46f88baa553435c38b86e85687214e5e0606fb2
an7s <an7s@git.zephyr-software.com>
2011-10-28 18:07:51 +0000

    New BED version: one can just register a test script that will be called by Pn to validate transforms
    Former-commit-id: c84c5af34103366c3333eb487b2603038aca64c9

---
+++ tools/bed_blackbox.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+#
+# BED: Behavioral Equivalence Detector
+#
+# Usage: bed_manual.sh <variantID> <ASPRI_file> <BSPRI_file>
+#
+# Use user-specified input/output pair 
+# 
+# Assumptions:
+#    (1) we are in the Peasoup sub-directory created by ps_analyze.sh
+#    (2) there exists a file "new_command_name" whose content is the basename of the peasoupified target binary
+#
+
+PEASOUP_DIR=`pwd`
+
+variantid=$1
+aspri=$2
+bspri=$3
+
+SCRIPT_NAME=$PEASOUP_DIR/manual_test_wrapper
+
+# generate the bspri code
+$SECURITY_TRANSFORMS_HOME/tools/spasm/spasm $aspri $bspri $PEASOUP_DIR/stratafier.o.exe  > spasm.out 2> spasm.err
+if [ ! $? -eq 0 ]; then
+  echo "BED: spasm error -- exiting"
+  exit 1
+fi
+
+# toolchain expects a BSPRI file at this location
+cp $bspri $PEASOUP_DIR/a.irdb.bspri
+
+NAME=`cat new_command_name`
+TRANSFORMED_PROGRAM="$PEASOUP_DIR/$NAME"
+
+# invoke the user-supplied test script
+# pass as argument the transformed program
+$SCRIPT_NAME $TRANSFORMED_PROGRAM
+status=$? 
+
+exit $status
