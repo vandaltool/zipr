9505e62e9c7cf15c5affd97dcff76e96a28ac3b5
an7s <an7s@git.zephyr-software.com>
2011-10-28 18:07:11 +0000

    Reorganization of BED validation tool
    Former-commit-id: 18757ded7bcffcd3539419ef9d0a79bfa724ad83

--- tools/ps_validate.sh
+++ tools/ps_validate.sh
@@ -17,6 +17,10 @@ BASELINE_OUTPUT_DIR=$INPUT_DIR/sandboxed-files
 
 REPLAYER_TIMEOUT=120        # timeout value for when replaying input -- for now 120 seconds per input
 
+TOP_LEVEL=`pwd`
+
+mkdir replay 2>/dev/null
+
 echo "=========================================="
 echo "Running ps_validate.sh"
 echo "                STRATAFIED_BINARY: $STRATAFIED_BINARY"
@@ -29,21 +33,25 @@ echo "=========================================="
 # name of files describing inputs is of the form: input_0001.json, input_0002.json, ...
 #
 
-echo "ps_validate: BED: warning: @todo: need to handle files other than stdout, stderr"
+echo "ps_validate.sh: BED: warning: @todo: need to handle files other than stdout, stderr"
+
+cd replay
 
 for i in `ls $INPUT_DIR/input*.json`
 do
-  echo ""
+  echo "Testing input spec: $i"
   input=`basename $i .json`
   input_number=`echo $input | sed "s/input_//"`
+
+  mkdir $input_number 2>/dev/null
+  cd $input_number
 
   # make sure the output files exist
   touch stdout.$input
   touch stderr.$input
 
-  echo "ps_validate.sh: cmd: STRATA_SPRI_FILE=$BSPRI timeout $REPLAYER_TIMEOUT $GRACE_HOME/concolic/bin/replayer --symbols=a.sym --stdout=stdout.$input --stderr=stderr.$input --engine=sdt ./a.stratafied $i"
-#  STRATA_SPRI_FILE="$BSPRI" "$GRACE_HOME/concolic/bin/replayer" --timeout=$REPLAYER_TIMEOUT --symbols=a.sym --stdout=stdout.$input --stderr=stderr.$input --engine=sdt $STRATAFIED_BINARY $i
-  STRATA_SPRI_FILE="$BSPRI" timeout $REPLAYER_TIMEOUT "$GRACE_HOME/concolic/bin/replayer" --timeout=120 --symbols=a.sym --stdout=stdout.$input --stderr=stderr.$input --engine=sdt $STRATAFIED_BINARY $i || exit 2
+  echo "ps_validate.sh: cmd: STRATA_SPRI_FILE=$BSPRI timeout $REPLAYER_TIMEOUT $GRACE_HOME/concolic/bin/replayer --timeout=$REPLAYER_TIMEOUT --symbols=$TOP_LEVEL/a.sym --stdout=stdout.$input --stderr=stderr.$input --engine=sdt $STRATAFIED_BINARY $i"
+  STRATA_SPRI_FILE="$BSPRI" timeout $REPLAYER_TIMEOUT "$GRACE_HOME/concolic/bin/replayer" --timeout=$REPLAYER_TIMEOUT --symbols=$TOP_LEVEL/a.sym --stdout=stdout.$input --stderr=stderr.$input --engine=sdt $STRATAFIED_BINARY $i || exit 2
 
   BASELINE_OUTPUT_STDOUT=$BASELINE_OUTPUT_DIR/run_$input_number/stdout
   BASELINE_OUTPUT_STDERR=$BASELINE_OUTPUT_DIR/run_$input_number/stderr
@@ -53,6 +61,9 @@ do
   
   if [ -f $BASELINE_OUTPUT_STDOUT ];
   then
+    echo ""
+	pwd
+	echo "diffing stdout.$input $BASELINE_OUTPUT_STDOUT"
     diff stdout.$input $BASELINE_OUTPUT_STDOUT
     if [ ! $? -eq 0 ]; then
       echo "ps_validate.sh: divergence detected for input: $i (stdout)"
@@ -86,9 +97,14 @@ do
   echo "ps_validate.sh: input $i validates"
 
   # remove tmp files
-  rm stdout.$input 2>/dev/null
-  rm stderr.$input 2>/dev/null
+#  rm stdout.$input 2>/dev/null
+#  rm stderr.$input 2>/dev/null
+
+  cd -
+
 done
+
+cd -
 
 echo "ps_validate.sh: All inputs validated"
 exit 0
