79c80e0176ccab9c84a03596a537ae61f8151511
an7s <an7s@git.zephyr-software.com>
2011-11-02 14:40:47 +0000

    ...
    Former-commit-id: 618b4d01a9cf4269a72056b304f56880f3798461

--- libtransform/tests/mul.c
+++ libtransform/tests/mul.c
@@ -3,9 +3,7 @@ int main(int argc, char **argv)
 {
   unsigned a = (unsigned) atoi(argv[1]);
   unsigned b = (unsigned) atoi(argv[2]);
-  unsigned d = a * b;
+  unsigned d = a * b; // IMUL
 
   printf("%u\n", d);
-
-//  printf("%u * %u = %u\n", a, b, d);
 }
