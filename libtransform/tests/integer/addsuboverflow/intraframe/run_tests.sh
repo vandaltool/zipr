abe3a05a17807cb0ad3e85cc5b7176693266ea30
an7s <an7s@git.zephyr-software.com>
2011-11-11 21:13:34 +0000

    ...
    Former-commit-id: dabd2f65489abebc2fa6d44dc5a3eae8c2fa33c1

--- libtransform/tests/integer/addsuboverflow/intraframe/run_tests.sh
+++ libtransform/tests/integer/addsuboverflow/intraframe/run_tests.sh
@@ -9,9 +9,9 @@ do
 	echo running $i
 	$TEST_HARNESS_HOME/run_one_test.sh $i no_redirect >> $outfile
 	
-#	if [ ! $? -eq 0 ]; then
-#		echo Test $i failed >> $outfile
-#	else
-#		echo Test $i success >> $outfile
-#	fi
+	if [ ! $? -eq 0 ]; then
+		echo Test $i failed >> $outfile
+	else
+		echo Test $i success >> $outfile
+	fi
 done
