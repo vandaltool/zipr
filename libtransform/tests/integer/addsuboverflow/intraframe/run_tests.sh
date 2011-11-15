d6e9a63649afb4e3e5c7173715001644d5877f73
an7s <an7s@git.zephyr-software.com>
2011-11-15 20:01:01 +0000

    ...
    Former-commit-id: 0650cdb5ac892bbd6d4f66ec64c84e3cec7e2810

--- libtransform/tests/integer/addsuboverflow/intraframe/run_tests.sh
+++ libtransform/tests/integer/addsuboverflow/intraframe/run_tests.sh
@@ -4,10 +4,12 @@ outfile=tests.out
 rm $outfile
 touch $outfile
 
+PWD=`pwd`
+
 for i in `ls addsub*.sh`
 do
 	echo running $i
-	$TEST_HARNESS_HOME/run_one_test.sh $i no_redirect >> $outfile
+	$TEST_HARNESS_HOME/run_one_test.sh $PWD/$i no_redirect >> $outfile
 	
 	if [ ! $? -eq 0 ]; then
 		echo Test $i failed >> $outfile
