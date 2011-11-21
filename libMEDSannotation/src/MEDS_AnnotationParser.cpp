6d203268b876edb6f665012e1544dcc71f7ed6ad
an7s <an7s@git.zephyr-software.com>
2011-11-21 16:22:45 +0000

    removed diagnostic/debug stmts
    Former-commit-id: a905750ec382b1adcc9ed3681354064594904358

--- libMEDSannotation/src/MEDS_AnnotationParser.cpp
+++ libMEDSannotation/src/MEDS_AnnotationParser.cpp
@@ -15,19 +15,12 @@ MEDS_AnnotationParser::MEDS_AnnotationParser(istream &p_inputStream)
 	    getline(p_inputStream, line);
 		if (line.empty()) continue;
 
-		cerr << "Testing line: " << line << endl;
-
 		MEDS_InstructionCheckAnnotation annot(line);
 
 		if (annot.isValid())
 		{
 			VirtualOffset vo = annot.getVirtualOffset();
 			m_annotations[vo] = annot;
-
-			if (annot.isOverflow()) std::cerr << "Overflow check annotation" << std::endl;
-			if (annot.isUnderflow()) std::cerr << "Underflow check annotation" << std::endl;
-			if (annot.isSigned()) std::cerr << "signed annotation" << std::endl;
-			if (annot.isUnsigned()) std::cerr << "unsigned annotation" << std::endl;
 		}
 	}
 }
