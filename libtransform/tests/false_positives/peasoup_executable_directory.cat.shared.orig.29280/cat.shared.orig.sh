afa091db8da03d3bfa0517ea306beb1a1ca9cd36
an7s <an7s@git.zephyr-software.com>
2011-11-25 21:57:51 +0000

    Better diagnostics for false positives
    Former-commit-id: 9ce64e20e6d53852aa3721c6d7fb6b26f9b070f5

--- libtransform/tests/false_positives/peasoup_executable_directory.cat.shared.orig.29280/cat.shared.orig.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/home/jdh8d/peasoup/uva/security_transforms.trunk/libtransform/tests/false_positives/peasoup_executable_directory.cat.shared.orig.29280/ps_run.sh /home/jdh8d/peasoup/uva/security_transforms.trunk/libtransform/tests/false_positives/peasoup_executable_directory.cat.shared.orig.29280 "$@"
