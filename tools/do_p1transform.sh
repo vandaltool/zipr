1321b197c6156864c866f6dfe0da795c95ed91c8
bdr7fv <bdr7fv@git.zephyr-software.com>
2012-04-12 21:59:49 +0000

    New BED validation, now using the replayer for both baseline and replaying a transform.
    Former-commit-id: 85f7cc488e3c8135244bb5d6d015d564231f4199

--- tools/do_p1transform.sh
+++ tools/do_p1transform.sh
@@ -11,17 +11,26 @@ ORIGINAL_BINARY=$2
 MEDS_ANNOTATION_FILE=$3
 BED_SCRIPT=$4
 TIMEOUT_VALUE=$5
+REPLAYER_TIMEOUT=120
+TOP_LEVEL=`pwd`
+BASELINE_DIR=$TOP_LEVEL/replayer_baseline
+
+mkdir $BASELINE_DIR
+
+STRATAFIED_BINARY=$TOP_LEVEL/a.stratafied
 
 # configuration variables
 P1_DIR=p1.xform
 CONCOLIC_DIR=concolic.files_a.stratafied_0001
-EXECUTED_ADDRESSES=$CONCOLIC_DIR/executed_address_list.txt
+#EXECUTED_ADDRESSES=$CONCOLIC_DIR/executed_address_list.txt
+EXECUTED_ADDRESSES_CONCOLIC=concolic_coverage.txt;
 EXECUTED_ADDRESSES_MANUAL=manual_coverage.txt
 EXECUTED_ADDRESSES_FINAL=final.coverage.txt
 LIBC_FILTER=$PEASOUP_HOME/tools/libc_functions.txt
-BLACK_LIST=$P1_DIR/p1.filtered_out                                            # list of functions to blacklist
+BLACK_LIST=$P1_DIR/p1.filtered_out        # list of functions to blacklist
 COVERAGE_FILE=$P1_DIR/p1.coverage
 P1THRESHOLD=0.50
+
 
 PN_BINARY=$SECURITY_TRANSFORMS_HOME/tools/transforms/p1transform.exe
 
@@ -60,68 +69,78 @@ $PEASOUP_HOME/tools/do_manual_cover.sh
 touch $EXECUTED_ADDRESSES_FINAL
 
 cat $EXECUTED_ADDRESSES_MANUAL >> $EXECUTED_ADDRESSES_FINAL
-cat $EXECUTED_ADDRESSES >> $EXECUTED_ADDRESSES_FINAL
+
+echo "Replaying all .json files"
+input_cnt=0
+#run all .json inputs through the replayer
+for i in `ls $CONCOLIC_DIR/*.json`
+do
+    #check to see if the sym file exists, if not create it.
+    if [ ! -e $TOP_LEVEL/a.sym ]; then
+	$GRACE_HOME/concolic/src/util/linux/objdump_to_grace $STRATAFIED_BINARY
+    fi
+
+    input=`basename $i .json`
+    #input_number=`echo $input | sed "s/.*input_//"`
+   # abridged_number=`echo $input_number | sed 's/0*\(.*\)/\1/'`
+
+    #generate baseline from the stratafied program, if only to prevent discrepencies when a program prints its own name. 
+    #also I want to be as consistent as possible to avoid replayer issues. 
+    timeout $REPLAYER_TIMEOUT "$GRACE_HOME/concolic/bin/replayer" --timeout=$REPLAYER_TIMEOUT --symbols=$TOP_LEVEL/a.sym --stdout=stdout.$input --stderr=stderr.$input --logfile=exit_status --engine=sdt --instruction_addresses=tmp_coverage $STRATAFIED_BINARY $i || continue
+
+	#the exit status file has long been a flag that replay worked, make sure it exists before continuing
+    if [ ! -f exit_status ]; then
+	continue;
+    fi
+
+	#don't consider inputs that cause 139 and 134 return values
+        #if baseline exited with 139, ignore input
+    grep 139 exit_status
+    if [ $? -eq 0 ]; then
+	continue
+    fi
+
+	#I am not sure why 134 is filtered, but this was present in the ps_validate.sh script
+	#before the most recent modification
+    grep 134 exit_status
+    if [ $? -eq 0 ]; then
+	continue
+    fi
+    
+    mkdir $BASELINE_DIR/$input
+
+    mv stdout.$input $BASELINE_DIR/$input/.
+    mv stderr.$input $BASELINE_DIR/$input/.
+    cat exit_status | grep "Subject exited with status" >tmp
+    mv tmp $BASELINE_DIR/$input/exit_status
+    mv grace_replay/ $BASELINE_DIR/$input/.
+
+	#add to coverage total
+    cat tmp_coverage >> $EXECUTED_ADDRESSES_CONCOLIC 	
+
+    input_cnt=`expr $input_cnt + 1`
+done
+
+echo "Finished replaying .json files: Replayed $input_cnt inputs"
+
+touch $EXECUTED_ADDRESSES_CONCOLIC
+
+cat $EXECUTED_ADDRESSES_CONCOLIC >> $EXECUTED_ADDRESSES_FINAL
 
 # sanity filter, keep only well formed addresses
 cat $EXECUTED_ADDRESSES_FINAL | sed 's/.*\(0x.*\)/\1/' >tmp
 mv tmp $EXECUTED_ADDRESSES_FINAL
 
+sort $EXECUTED_ADDRESSES_FINAL | uniq > tmp
+mv tmp $EXECUTED_ADDRESSES_CONCOLIC
+
 # produce coverage file
 $PEASOUP_HOME/tools/cover.sh $ORIGINAL_BINARY $MEDS_ANNOTATION_FILE $EXECUTED_ADDRESSES_FINAL $LIBC_FILTER $COVERAGE_FILE $BLACK_LIST
-
-baseline_flag=0
-#delete the coverage file if all baseline exit status results are 139
-#for i in `ls $CONCOLIC_DIR/generated_input*.json`
-for i in `ls $CONCOLIC_DIR/*.json`
-do
-  input=`basename $i .json`
-  input_number=`echo $input | sed "s/.*input_//"`
-
-  abridged_number=`echo $input_number | sed 's/0*\(.*\)/\1/'`
-
-  #if there is no exit code for the input number, skip for now.
-  if [ ! -f "$CONCOLIC_DIR/exit_code.run_$abridged_number.log" ]; then
-      echo "do_p1transform.sh: No baseline data for input $input_number, missing exit status"
-      continue;
-  fi
-  echo "Exit status baseline file: $CONCOLIC_DIR/exit_code.run_$abridged_number.log"
-  #if baseline exited with 139, ignore input
-  grep 139 $CONCOLIC_DIR/exit_code.run_$abridged_number.log
-  if [ $? -eq 0 ]; then
-      echo "Baseline exit status was 139 for input $i, ignoring input"
-      continue
-  fi
-
-  echo "do_p1transform.sh: Found at least one grace produced valid baseline comparison output"
-  #at this point we know we have baseline data to compare against
-  #we assume that if exit status was produced, baseline information is available
-  #if I find one, break the loop
-  baseline_flag=`expr $baseline_flag + 1`
-  break
-done
-
-#if no baseline comparison output exists, delete the coverage file (when using Grace)
-if [ ! -f $EXECUTED_ADDRESSES_MANUAL ]; then
-  if [ $baseline_flag -eq 0 ]; then
-    echo "do_p1transform.sh: no valid baseline comparison output, deleting coverage file"
-    rm $COVERAGE_FILE
-    touch $COVERAGE_FILE
-  fi
-fi
 
 #just in case something went wrong, touch the coverage file. An empty coverage file is permissible, but a missing one will cause PN to crash
 
 touch $COVERAGE_FILE
 
-if [ $? -eq 0 ]; then
-	if [ -f $COVERAGE_FILE ]; then
-	    execute_pn $CLONE_ID $BED_SCRIPT $BLACK_LIST $COVERAGE_FILE $P1THRESHOLD $TIMEOUT_VALUE
-	else
-		echo "No coverage file -- do not attempt P1 transform" > p1transform.out
-		exit 1
-	fi
-else
-    execute_pn $CLONE_ID $BED_SCRIPT $LIBC_FILTER $COVERAGE_FILE $P1THRESHOLD $TIMEOUT_VALUE
-fi
+execute_pn $CLONE_ID $BED_SCRIPT $LIBC_FILTER $COVERAGE_FILE $P1THRESHOLD $TIMEOUT_VALUE
 
 exit 0
--- tools/ps_validate.sh
+++ tools/ps_validate.sh
@@ -16,13 +16,15 @@ STRATAFIED_BINARY=$1        # stratafied subject program (a.stratafied)
 BSPRI=$2                    # transformation specification SPRI file (some bspri file)
 INPUT_DIR=$3                # directory containing inputs (.../concolic.files_a.stratafied_0001)
 
-BASELINE_OUTPUT_DIR=$INPUT_DIR/sandboxed-files
-
-REPLAYER_TIMEOUT=120        # timeout value for when replaying input -- for now 120 seconds per input
+ # timeout value for when replaying input -- for now 120 seconds per input,
+# make sure identical with baseline replayer value (see do_p1transform.sh)
+REPLAYER_TIMEOUT=120       
 
 ORIG_PROG=a.ncexe
 baseline_cnt=0
 TOP_LEVEL=`pwd`
+BASELINE_DIR=$TOP_LEVEL/replayer_baseline
+
 
 EMPTY_JSON=$PEASOUP_HOME/tools/empty.json
 
@@ -36,158 +38,50 @@ echo "                 BSPRI: $BSPRI"
 echo "             INPUT_DIR: $INPUT_DIR"
 echo "   BASELINE_OUTPUT_DIR: $BASELINE_OUTPUT_DIR"
 echo "=========================================="
+echo ""
 
 #
 # name of files describing inputs is of the form: input_0001.json, input_0002.json, ...
 #
 
 echo "ps_validate.sh: BED: warning: @todo: need to handle files other than stdout, stderr"
-
-for i in `ls $INPUT_DIR/*.json`
+input_cnt=0
+for i in `ls $BASELINE_DIR/`
 do
-
- #Ben Modification: run replayer from top level to keep the sandbox layout the same as produced by the concolic test engine. This makes output comparison easier
-  echo "Testing input spec: $i"
-  input=`basename $i .json`
-  input_number=`echo $input | sed "s/.*input_//"`
-
-
-  abridged_number=`echo $input_number | sed 's/0*\(.*\)/\1/'`
-  #if there is no exit code for the input number, skip for now.
-  echo  "$INPUT_DIR/exit_code.run_$abridged_number.log"
-  if [ ! -e "$INPUT_DIR/exit_code.run_$abridged_number.log" ]; then
-      echo "ps_validate.sh: No baseline data for input $input_number, missing exit status"
-      continue;
-  fi
-  echo "Exit status baseline file: $INPUT_DIR/exit_code.run_$abridged_number.log"
-  #if baseline exited with 139, ignore input
-  grep 139 $INPUT_DIR/exit_code.run_$abridged_number.log
-  if [ $? -eq 0 ]; then
-      echo "Baseline exit status was 139 for input $i, ignoring input"
-      continue
-  fi
-  #if baseline exited with 134, ignore input
-  grep 134 $INPUT_DIR/exit_code.run_$abridged_number.log
-  if [ $? -eq 0 ]; then
-      echo "Baseline exit status was 134 for input $i, ignoring input"
-      continue
-  fi
-
-  #at this point we know we have baseline data to compare against
-  #we assume that if exit status was produced, baseline information is available
-  baseline_cnt=`expr $baseline_cnt + 1`
-
-  mkdir replay/$input_number 2>/dev/null
-# make sure the output files exist
-  touch replay/$input_number/stdout.$input
-  touch replay/$input_number/stderr.$input
-
-  #cleanup any previous runs
-  rm -rf grace_replay
-  rm -f stdout.* stderr.* exit_status
-
-  echo "ps_validate.sh: cmd: STRATA_SPRI_FILE=$BSPRI timeout $REPLAYER_TIMEOUT $GRACE_HOME/concolic/bin/replayer --timeout=$REPLAYER_TIMEOUT --symbols=$TOP_LEVEL/a.sym --stdout=stdout.$input --stderr=stderr.$input --logfile=exit_status --engine=sdt $STRATAFIED_BINARY $i"
-  STRATA_SPRI_FILE="$BSPRI" timeout $REPLAYER_TIMEOUT "$GRACE_HOME/concolic/bin/replayer" --timeout=$REPLAYER_TIMEOUT --symbols=$TOP_LEVEL/a.sym --stdout=stdout.$input --stderr=stderr.$input --logfile=exit_status --engine=sdt $STRATAFIED_BINARY $i || exit 2
-
-  mv stderr.$input replay/$input_number/stderr.$input
-  mv stdout.$input replay/$input_number/stdout.$input
-  mv exit_status replay/$input_number/exit_status
-
-#first verify the exit status 
-  cat replay/$input_number/exit_status | grep "Subject exited with status">exit_status1
-  cat $INPUT_DIR/exit_code.run_$abridged_number.log | grep "Subject exited with status">exit_status2
-  #diff replay/$input_number/exit_status $INPUT_DIR/exit_code.run_$abridged_number.log
-  diff exit_status1 exit_status2
-  diff_result=$?
-  rm -f exit_status1 exit_status2
-
-  if [ ! $diff_result -eq 0 ]; then
-      echo "ps_validate.sh: divergence detected for input: $i (exit status)"
-      #echo "expected: "
-      #cat $INPUT_DIR/exit_code.run_$abridged_number.log
-      #echo "observed: "
-      #cat replay/$input_number/exit_status
-      exit 1
-  fi
-
-  #next check program produced output files, if any exist
-
-  if [ -d "grace_replay" ];then
-      echo "ps_validate.sh: Discovered output files, validating contents"
-      mv  grace_replay/ replay/$input_number/
-      cp $BASELINE_OUTPUT_DIR/run_$input_number/* replay/$input_number/grace_replay/replay_0001/
-      diff -r -B -I stratafied $BASELINE_OUTPUT_DIR/run_$input_number/ replay/$input_number/grace_replay/replay_0001 >diff_tmp
-      if [ ! $? -eq 0 ]; then
-	  echo "ps_validate.sh: divergence detected for input: $i (output files)"
-	  echo "Diff output:"
-	  cat diff_tmp
-	  rm diff_tmp
-	  exit 1
-      fi
-      rm diff_tmp
-  fi
-
-  cd replay/$input_number
-
-  BASELINE_OUTPUT_STDOUT=$BASELINE_OUTPUT_DIR/run_$input_number/stdout
-  BASELINE_OUTPUT_STDERR=$BASELINE_OUTPUT_DIR/run_$input_number/stderr
-
-  echo "$BASELINE_OUTPUT_STDOUT"
-  echo "$BASELINE_OUTPUT_STDERR"
-
-  if [ -f $BASELINE_OUTPUT_STDOUT ];
-  then
+    input_file=$INPUT_DIR/$i.json
+    rm -fr stdout.$i stderr.$i exit_status grace_replay/
+    echo "STRATA_SPRI_FILE="$BSPRI" timeout $REPLAYER_TIMEOUT "$GRACE_HOME/concolic/bin/replayer" --timeout=$REPLAYER_TIMEOUT --symbols=$TOP_LEVEL/a.sym --stdout=stdout.$i --stderr=stderr.$i --logfile=exit_status --engine=sdt $STRATAFIED_BINARY $input_file || exit 2"
     echo ""
-	pwd
-	echo "diffing stdout.$input $BASELINE_OUTPUT_STDOUT"
-    diff -B -I stratafied stdout.$input $BASELINE_OUTPUT_STDOUT
+    STRATA_SPRI_FILE="$BSPRI" timeout $REPLAYER_TIMEOUT "$GRACE_HOME/concolic/bin/replayer" --timeout=$REPLAYER_TIMEOUT --symbols=$TOP_LEVEL/a.sym --stdout=stdout.$i --stderr=stderr.$i --logfile=exit_status --engine=sdt $STRATAFIED_BINARY $input_file || exit 2
+
+    rm -rf input_test
+    mkdir input_test
+
+    mv stdout.$i input_test/.
+    mv stderr.$i input_test/.
+    cat exit_status | grep "Subject exited with status" >tmp
+    mv tmp input_test/exit_status
+    mv grace_replay/ input_test/.
+
+    diff -r input_test/ $BASELINE_DIR/$i &>diff_out
     if [ ! $? -eq 0 ]; then
-      echo "ps_validate.sh: divergence detected for input: $i (stdout)"
-
-      echo "--- Baseline file (stdout) ---"
-      cat $BASELINE_OUTPUT_STDOUT
-
-      echo "--- Output stdout (stdout) for input: $input ---"
-      cat stdout.$input
-
-      exit 1
+	echo "ps_validate.sh: divergence detected for input: $i"
+	cat diff_out
+	exit 1
     fi
-  fi
-
-  if [ -f $BASELINE_OUTPUT_STDERR ];
-  then
-    diff -B -I stratafied stderr.$input $BASELINE_OUTPUT_STDERR
-    if [ ! $? -eq 0 ]; then
-      echo "ps_validate.sh: divergence detected for input: $i (stderr)"
-
-      echo "--- Baseline file (stderr) ---"
-      cat $BASELINE_OUTPUT_STDERR
-
-      echo "--- Output stderr (stderr) for input: $input ---"
-      cat stderr.$input
-
-      exit 1
-    fi
-  fi
-
-  echo "ps_validate.sh: input $i validates"
-
-  # remove tmp files
-#  rm stdout.$input 2>/dev/null
-#  rm stderr.$input 2>/dev/null
-
-  cd ../..
-
+    
+    input_cnt=`expr $input_cnt + 1`
 done
 
+rm -fr stdout.$i stderr.$i exit_status grace_replay/ input_test/
+
+
 #if no baseline run was found, run the original program with no input
-#If the original program doesn't segfault (139), compare the original run
-#exit status against the transformed program exist status
-if [ $baseline_cnt -eq 0 ];then
+#Under the new PN configuration, this should never be called, but is here
+#as a backup
+if [ $input_cnt -eq 0 ];then
 
     echo "ps_validate.sh: No valid baseline to compare against, performing simple no args sanity check instead"
-
-#TODO: need to remove EMPTY_JSON, used only until duc provides empty functionality to grace.
 
 #check to see if the sym file exists, if not create it.
     if [ ! -e $TOP_LEVEL/a.sym ]; then
@@ -207,16 +101,6 @@ if [ $baseline_cnt -eq 0 ];then
     if [ $orig_status -ne 139 ];then
 	echo "STRATA_SPRI_FILE="$BSPRI" timeout $REPLAYER_TIMEOUT "$GRACE_HOME/concolic/bin/replayer" --timeout=$REPLAYER_TIMEOUT --symbols=$TOP_LEVEL/a.sym --stdout=stdout.$input --stderr=stderr.$input --logfile=exit_status --engine=sdt $STRATAFIED_BINARY $EMPTY_JSON || exit 2"
 	STRATA_SPRI_FILE="$BSPRI" timeout $REPLAYER_TIMEOUT "$GRACE_HOME/concolic/bin/replayer" --timeout=$REPLAYER_TIMEOUT --symbols=$TOP_LEVEL/a.sym --stdout=stdout.$input --stderr=stderr.$input --logfile=exit_status --engine=sdt $STRATAFIED_BINARY $EMPTY_JSON || exit 2
-#	echo "STRATA_SPRI_FILE=$BSPRI timeout $REPLAYER_TIMEOUT  $TOP_LEVEL/a.stratafied" 
-#	STRATA_SPRI_FILE=$BSPRI timeout $REPLAYER_TIMEOUT  $TOP_LEVEL/a.stratafied 
-##	timeout $REPLAYER_TIMEOUT ./ps_run.sh $TOP_LEVEL
-#	trans_status=$?
-
-#	if [ $orig_status -ne $trans_status ]; then
-#	    echo "Status values do not equal, orig_status $orig_status, trans_status $trans_status"
-#	    exit 1
-	    #else fall through, exit 0, 
-#	fi
 
 	#just in case there is a replayer failure, create an exit status file
 	touch exit_status
