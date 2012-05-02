fbebe488ebc15a40cbe5db94b5cca68859c9ec7a
an7s <an7s@git.zephyr-software.com>
2012-05-02 14:15:11 +0000

    get rid of peasoup exec directory
    Former-commit-id: 09ccce22d96b31a9154c1e203d5d57578d3ae560

--- libtransform/tests/false_positives/peasoup_executable_directory.ls.shared.orig.803/ls.shared.orig.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/home/jdh8d/peasoup/uva/security_transforms.trunk/libtransform/tests/false_positives/peasoup_executable_directory.ls.shared.orig.803/ps_run.sh /home/jdh8d/peasoup/uva/security_transforms.trunk/libtransform/tests/false_positives/peasoup_executable_directory.ls.shared.orig.803 "$@"
