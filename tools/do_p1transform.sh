c844a6ba7d89838748ecabc1056be9f125538986
bdr7fv <bdr7fv@git.zephyr-software.com>
2012-05-31 17:08:19 +0000

    Set option in do_p1transform.sh to no validate on p1.
    Former-commit-id: 4db01385ec66bd0a781918a5873627b1a6a0753a

--- tools/do_p1transform.sh
+++ tools/do_p1transform.sh
@@ -154,5 +154,5 @@ touch $COVERAGE_FILE
 
 echo "$PN_BINARY --variant_id=$CLONE_ID --bed_script=$BED_SCRIPT --coverage_file=$COVERAGE_FILE --pn_threshold=$P1THRESHOLD --canaries=$DO_CANARIES --blacklist=$LIBC_FILTER"
 
-$PN_BINARY --variant_id=$CLONE_ID --bed_script=$BED_SCRIPT --coverage_file=$COVERAGE_FILE --pn_threshold=$P1THRESHOLD --canaries=$DO_CANARIES --blacklist=$LIBC_FILTER
+$PN_BINARY --variant_id=$CLONE_ID --bed_script=$BED_SCRIPT --coverage_file=$COVERAGE_FILE --pn_threshold=$P1THRESHOLD --canaries=$DO_CANARIES --blacklist=$LIBC_FILTER --no_p1_validate
 exit 0
