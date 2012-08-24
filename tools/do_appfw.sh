c0930628b9dc906dd22d67dfb10397a1506c68fd
an7s <an7s@git.zephyr-software.com>
2012-08-24 19:17:56 +0000

    Added script to invoke command injection firewall functionality
    Former-commit-id: 7be497df81893078cebfecacf8e7b7b5b2f496d0

---
+++ tools/do_appfw.sh
@@ -0,0 +1,10 @@
+#!/bin/sh 
+
+program=$1
+
+# generate string signatures off the binary
+$PEASOUP_HOME/tools/generate_string_signatures.sh $program $program.sigs
+
+# copy application firewall library 
+# for now, it's only SQL
+cp $SECURITY_TRANSFORMS_HOME/appfw/lib/libappfw.so .
