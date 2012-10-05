d1a86225f84a3d3180708ba10919a577952dceee
an7s <an7s@git.zephyr-software.com>
2012-10-05 13:12:54 +0000

    renamed top level directory
    Former-commit-id: ec479e0cac7eedced0c08acbae7091c2a8bb087e

---
+++ appfw/tests/mysql/testmysql.c
@@ -0,0 +1,81 @@
+/* This software was developed at the National Institute of Standards and
+ * Technology by employees of the Federal Government in the course of their
+ * official duties. Pursuant to title 17 Section 105 of the United States
+ * Code this software is not subject to copyright protection and is in the
+ * public domain. NIST assumes no responsibility whatsoever for its use by
+ * other parties, and makes no guarantees, expressed or implied, about its
+ * quality, reliability, or any other characteristic.
+
+ * We would appreciate acknowledgement if the software is used.
+ * The SAMATE project website is: http://samate.nist.gov
+
+@GOOD_ARGS David
+@NORMAL_OUTPUT_CONTAINS David
+@TEST_SCRIPT ./runtest
+@BAD_ARGS "David' or '0'='0"
+@ATTACK_SUCCEEDED_OUTPUT_CONTAINS William
+
+*/
+
+#include <mysql.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[]) 
+{
+   MYSQL *conn;
+   MYSQL_RES *res;
+   MYSQL_ROW row;
+
+//   printf("MySQL client version: %s\n", mysql_get_client_info());
+
+   char *server = "localhost";
+   char *user = "root";
+//   char *password = "ps2";
+   char *password = NULL;
+   char *database = "testdata";
+   
+	if (argc < 2)
+	{
+		printf("You should give an entry parameter!\n");
+		return 0;		
+	}
+	
+   conn = mysql_init(NULL);
+   
+   /* Connect to database */
+   if (!mysql_real_connect(conn, server,
+         user, password, database, 0, NULL, 0)) {
+      fprintf(stderr, "%s\n", mysql_error(conn));
+      return 0;
+   }
+
+   /* send SQL query */
+   char query[512];
+   char *fmtString = "SELECT * FROM users_1796 WHERE first LIKE '%s'";
+   
+   /* No more buffer overflow */
+	if ((strlen(argv[1]) + strlen(fmtString)) > 512){
+		printf("The entry is too long...\n");
+		return 0;		
+	}
+	sprintf(query,fmtString,argv[1]);
+        printf("%s\n", query);
+	if (mysql_query(conn, query)) {
+	  fprintf(stderr, "%s\n", mysql_error(conn));
+	  return 0;
+	}
+	
+	res = mysql_use_result(conn);
+	
+	/* output fields 1 and 2 of each row */
+	while ((row = mysql_fetch_row(res)) != NULL)
+	  printf("%s %s\n", row[0], row[1]);
+	
+	/* Release memory used to store results and close connection */
+	mysql_free_result(res);
+	mysql_close(conn);
+	return 0;
+}
+
---
+++ appfw/tests/sqlite/sqlite.test.sh
@@ -0,0 +1,105 @@
+#!/bin/sh -x
+
+# Assumptions:
+# 	$1 is the full pathname to output file
+
+# For PEASOUP, Required XML fields are
+# name - name of the test
+# host - name of the host where the test was run
+# project - project name
+# date_time - date time in specific format date +%FT%R:%S
+# key_value pairs, any number
+#   may include result, user, host platform, build platform
+
+# Fixed attributes
+# ATTRIBUTE ModDep=strata
+# ATTRIBUTE ModDep=diablo_toolchain
+# ATTRIBUTE ModDep=binutils-2.19
+# ATTRIBUTE ModDep=stratafier
+# ATTRIBUTE ModDep=idapro61
+# ATTRIBUTE ModDep=idapro61_sdk
+# ATTRIBUTE TestsWhat=lang_C
+# ATTRIBUTE TestsWhat=strata
+# ATTRIBUTE TestsWhat=commandinjection
+# ATTRIBUTE OS=linux
+# ATTRIBUTE Compiler=gcc
+# ATTRIBUTE Arch=x86_32
+# ATTRIBUTE TestName=smartfuzz
+# ATTRIBUTE BenchmarkName=TandE
+# ATTRIBUTE CompilerFlags="-w"
+
+COMPFLAGS="-w"
+
+# export general IDA pro environment vars
+export IDAROOT=$IDAROOT61
+export IDASDK=$IDASDK61
+
+PWD=`pwd`
+TESTLOC="${PWD}"
+tmp=$$.tmp
+
+outfile=$1
+
+cleanup()
+{
+	exit_code=$1
+	shift
+	msg=$*
+
+	if [ $exit_code -eq 0 ]; then 
+		report_test_success $outfile "$msg"
+	else
+		report_test_failure $outfile "Intermediate step failed, exit code is $exit_code, msg='$msg'"
+	fi
+
+	cd $TESTLOC
+	rm -f $tmp 2>/dev/null
+	make clean
+	cd -
+
+ 	rm -f $tmp
+	exit $exit_code
+}
+
+# suck in utils
+. ${TEST_HARNESS_HOME}/test_utils.sh || cleanup 1 "Cannot source utils file"
+
+assert_test_args $*
+assert_test_env $outfile STRATAFIER STRATA TOOLCHAIN STRATAFIER_OBJCOPY IDAROOT IDASDK PEASOUP_HOME SECURITY_TRANSFORMS_HOME
+
+# path to source
+cd $TESTLOC
+make clean peasoup
+if [ ! $? -eq 0 ]; then
+	cleanup 1 "Failed to build postgres tests"
+fi
+
+
+#
+# testpg2.exe.peasoup
+#
+dbname=testdata
+
+sqlite3 $dbname < ./teardown.sql 2>/dev/null   # in case we have leftover from previous run
+sqlite3 $dbname < ./setup.sql
+
+# good query
+rm -f $tmp 2>/dev/null
+./testsqlite.exe.peasoup David > $tmp 2>&1
+grep -i "last = Hyde" $tmp
+if [ ! $? -eq 0 ]; then
+	cat $tmp
+	cleanup 2 "False positive detected: query for testsqlite.exe.peasoup should have succeeded"
+fi
+
+rm -f $tmp
+./testsqlite.exe "David' or '0'='0" > $tmp 2>&1
+grep -i William $tmp
+if [ ! $? -eq 0 ]; then
+	cat $tmp
+	cleanup 3 "False negative detected: attack query for testsqlite.exe.peasoup should have failed"
+fi
+
+sqlite3 $dbname < ./teardown.sql
+
+cleanup 0 "Successfully detected sqlite SQL Injection"
