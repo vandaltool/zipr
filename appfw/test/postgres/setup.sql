ef2fdd2dcc16e1eb661aa7ebf907dc3c1b075970
an7s <an7s@git.zephyr-software.com>
2012-10-11 14:58:21 +0000

    Missing sqlite3.h
    Former-commit-id: 8e61031c2be0950da31bb7655418aa52f1c903ce

--- appfw/test/postgres/setup.sql
+++
@@ -1,8 +0,0 @@
-create table users_1796 (
-  first varchar(40),
-  last varchar(40),
-  balance float);
-
-insert into users_1796 (first, last, balance) values ('David', 'Hyde', 1000.0);
-insert into users_1796 (first, last, balance) values ('William', 'Ella', 1000.0);
-insert into users_1796 (first, last, balance) values ('Brian', 'Mastropietro', 1000.0);
