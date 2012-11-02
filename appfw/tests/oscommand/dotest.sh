9080d48f82c81abc614da6c6590d00afe61a323c
jdh8d <jdh8d@git.zephyr-software.com>
2012-11-02 15:52:57 +0000

    script to run tests.  need to formalize into test harness test case.
    Former-commit-id: db8305fd76fece881f1beae9c2cd6da8fab44efa

---
+++ appfw/tests/oscommand/dotest.sh
@@ -0,0 +1,36 @@
+#!/bin/bash 
+
+APPFW_LIB=$SECURITY_TRANSFORMS_HOME/appfw/lib/libappfw.so
+
+for i in {1..11}
+do
+	
+	clear
+	echo ----------------- unmodified: input \"$i Makefile\" and  \"$i -l\"
+	./testosc.exe $i Makefile
+	./testosc.exe $i -l
+	echo ----------------- peasoup
+	LD_PRELOAD=$APPFW_LIB APPFW_DB=appfw.db APPFW_SIGNATURE_FILE=testosc.exe.sigs ./testosc.exe $i Makefile
+	LD_PRELOAD=$APPFW_LIB APPFW_DB=appfw.db APPFW_SIGNATURE_FILE=testosc.exe.sigs ./testosc.exe $i -l
+	echo Press [enter] to continue ...
+	read name
+
+	
+done
+
+
+for i in {101..111}
+do
+	
+	clear
+	echo ----------------- unmodified: input \"$i Makefile\" and  \"$i -l\"
+	./testosc.exe $i env | head -5
+	./testosc.exe $i -i env 
+	echo ----------------- peasoup
+	LD_PRELOAD=$APPFW_LIB APPFW_DB=appfw.db APPFW_SIGNATURE_FILE=testosc.exe.sigs ./testosc.exe $i env |head -5
+	LD_PRELOAD=$APPFW_LIB APPFW_DB=appfw.db APPFW_SIGNATURE_FILE=testosc.exe.sigs ./testosc.exe $i -i env
+	echo Press [enter] to continue ...
+	read name
+
+	
+done
