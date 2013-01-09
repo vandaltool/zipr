d94dfe0256f803c8f196cc122d21d7e479a89c0d
an7s <an7s@git.zephyr-software.com>
2013-01-09 21:19:19 +0000

    added mysql_real_query() to interposition layer
    Former-commit-id: 88c12aee906c7344c53697a17e12ed653474df29

--- appfw/src/mysql_hook.c
+++ appfw/src/mysql_hook.c
@@ -8,6 +8,10 @@
 
 #include "sqlfw.h"
 
+// @todo: fix potential buffer overflow in error message
+// @todo: use length to figure out taint buffer size 
+
+// intercept mysql_query
 int mysql_query(MYSQL* p_conn, const char *p_query)
 {
   char tainted[MAX_QUERY_LENGTH];
@@ -29,6 +33,32 @@ int mysql_query(MYSQL* p_conn, const char *p_query)
 	char errmsg[2048];
 	sprintf(errmsg, "error: security violation: %s", p_query);
     int ret = intercept_sqlQuery(p_conn, errmsg);
+	return ret;
+  }
+}
+
+// intercept mysql_real_query
+int mysql_real_query(MYSQL* p_conn, const char *p_query, unsigned long p_length)
+{
+  char tainted[MAX_QUERY_LENGTH];
+  static int (*intercept_sqlRealQuery)(MYSQL*, const char *, unsigned long) = NULL;
+  if (!intercept_sqlRealQuery)
+    intercept_sqlRealQuery = dlsym(RTLD_NEXT, "mysql_real_query");
+
+  sqlfw_init(); // will do this automagically later 
+
+  char *errMsg = NULL;
+  if (sqlfw_verify_taint(p_query, tainted, &errMsg))
+  {
+    int ret = intercept_sqlRealQuery(p_conn, p_query, p_length);
+	return ret;
+  }
+  else
+  {
+	// error policy: issue bad query on purpose so that we return what PG would have returned
+	char errmsg[2048];
+	sprintf(errmsg, "error: security violation: %s", p_query);
+    int ret = intercept_sqlRealQuery(p_conn, errmsg, strlen(errmsg));
 	return ret;
   }
 }
