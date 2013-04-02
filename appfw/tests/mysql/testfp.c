cb2dbac7b34ff7eba39adf1bee35989cd38d89b0
an7s <an7s@git.zephyr-software.com>
2013-04-02 19:30:46 +0000

    More false positive tests
    Former-commit-id: 27202ecc8095caccad4c57ba0f66eedacb407c55

--- appfw/tests/mysql/testfp.c
+++ appfw/tests/mysql/testfp.c
@@ -29,6 +29,10 @@ int main(int argc, char *argv[])
 
    /* test for false positives */
    test1(conn);
+   test2(conn);
+   test3(conn);
+   test4(conn);
+   test5(conn);
 
    /* cleanup */
 	mysql_close(conn);
@@ -58,6 +62,86 @@ void test1(MYSQL *conn)
 	mysql_query(conn, query);
 
 	sprintf(query, "Select * FROM users_1796 LIMIT 10 ; SELECT * FROM users_1796 LIMIT 2 ;");
+	mysql_query(conn, query);
+}
+
+void test2(MYSQL *conn)
+{
+	char query[1024];
+
+	printf("Testing AS, GROUP BY, MIN, MAX, INSERT, CREATE\n");
+
+	sprintf(query,"SELECT foo, MIN(bar) AS bar FROM tbl GROUP BY foo");
+	mysql_query(conn, query);
+
+	sprintf(query,"SELECT foo, MIN(bar), count(*) AS bar FROM tbl GROUP BY foo;");
+	mysql_query(conn, query);
+
+	sprintf(query,"SELECT foo, MAX(bar) AS Count FROM tbl GROUP BY foo ORDER BY Count DESC;");
+	mysql_query(conn, query);
+
+	sprintf(query,"SELECT a.foo, a.bar FROM tbl a JOIN ( SELECT foo, MAX(bar) AS Count FROM tbl GROUP BY foo ) b ON a.foo=b.foo AND a.bar=b.count ORDER BY a.foo, a.bar; "); 
+
+	sprintf(query," CREATE TABLE supparts(supID char(2),partID char(2)); INSERT INTO supparts VALUES ('s1','p1'),('s1','p2'),('s1','p3'),('s1','p4'),('s1','p5'),('s1','p6'), ('s2','p1'),('s2','p2'),('s3','p2'),('s4','p2'),('s4','p4'),('s4','p5'); ");
+	mysql_query(conn, query);
+}
+
+void test3(MYSQL *conn)
+{
+	char query[1024];
+
+	printf("Testing complex queries\n");
+
+	sprintf(query, "select s.name, sum(l.qty) as n from salespersons s join orders o using(salespersonid) join orderlines l using(orderid) join products     p using(productid) where p.name='computer desk' group by s.name order by n desc limit 1;"); 
+	mysql_query(conn, query);
+}
+
+void test4(MYSQL *conn)
+{
+	char query[1024];
+
+	printf("Testing multi-line statements\n");
+
+	sprintf(query, "select s.name, sum(l.qty) as n \
+	from salespersons s join orders o using(salespersonid) join \
+	orderlines l using(orderid) join products     p using(productid) where p.name='computer desk' group by s.name order by n desc limit 1;"); 
+	mysql_query(conn, query);
+	return;
+
+	// bug -- we're not handling \n or \ right
+	sprintf(query, "\n \
+select s.name, sum(l.qty) as n      -- sum quantities \n \
+from salespersons s \n \
+join orders       o using(salespersonid) \n \
+join orderlines   l using(orderid) \n \
+join products     p using(productid) \n \
+where p.name='computer desk'     \n \
+group by s.name                     -- aggregate by salesperson \n \
+order by n desc limit 1;            -- order by descending sum, pick off top value ");
+	mysql_query(conn, query);
+
+	sprintf(query, "\
+	SELECT s.partID, s, thiscol, s.thatcol, anothercol, x.Suppliers \
+	FROM supparts s \
+	JOIN ( \
+	  SELECT partID,GROUP_CONCAT(supID ORDER BY supID) AS Suppliers  \
+	    FROM supparts  \
+		  GROUP BY partID \
+		  ) x USING(partID)"); 
+	mysql_query(conn, query);
+}
+
+
+void test5(MYSQL *conn)
+{
+	char query[1024];
+
+	printf("Testing INSERT\n");
+
+	sprintf(query,"INSERT INTO bookings ( court_id , member1 , member2 , time , fee ) VALUES  (1, 1000, 1001, '2009-09-09 15:49:38', 3.00),  (2, 1000, 1000, '2009-09-08 15:50:04', 3.00);"); 
+	mysql_query(conn, query);
+
+	sprintf(query,"SELECT member, ROUND(SUM(fee/2),2) AS total FROM (SELECT member1 AS member, fee FROM bookings UNION ALL SELECT member2, fee FROM bookings ) AS tmp GROUP BY member; ");
 	mysql_query(conn, query);
 }
 
