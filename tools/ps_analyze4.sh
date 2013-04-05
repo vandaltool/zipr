0d9cbd8559150ca8cb4652ed6fa6d63cec656d4c
jdh8d <jdh8d@git.zephyr-software.com>
2013-04-05 16:58:08 +0000

    Added a version of ps_analyze which does 4 protections, and sets up a script to invoke one randomly.
    Former-commit-id: 890b52066dbcb61c87b0f61892366116f6204dea

---
+++ tools/ps_analyze4.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+in=$1
+shift
+out=$1
+shift
+rest=$*
+
+variants=2
+
+for i in $(seq 0 $(expr $variants - 1)); do
+	$PEASOUP_HOME/tools/ps_analyze.sh $in $out.$i $rest
+done
+
+echo "#!/bin/bash" > $out
+chmod +x $out
+echo "var=\$RANDOM" >> $out
+echo "let \"var %= $variants\"" >> $out
+echo "exec `realpath $out`.\$var \"\$@\"" >> $out
+
+
+
