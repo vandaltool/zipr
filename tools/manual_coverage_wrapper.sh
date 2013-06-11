f124c1c2e854b03fa3299763449598c84e037b85
bdr7fv <bdr7fv@git.zephyr-software.com>
2013-06-11 21:14:39 +0000

    updaing manual_coverage_wrapper.sh for pin tool.
    Former-commit-id: 4791add3c7cf8a55b5a6dca7f6787da884a98de7

--- tools/manual_coverage_wrapper.sh
+++ tools/manual_coverage_wrapper.sh
@@ -11,18 +11,18 @@ MANUAL_EXE_ADDRESS_OUTPUT_FILE=$3
 
 
 PIN_BENCH=`pwd`/pin_bench
-EXE_ADDRESSES=$PIN_RESULTS/itrace.out
+COVERAGE_RESULTS_FILE=$PEASOUP_HOME/coverage_results/itrace.out
 COVER_SCRIPT=$SECURITY_TRANSFORMS_HOME/tools/cover/cover
 
 echo "manual coverage script"
 
 #clean exe addresses
-rm -f $EXE_ADDRESSES
+rm -f $COVERAGE_RESULTS_FILE
 
 #Assuming exe addresses are accumulated by pin in itrace.out located in
 #PIN_RESULTS
 echo "INSTALL PIN BINARY"
-echo "setarch i386 -RL $PIN_HOME/pin -injection child -t $PIN_HOME/itraceunique.so -- $BENCH \$@" > $PIN_BENCH
+echo "COVERAGE_RESULTS_FILE=$COVERAGE_RESULTS_FILE setarch i386 -RL $PIN_HOME/pin -injection child -t $PIN_HOME/source/tools/ManualExamples/obj-ia32/itraceunique.so -- $BENCH \$@" > $PIN_BENCH
 
 chmod +x $PIN_BENCH
 
@@ -37,10 +37,10 @@ if [ $? -ne 0 ]; then
 fi
 
 # sanity filter, keep only well formed addresses
-cat $EXE_ADDRESSES | sed 's/.*\(0x.*\)/\1/' >tmp
-mv tmp $EXE_ADDRESSES
+cat $COVERAGE_RESULTS_FILE | sed 's/.*\(0x.*\)/\1/' >tmp
+mv tmp $COVERAGE_RESULTS_FILE
 
-cp $EXE_ADDRESSES $MANUAL_EXE_ADDRESS_OUTPUT_FILE
+cp $COVERAGE_RESULTS_FILE $MANUAL_EXE_ADDRESS_OUTPUT_FILE
 
 exit 0
 
