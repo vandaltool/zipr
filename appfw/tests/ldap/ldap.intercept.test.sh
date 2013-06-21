cb17f4f2459947225f35b6cae47aa35d6ffa21be
an7s <an7s@git.zephyr-software.com>
2013-06-21 15:45:57 +0000

    Added ldap interception layer test
    Former-commit-id: 29b53f12f6de81091d81aedb002dc6e0775ee573

---
+++ appfw/tests/ldap/ldap.intercept.test.sh
@@ -0,0 +1,80 @@
+#!/bin/sh -x
+
+# Assumptions:
+# 	$1 is the full pathname to output file
+
+# For PEASOUP, Required XML fields are
+# name - name of the test
+# host - name of the host where the test was run
+# project - project name
+# date_time - date time in specific format date +%FT%R:%S
+# key_value pairs, any number
+#   may include result, user, host platform, build platform
+
+# Fixed attributes
+# ATTRIBUTE ModDep=strata
+# ATTRIBUTE ModDep=diablo_toolchain
+# ATTRIBUTE ModDep=stratafier
+# ATTRIBUTE ModDep=idapro61
+# ATTRIBUTE ModDep=idapro61_sdk
+# ATTRIBUTE TestsWhat=lang_C
+# ATTRIBUTE TestsWhat=strata
+# ATTRIBUTE TestsWhat=commandinjection
+# ATTRIBUTE OS=linux
+# ATTRIBUTE Compiler=gcc
+# ATTRIBUTE Arch=x86_32
+# ATTRIBUTE TestName=ldap_interception
+# ATTRIBUTE BenchmarkName=TandE
+# ATTRIBUTE CompilerFlags="-w"
+
+COMPFLAGS="-w"
+
+PWD=`pwd`
+TESTLOC="${PWD}"
+tmp=$$.tmp
+
+outfile=$1
+
+cleanup()
+{
+	exit_code=$1
+	shift
+	msg=$*
+
+	if [ $exit_code -eq 0 ]; then 
+		report_test_success $outfile "$msg"
+	else
+		report_test_failure $outfile "Intermediate step failed, exit code is $exit_code, msg='$msg'"
+	fi
+
+	cd $TESTLOC
+	rm -f $tmp 2>/dev/null
+	make clean
+	cd -
+
+ 	rm -f $tmp
+	exit $exit_code
+}
+
+# suck in utils
+. ${TEST_HARNESS_HOME}/test_utils.sh || cleanup 1 "Cannot source utils file"
+
+assert_test_args $*
+assert_test_env $outfile STRATAFIER STRATA TOOLCHAIN IDAROOT IDASDK PEASOUP_HOME SECURITY_TRANSFORMS_HOME
+
+# path to source
+cd $TESTLOC
+make clean all
+if [ ! $? -eq 0 ]; then
+	cleanup 1 "Failed to build mysql intercept tests"
+fi
+
+make run 2>$tmp >/dev/null
+
+num_detected=$(grep -i detected $tmp | wc -l)
+echo "Detected $num_detected violations"
+if [ ! $num_detected -eq 8 ]; then
+	cleanup 1 "Failed to intercept and/or detect security violation"
+fi
+
+cleanup 0 "Successfully tested ldap interception layer"
---
+++ appfw/tests/ldap/testldapintercept2.c
@@ -0,0 +1,20 @@
+// Test LDAP interception layer
+
+#include <stdio.h>
+#include <ldap.h>
+
+int main(int argc, char **argv)
+{
+	LDAP *ld = NULL;
+	char *filter_data = getenv("LDAP_DATA");
+	char base[1024];
+	int scope = LDAP_SCOPE_BASE;
+	char filter[1024];
+	int attrsonly = 0;
+
+	sprintf(base,"virginia.edu"); // this doesn't really matter
+	sprintf(filter,"(cn=%s)", filter_data);
+
+	if (ldap_search_ext(ld,base,scope,filter,NULL,attrsonly,NULL,NULL,NULL,25,NULL) == -1)
+		fprintf(stderr,"(2) ldap_search returned an error\n");
+}
