8b96f5495eaf83fedd516dc45c29f1e747ca02bf
abiusx <abiusx@git.zephyr-software.com>
2014-01-30 17:42:10 +0000

    read query from a file, its almost impossible  to have valid query syntax in command-line
    Former-commit-id: 80ad46154c49beae036a2a5ed8f1e3f36661b4e1

--- appfw/tests/standalone/sqlattackdetect.c
+++ appfw/tests/standalone/sqlattackdetect.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "appfw.h"
 #include "sqlfw.h"
-
+#include <stdlib.h>
 const int EXIT_CODE_ATTACK_DETECTED = 0;
 const int EXIT_CODE_NO_ATTACK = 1;
 const int EXIT_CODE_USAGE = 2;
@@ -13,22 +13,30 @@ int main(int argc, char **argv)
 
 	if (argc < 3)
 	{
-		fprintf(stderr, "usage: %s <signatureFile> <queryString>\n", argv[0]);
+		fprintf(stderr, "usage: %s <signatureFile> <queryFile>\n", argv[0]);
 		return EXIT_CODE_USAGE;
 	}
 
 	// initialize signatures from file
 	appfw_init_from_file(argv[1]);
 	sqlfw_init();
-
-	if (sqlfw_verify(argv[2], &errorMessage))
+	char *file_contents;
+	long input_file_size;
+	FILE *input_file = fopen(argv[2], "rb");
+	fseek(input_file, 0, SEEK_END);
+	input_file_size = ftell(input_file);
+	rewind(input_file);
+	file_contents = malloc(input_file_size * (sizeof(char)));
+	fread(file_contents, sizeof(char), input_file_size, input_file);
+	fclose(input_file);
+	if (sqlfw_verify(file_contents, &errorMessage))
 	{
 		fprintf(stderr, "no attack detected\n");
 		return EXIT_CODE_NO_ATTACK;
 	}
 	else
 	{
-		fprintf(stderr, "attack detected: %s\n", argv[2]);
+		fprintf(stderr, "attack detected: %s\n", file_contents);
 		return EXIT_CODE_ATTACK_DETECTED;
 	}
 }
