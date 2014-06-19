7d97273a2572430801aa093d1a0f7414d7853581
an7s <an7s@git.zephyr-software.com>
2014-06-19 15:26:50 +0000

    Add truncation test programs
    Former-commit-id: a2124173bfb86de60f59c90cef1a2fe31cc13117

---
+++ libtransform/tests/trunc.32.16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  short s;
+  int i;
+
+  i = atoi(argv[1]);
+  s = i;
+  if (s > 256)
+    printf("too big\n");
+  else
+    printf("just right: s = %d\n", s);
+}
