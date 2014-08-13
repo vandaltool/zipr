8d1a7b6b8fc03ab2776ef9b02da3496ed0d4004a
mc2zk <mc2zk@git.zephyr-software.com>
2014-08-13 19:53:22 +0000

    Added apt-get for packages related to getting GNOME desktop working.

--- cgc_setup.sh
+++ cgc_setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# This script automatically sets up the environment
+# This script automatically takes the user through the environment setup and the build of PEASOUP for CGC
+
+# update the apt-get databases
+sudo apt-get update
+
+# grab the packages related to setting up GNOME desktop environment
+sudo apt-get install aptitude tasksel xorg gnome-core dkms
 
 # add the following lines to .bashrc so that subsequent terminals opened will contain the right ENV VAR settings
 echo "# Adding lines to set up environment variables for CGC DEV" >> $HOME/.bashrc
@@ -20,3 +26,5 @@ echo "cd .." >> $HOME/.bashrc
 # set up the irdb
 ./postgres_setup.sh 2>&1 | tee pgsetup.output
 
+# reboot machine to start up the GNOME Desktop environment
+sudo reboot
