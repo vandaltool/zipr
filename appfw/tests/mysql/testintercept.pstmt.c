abd6f51a6fceb2d67c51945eba1eeb1f479f8e8b
an7s <an7s@git.zephyr-software.com>
2014-08-21 18:41:29 +0000

    updated tests
    Former-commit-id: 1589845fda8661c6fca93b015b485610cc6250a0

--- appfw/tests/mysql/testintercept.pstmt.c
+++ appfw/tests/mysql/testintercept.pstmt.c
@@ -1,6 +1,7 @@
 #include <mysql.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) 
 {
--- appfw/tests/mysql/testmysql.c
+++ appfw/tests/mysql/testmysql.c
@@ -20,7 +20,7 @@
 #include <mysql.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) 
 {
