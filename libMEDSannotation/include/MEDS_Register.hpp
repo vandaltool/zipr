f4279727c8893bc2484903cea2ba92466344a667
nkennedy <nkennedy@git.zephyr-software.com>
2014-09-12 15:15:45 +0000

    Add EFLAGS. Be sure to make clean security_transforms after pulling this in. ok nkennedy@ ip:peasoup
    Former-commit-id: 821d5ba403fda1486448885bc8e627cbcbaeee0c

--- libMEDSannotation/include/MEDS_Register.hpp
+++ libMEDSannotation/include/MEDS_Register.hpp
@@ -9,7 +9,7 @@ namespace MEDS_Annotation
 
 class Register {
 public:
-  enum RegisterName { UNKNOWN, EAX, EBX, ECX, EDX, ESI, EDI, EBP, ESP, AX, BX, CX, DX, BP, SP, SI, DI, AH, BH, CH, DH, AL, BL, CL, DL, RAX, RBX, RCX, RDX, RBP, RSP, RSI, RDI, R8, R9, R10, R11, R12, R13, R14, R15, R8D, R9D, R10D, R11D, R12D, R13D, R14D, R15D, R8W, R9W, R10W, R11W, R12W, R13W, R14W, R15W, SIL, DIL, BPL, SPL, R8B, R9B, R10B, R11B, R12B, R13B, R14B, R15B };
+  enum RegisterName { UNKNOWN, EFLAGS, EAX, EBX, ECX, EDX, ESI, EDI, EBP, ESP, AX, BX, CX, DX, BP, SP, SI, DI, AH, BH, CH, DH, AL, BL, CL, DL, RAX, RBX, RCX, RDX, RBP, RSP, RSI, RDI, R8, R9, R10, R11, R12, R13, R14, R15, R8D, R9D, R10D, R11D, R12D, R13D, R14D, R15D, R8W, R9W, R10W, R11W, R12W, R13W, R14W, R15W, SIL, DIL, BPL, SPL, R8B, R9B, R10B, R11B, R12B, R13B, R14B, R15B };
   static RegisterName getRegister(std::string);
   static RegisterName getRegister(char *str);
   static bool is64bit(RegisterName);
