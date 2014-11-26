b26f7ace92ad0f0d43373644208b331ff390a476
an7s <an7s@git.zephyr-software.com>
2014-11-26 18:12:28 +0000

    better sanity checks in case tries to install 2x
    Former-commit-id: e62a21e7a5bee89f321ae6073d5c6636400f7c60

--- tools/ps_install.sh
+++ tools/ps_install.sh
@@ -6,6 +6,14 @@ PROG=$(basename $0 .install.sh)
 # Default installation directory
 INSTALL_DIR=/tmp/protected/bin
 
+if [ ! -d analysis_dir ]; then
+	echo "Looks like you've already installed ${PROG}"
+	echo
+	echo "Type any key to continue"
+	read anykey
+	exit 1
+fi
+
 echo "Installing protected version of $PROG"
 echo "Installation directory: $INSTALL_DIR"
 echo "Fully qualified path: $INSTALL_DIR/$PROG"
@@ -14,18 +22,19 @@ echo
 echo "Type any key to continue"
 read anykey
 
+# in case it's not there yet
 mkdir -p $INSTALL_DIR
 
 chmod +x analysis_dir/a.stratafied
 
 # wipe out any old installs
 if [ -d "$INSTALL_DIR/${PROG}_analysis" ]; then
-		rm -fr "$INSTALL_DIR/${PROG}_analysis"
+	rm -fr "$INSTALL_DIR/${PROG}_analysis"
 fi
 
 #cp -r analysis_dir $INSTALL_DIR/${PROG}_analysis
 mv analysis_dir $INSTALL_DIR/${PROG}_analysis
-echo "$INSTALL_DIR/${PROG}_analysis/ps_run.sh $INSTALL_DIR/${PROG}_analysis     \"\$0\" \"\$@\"" > $INSTALL_DIR/$PROG
+echo "$INSTALL_DIR/${PROG}_analysis/ps_run.sh $INSTALL_DIR/${PROG}_analysis \"\$0\" \"\$@\"" > $INSTALL_DIR/$PROG
 chmod +x $INSTALL_DIR/$PROG
 
 echo "Installation complete"
