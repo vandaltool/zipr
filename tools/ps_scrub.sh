2e717bb0c534642dec48174ca12c6b47f72dcd94
an7s <an7s@git.zephyr-software.com>
2014-11-26 17:19:55 +0000

    Scrub peasoup directory
    Former-commit-id: 2a636549b2976408e8d781bc086be7fd3744a742

--- tools/ps_scrub.sh
+++ tools/ps_scrub.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # Script to scrub PEASOUP directory
 
 usage()
 {
-  echo "usage: $0 [ <peasoup_program> | <peasoup_dir> ]"
+  echo "usage: $0 [ <peasoup_dir> ]"
   exit 1
 }
 
@@ -23,14 +23,6 @@ verify_peasoup_dir()
   fi
 }
 
-verify_peasoup_app()
-{
-  grep ps_run $1 
-  if [ ! $? -eq 0 ]; then
-    error "$1 not a valid peasoup program"
-  fi
-}
-
 #
 # sanity checks before scrubbing directory
 #
@@ -39,36 +31,19 @@ if [ -d $1 ]; then
   peasoup_dir=$1
   verify_peasoup_dir $peasoup_dir
 else
-  verify_peasoup_app $1
-  peasoup_dir=`grep ps_run $1 | cut -d' ' -f2`
-  verify_peasoup_dir $peasoup_dir
+  usage
 fi
 
 echo "$0: scrubbing peasoup directory: $peasoup_dir"
 
-peasoup_dir_basename=`basename $1`
+cd $peasoup_dir
 
-cd "$peasoup_dir/.."
-
-# make sure we have permission to write by creating a temporary directory
-TMP=$peasoup_dir_basename.$$
-mkdir "$TMP"
-
-if [ ! $? -eq 0 ]; then
-  error "Could not create temporary directory: $TMP"
-fi
-
-cp $peasoup_dir/a.irdb.bspri $TMP
-cp $peasoup_dir/a.ncexe.annot $TMP
-cp $peasoup_dir/a.stratafied $TMP
-cp $peasoup_dir/ps_run.sh $TMP
-
-# mv directory
-junk_it="$peasoup_dir_basename.about_to_be_removed"
-mv "$peasoup_dir_basename" "$junk_it"
-mv "$TMP" "$peasoup_dir"
-
-if [ -d "$junk_it" ]; then
-  rm -fr "$junk_it"
-fi
-
+# start removing stuff
+rm -fr logs/*.log
+rmdir logs
+rm a.irdb.aspri*
+rm a.irdb.bspri
+rm a.ncexe new.exe
+rm *annot.full
+rm *.asm
+rm *.SMPobjdump
