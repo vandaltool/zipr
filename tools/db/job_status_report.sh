c2b52f4f3ed8251157ef26f50ff77b325abf8ee6
an7s <an7s@git.zephyr-software.com>
2014-12-04 21:15:01 +0000

    cleanup
    Former-commit-id: 9e17a1cf8dc7fa1badae551ec6ce2948e30c70df

--- tools/db/job_status_report.sh
+++ tools/db/job_status_report.sh
@@ -1,4 +1,23 @@
 #!/bin/sh -x
+#
+# Copyright (c) 2014 - Zephyr Software LLC
+#
+# This file may be used and modified for non-commercial purposes as long as
+# all copyright, permission, and nonwarranty notices are preserved.
+# Redistribution is prohibited without prior written consent from Zephyr
+# Software.
+#
+# Please contact the authors for restrictions applying to commercial use.
+#
+# THIS SOURCE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+# MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+# Author: Zephyr Software
+# e-mail: jwd@zephyr-software.com
+# URL   : http://www.zephyr-software.com/
+#
+
 
 JOB_ID=$1
 STEP=$2
