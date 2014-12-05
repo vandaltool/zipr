2ad5cba06870d20cff0713044c1fc475041ffebb
an7s <an7s@git.zephyr-software.com>
2014-12-05 17:09:46 +0000

    license txt
    Former-commit-id: 6dcaf6e2a451570f9f4221c4d3a5593e62c8fe4a

--- tools/ps_scrub.sh
+++ tools/ps_scrub.sh
@@ -17,8 +17,18 @@
 # e-mail: jwd@zephyr-software.com
 # URL   : http://www.zephyr-software.com/
 #
-
-
+#
+# This software was developed with SBIR funding and is subject to SBIR Data Rights, 
+# as detailed below.
+#
+# SBIR DATA RIGHTS
+#
+# Contract No. __N00014-14-C-0197___W31P4Q-14-C-0086________.
+# Contractor Name __Zephyr Software LLC_____________________.
+# Address __2040 Tremont Road, Charlottesville, VA 22911____.
+# Expiration of SBIR Data Rights Period __16-JUNE-2021______.
+#
+#
 # Script to scrub PEASOUP directory
 
 usage()
