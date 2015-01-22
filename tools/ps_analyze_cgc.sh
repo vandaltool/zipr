df171cb5cc52e22d5300cbf3efcb94baed3d74ab
jdh8d <jdh8d@git.zephyr-software.com>
2015-01-22 18:51:48 +0000

    Former-commit-id: c67ed91d03e5e420e75470ae1b430cb35a3fddb1

--- tools/ps_analyze_cgc.sh
+++ tools/ps_analyze_cgc.sh
@@ -6,4 +6,7 @@ $PEASOUP_HOME/tools/ps_analyze.sh $* 	\
 	--step find_strings=off 	\
 	--step preLoaded_ILR1=off	\
 	--step preLoaded_ILR2=off	\
+	--step fast_spri=off	\
+	--step fast_annot=off	\
+	--step zipr=on	\
 
