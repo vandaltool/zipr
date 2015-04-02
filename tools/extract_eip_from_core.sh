94826d597245d629d797fad168d93547ef9805ea
an7s <an7s@git.zephyr-software.com>
2015-04-02 19:28:30 +0000

    towards end-to-end
    Former-commit-id: 8f8758a9698e9ee0b05e7eaa3fc164576c38b1d3

---
+++ tools/extract_eip_from_core.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#
+# Extract eip from valid CGC core file
+# Outputs eip value in hex, e.g., 0x804823c
+#
+
+bin=$1
+core=$2
+
+if [ ! -f $bin ]; then
+	echo "binary file: $bin does not exist"
+	exit 1
+fi
+
+if [ ! -f $core ]; then
+	echo "core file: $core does not exist"
+	exit 1
+fi
+
+if [ -z $PS_READELF ]; then
+	echo "Environment variable PS_READELF is not defined"
+	exit 1
+fi
+
+$PS_READELF -h $core | grep -i Type | grep -i core &> /dev/null
+if [ ! $? -eq 0 ]; then
+	echo "$core is not a valid core file"
+	exit 1
+fi
+
+gdb $1 $2 --batch --ex "info registers eip" | grep eip | awk -F " " '{print $2;}'
---
+++ tools/sfuzz/replay_seed_inputs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cgc_binary=$1            # binary 
+seeds_dir=$2             # path of seed input directory
+crash_dir=$3             # where to copy any crashing inputs
+crash_eip_file=$4        # out: name of file where we output the crashing EIPs
+
+#seeds_dir=${PEASOUP_HOME}/tools/sfuzz/seed_inputs
+
+TIMEOUT=10
+
+# make sure we can get a core file
+ulimit -c unlimited
+
+#
+# feed seed inputs to binary
+#
+for i in `ls $seeds_dir`
+do
+	input=${seeds_dir}/$i
+	rm core &> /dev/null
+	timeout $TIMEOUT $cgc_binary < $input
+	if [ -f core ]; then
+		cp $input $crash_dir
+		eip=`${PEASOUP_HOME}/tools/extract_eip_from_core.sh $cgc_binary core`
+		if [ $? -eq 0 ]; then
+			echo $eip >> $crash_eip_file			
+		fi
+
+	fi
+done
+
+if [ -f $crash_eip_file ]; then
+	sort $crash_eip_file | uniq > tmp.$$
+	mv tmp.$$ $crash_eip_file
+fi
