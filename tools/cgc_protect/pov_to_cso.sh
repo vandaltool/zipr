d931b57af970e2db04d90575e60489420bff66fd
an7s <an7s@git.zephyr-software.com>
2015-05-06 20:33:34 +0000

    Script that takes a POV dir and produces a CSO warning file suitable for the sandboxing step
    Former-commit-id: 2dfd33c69e03e05e417b0dd45535bdc128c0c047

---
+++ tools/cgc_protect/pov_to_cso.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#
+# Given a directory full of POVs, protect a singleton CB
+# by sandboxing the appropriate instructions 
+# 
+#
+
+CGC_BIN=$1       # input cgc binary
+CGC_CSID=$2      # cgc name
+POV_DIR=$3       # directory containing POVs 
+CSO_FILE=$4      # output: CSO warning file suitable for sandboxing step
+
+log=tmp.log.$$
+
+CRASH_SITES=tmp.crashes.$$
+
+# run cb-test on each POV invidually
+for i in `ls ${POV_DIR}/*.xml`
+do
+	echo ""
+	echo $i
+
+	one_pov=$i
+	binary=`basename $CGC_BIN`
+	binary_dir=`dirname $CGC_BIN`
+	core=${binary_dir}/core
+
+	sudo rm $core 2>/dev/null
+	sudo cb-test --debug --xml ${one_pov} --timeout 20 --directory ${binary_dir} --cb ${binary} --log $log 
+	grep "core identified" $log
+	if [ $? -eq 0 ]; then
+		if [ -f $core ]; then
+			sudo chown `whoami` $core 
+			eip=`$PEASOUP_HOME/tools/extract_eip_from_core.sh ${CGC_BIN} $core`
+			if [ $? -eq 0 ]; then
+				echo "$eip" >> $CRASH_SITES
+			fi
+			echo "EIP: $eip"
+		fi
+	fi
+
+	sudo rm $core 2>/dev/null
+done
+
+#
+# sandbox all uncovered faulting instructions
+#
+if [ -f $CRASH_SITES ]; then
+	tmp=tmp.$$
+	sort $CRASH_SITES | uniq > $tmp
+	mv $tmp $CRASH_SITES
+
+	while read -r LINE || [[ -n $LINE ]]; do
+	echo "$CGC_CSID,$LINE,,Tainted Dereference" >> $CSO_FILE
+	done < $CRASH_SITES
+fi
+
+sudo rm $log 2>/dev/null
+rm $CRASH_SITES 2>/dev/null
+
+exit 0
