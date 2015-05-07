eb30b2009e81e09bcc79557c3194f532d034f428
an7s <an7s@git.zephyr-software.com>
2015-05-07 15:35:18 +0000

    renamed cgc_protect_pov step to just protect_pov
    Former-commit-id: 892ffb8b0ea223f03ff6329ffe8da1842112167f

--- tools/ps_analyze.sh
+++ tools/ps_analyze.sh
@@ -827,9 +827,9 @@ perform_step find_strings none $SECURITY_TRANSFORMS_HOME/libIRDB/test/find_strin
 perform_step appfw find_strings $PEASOUP_HOME/tools/do_appfw.sh $arch_bits $newname.ncexe logs/find_strings.log
 
 #
-# cgc_protect_pov
+# protect_pov
 #
-perform_step cgc_protect_pov fill_in_indtargs $PEASOUP_HOME/tools/do_protect_pov.sh a.ncexe $name crash.cso $step_options_cgc_protect_pov
+perform_step protect_pov fill_in_indtargs $PEASOUP_HOME/tools/do_protect_pov.sh a.ncexe $name crash.cso $step_options_protect_pov
 if [ -f crash.cso  ]; then
 	step_options_watch_allocate="$step_options_watch_allocate --warning_file=crash.cso"
 fi
