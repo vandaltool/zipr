041c5eb99954835ee68c380f68865771e0004514
an7s <an7s@git.zephyr-software.com>
2015-05-26 02:14:35 +0000

    replay and extract crashing eip using gdb does not rely on generating core files
    Former-commit-id: b1fe4c72062ee8c2bb47ea9023bab139068f0a94

---
+++ tools/replay_with_gdb.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#
+# Run program under gdb
+# Detect seg faults
+# Make sure valid address
+# Extract eip
+#
+
+bin=$1
+input_file=$2
+
+if [ ! -f $bin ]; then
+	echo "binary file: $bin does not exist"
+	exit 1
+fi
+
+if [ ! -f $bin ]; then
+	echo "input file: $input_file does not exist"
+	exit 1
+fi
+
+# look for segmentation fault
+gdb $1 --batch --ex "run < $2" --ex "info registers \$eip" > gdb.out 2>&1
+grep -i segmentation gdb.out &>/dev/null 
+if [ $? -eq 0 ]; then
+    eip=`grep eip gdb.out | awk -F " " '{print $2;}'`
+    # does eip point to a valid instruction?
+    gdb $1 --batch --ex "x/i $eip" 2>&1 | grep -i "cannot access" &>/dev/null
+    if [ $? -eq 0 ]; then
+       echo "crashed but eip $eip does not point to a valid instruction" 
+       exit 1
+    fi
+    echo $eip
+    exit 0
+fi
+
+exit 1
