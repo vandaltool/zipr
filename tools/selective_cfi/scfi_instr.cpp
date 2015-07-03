f923ba56a9e5313f6d83ea680e24102f78923634
an7s <an7s@git.zephyr-software.com>
2015-07-03 13:35:30 +0000

    added copyright headers
    Former-commit-id: fcd03f0d69b838b93aa63887619bf2ad0de50152

--- tools/selective_cfi/scfi_instr.cpp
+++ tools/selective_cfi/scfi_instr.cpp
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2014-2015 - Zephyr Software LLC
+ *
+ * This file may be used and modified for non-commercial purposes as long as
+ * all copyright, permission, and nonwarranty notices are preserved.
+ * Redistribution is prohibited without prior written consent from Zephyr
+ * Software.
+ *
+ * Please contact the authors for restrictions applying to commercial use.
+ *
+ * THIS SOURCE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+ * MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Author: Zephyr Software
+ * e-mail: jwd@zephyr-software.com
+ * URL   : http://www.zephyr-software.com/
+ *
+ */
+
 
 #include "utils.hpp"
 #include "scfi_instr.hpp"
