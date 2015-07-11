8070f5cf1b6d2756f9ee2de1492b91a5a2f09565
jdh8d <jdh8d@git.zephyr-software.com>
2015-07-11 15:49:09 +0000

    Former-commit-id: b8010e56ce9adb7ce80601bc144a17cbe3bd030e

--- libEXEIO/src/exeio_src.cpp
+++ libEXEIO/src/exeio_src.cpp
@@ -19,15 +19,19 @@ void exeio::load(char* filename)
 	if(!instream)
 		assert(0 && "Cannot open file");
 
+	int c0=instream.get();
+	int c1=instream.get();
+	int c2=instream.get();
+	int c3=instream.get();
 
 	// check for elf magic number
-	if( instream.get()=='E' && instream.get()=='L' && instream.get()=='F' )
+	if(c0 == '\177' && c1=='E' && c2=='L' && c3=='F')
 	{
-		backend=new exeio_elf_backend_t();
+		backend=new exeio_elf_backend_t;
 	}
 	// we assume it's ELF or PE.
 	else
-		backend=new exeio_pe_backend_t();
+		backend=new exeio_pe_backend_t;
 
 	backend->load(this, filename);
 
