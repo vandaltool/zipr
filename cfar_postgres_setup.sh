aa43a231aeebd14ae147ee2eccf8b6e7f40db19c
jdh8d <jdh8d@git.zephyr-software.com>
2015-10-22 20:51:11 +0000

    Updates to get large-only working, and fixes to cfar.sh and cfar_postgres setup to avoid raciness.

--- cfar_postgres_setup.sh
+++ cfar_postgres_setup.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/bin/bash -x
 
+
+echo \$USER is $USER
 #Create Database User and Table
 source set_env_vars
 for i in $(seq 0 4)
 do
-	sudo su -c "createdb -O $USER peasoup_$USER_v$i" postgres
+	sudo su -c "dropdb peasoup_${USER}_v$i" postgres
+	sudo su -c "createdb -O ${USER} peasoup_${USER}_v$i" postgres
 
 	#Setup the Database to store PEASOUP info
-	PGDATABASE=peasoup_$USER_v$i $PEASOUP_HOME/tools/db/pdb_setup.sh
+	PGDATABASE=peasoup_${USER}_v$i $PEASOUP_HOME/tools/db/pdb_setup.sh
 
 done
