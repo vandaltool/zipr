6eb671f09e8b50795a7d4bbd8ba810fb0e427118
jdh8d <jdh8d@git.zephyr-software.com>
2015-10-22 15:56:56 +0000

    Broken version being committed, so will can commit his broken version so we can both finish our hacking without dealing with repo conflicts.
    Former-commit-id: 617441b93a4eb9c1dc25acfca88fdbbfd21447cc

--- tools/cfar.sh
+++ tools/cfar.sh
@@ -18,13 +18,16 @@ for seq in $(seq 0 $(expr $variants - 1) )
 do
 
 	sharepath_key="$seq:$variants:dir://$share_path "
+	zipr_env="ZIPR_VARID=$sharepath_key"
 	p1options=" --step-option p1transform:--canary_value --step-option p1transform:0xFF0${seq}${seq}0FF --step-option p1transform:--random_seed --step-option p1transform:$anyseed "
-	cmd=" PG_DATABASE=peasoup_`whoami`_$seq $PEASOUP_HOME/tools/ps_analyze.sh $in $out.v$seq $@ $p1options > peasoup.v$seq 2>&1 &"
+	cmd=" PG_DATABASE=peasoup_`whoami`_$seq $zipr_env $PEASOUP_HOME/tools/ps_analyze.sh $in $out.v$seq $@ $p1options > peasoup.v$seq 2>&1 &"
 	echo $cmd
 	eval $cmd
 	pids="$pids $!"
 
 done
+
+ok=1
 
 for i in $pids;
 do
@@ -32,7 +35,19 @@ do
 	exit_code=$?
 	if [ $exit_code != 0 ]; then
 		echo "Peasoup process $i failed with excode code: $exit_code."
+		ok=0
 	fi
 done
 
-exit 0
+if [ $ok = 1 ] ; then
+	echo "Successfully generated $variants variants"
+	exit 0
+else
+	echo
+	echo
+	echo "Some variants failed"
+	echo
+	echo
+	exit 1
+fi
+
