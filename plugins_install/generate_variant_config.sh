a89496d1c7851ff4dd1ae3db725f8ac915fc5b2c
jdh8d <jdh8d@git.zephyr-software.com>
2015-11-27 03:21:18 +0000

    Former-commit-id: 85fe6baeb6e8754c3b14820f0b2296cdd11c297c

--- plugins_install/generate_variant_config.sh
+++ plugins_install/generate_variant_config.sh
@@ -14,8 +14,33 @@ if [ $backend = "zipr" ]; then
 
 
 elif [ $backend = "strata" ]; then
-	echo Found strata backend.
-	exit 1
+
+	echo "Found strata backend."
+
+#
+# note that these are all hard-coded in the config file right now.
+# plan:  mine these values out of ps_run.sh and replace in config file.
+#
+#"STRATA_LOG=detectors",
+#"PEASOUP_SCHEDULE_PERTURB=0",
+#"STRATA_WATCHDOG=0",
+#"STRATA_NUM_HANDLE=0",
+#"STRATA_DOUBLE_FREE=0",
+#"STRATA_HEAPRAND=0",
+#"STRATA_SHADOW_STACK=0",
+#"STRATA_CONTROLLED_EXIT=0",
+#"STRATA_DETECT_SERVERS=0",
+#"STRATA_PC_CONFINE=0",
+#"STRATA_PC_CONFINE_XOR=0",
+#"STRATA_REKEY_AFTER=0",
+#"STRATA_PC_CONFINE_XOR_KEY_LENGTH=1024",
+#"STRATA_IS_SO=0",
+#"STRATA_MAX_WARNINGS=500000"
+
+	base_peasoup_dir=$(basename $newdir)
+
+	cat $PEASOUP_HOME/tools/cfar_configs/strata_variant.json.template| sed "s|<<PS_DIR>>|$base_peasoup_dir|" > variant_config.json
+	exit 0
 
 else
 	echo Unknown backend.
