14ca99382c3e7f4a55bc342d420b06e09e3bb8d5
an7s <an7s@git.zephyr-software.com>
2016-01-02 23:26:35 +0000

    IBT checker driven off STARS xref annot file and QEMU trace
    Former-commit-id: 3940a4497f6a67fd5f504b9b8fdd334e861b39a6

---
+++ tools/ibtcheck/ibtcheck.py
@@ -0,0 +1,122 @@
+import sys
+import argparse
+from sets import Set
+
+# only works for single-threaded programs
+
+def display_violation(ib_src, ib_seen, allowed):
+	allowed_hex = []
+	for tgt in allowed:
+		allowed_hex.append(hex(tgt))
+
+	print 'IBT violation detected at: ', hex(ib_src), ' --> ', hex(ib_seen), ' allowed targets: ', allowed_hex
+
+def display_violations(violations, ibtargets):
+	for src in violations:
+		for v in violations[src]:
+			display_violation(src, v, ibtargets[src]['targets'])
+
+		
+if __name__ == "__main__":
+
+	parser = argparse.ArgumentParser(description='Validate STARS IB COMPLETE annotations')
+	parser.add_argument('stars_xrefs', type=file,
+                   help='STARS Xrefs annotation file')
+	parser.add_argument('trace_file', type=file,
+                   help='QEMU-style instructions')
+
+	args = parser.parse_args();
+
+# stash away the annotations
+#  cols   0         1  2     3    4       5       6        7
+#       465d0f      1 INSTR XREF IBT    FROMIB   465c72 RETURNTARGET
+#       465de1      1 INSTR XREF IBT    FROMIB   465c72 RETURNTARGET
+#       465c72      1 INSTR XREF FROMIB COMPLETE    2
+	ibtargets = {}
+	for line in args.stars_xrefs:
+		cols = line.split()
+		if cols[5] == 'FROMIB':
+			src = long(cols[6], 16)
+			dst = long(cols[0], 16)
+
+			if not src in ibtargets:
+				ibtargets[src] = {}
+
+			if not 'targets' in ibtargets[src]:
+				ibtargets[src]['targets'] = []
+
+			if not 'complete' in ibtargets[src]:
+				ibtargets[src]['complete'] = False
+
+			ibtargets[src]['targets'].append(dst)
+		elif cols[5] == 'COMPLETE':
+			src = long(cols[0], 16)
+
+			if not src in ibtargets:
+				ibtargets[src] = {}
+
+			ibtargets[src]['complete'] = True
+
+			
+	print ibtargets
+
+# 0x000000000045d7b9:  retq   
+# 0x00000040013575e0:  push   %r13
+
+	violations = {}
+
+	to_check = -1L
+	for line in args.trace_file:
+		if not line.startswith('0x'):
+			continue
+
+		cols = line.split(':')
+		instr = long(cols[0], 16)
+
+		if to_check >= 0:
+			if not instr in ibtargets[to_check]['targets']:
+				print 'Detected ibtarget violation at ', to_check, '(hex) ', hex(to_check), instr, '(hex) ', hex(instr), '(', len(ibtargets[to_check]['targets']), ')', ibtargets[to_check]['targets']	
+				if not to_check in violations:
+					violations[to_check] = Set()
+				violations[to_check].add(instr)
+			else:
+				if not 'covered' in ibtargets[to_check]:
+					ibtargets[to_check]['covered'] = Set()
+
+				ibtargets[to_check]['covered'].add(instr)
+					
+			to_check = -1L
+
+		# @todo: other source of indirect control flow transfer
+		if not 'retq' in line:
+			continue
+
+		# did STARS mark complete?
+		# if yes check the next instruction
+		if instr in ibtargets and ibtargets[instr]['complete'] == True:
+			to_check = instr		
+	
+	# get some statistics
+	total_ibtargets = 0
+	ibtargets_covered = 0
+	icfs_coverage_count = 0
+	for src_instr in ibtargets:
+		total_ibtargets += len(ibtargets[src_instr]['targets'])
+		if 'covered' in ibtargets[src_instr]:
+			icfs_coverage_count += 1
+			ibtargets_covered += len(ibtargets[src_instr]['covered'])
+			print 'icfs coverage: #targets_visited', len(ibtargets[src_instr]['covered']), '/', len(ibtargets[src_instr]['targets'])
+
+#	print ibtargets
+
+	if len(violations) > 0:
+		display_violations(violations, ibtargets)
+	else:
+		print 'No ICFS violations detected'
+
+	print '#icfs_covered: ', icfs_coverage_count, '/', len(ibtargets)
+	print 'icfs_coverage: ', icfs_coverage_count * 1.0 / len(ibtargets)
+
+	print '#ibibtargets_covered: ', ibtargets_covered, '/', total_ibtargets
+	print 'ibtargets_coverage', 1.0*ibtargets_covered / total_ibtargets
+
