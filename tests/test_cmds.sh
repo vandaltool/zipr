bcd2b9a0fb76acdacc72bfdf77bad50a11f47248
an7s <an7s@git.zephyr-software.com>
2016-01-23 17:50:58 +0000

    Tests whole bunch of configurations at once
    Former-commit-id: 6771030916225b2c53a5e362c3cf207836b9b020

--- tests/test_cmds.sh
+++ tests/test_cmds.sh
@@ -1,23 +1,24 @@
-#orig_progs="xcalc xeyes nano"
-#orig_progs="bzip2 cal du egrep fgrep grep ls objdump readelf sort tar tcpdump touch"
-#orig_progs="objdump"
-
+# specify configs here
+configs="zipr scfi scfi.color scfi.color.nojmps kill_deads"
+# specify programs to test
 orig_progs="bzip2 cal du grep ls objdump readelf sort tar tcpdump touch"
-
-if [ -d tmp_test_area ]; then
-	rm -fr tmp_test_area
-fi
-
-mkdir tmp_test_area
-
-pushd .
-cd tmp_test_area
-
 build_only=0
+
+for config in $configs
+do
 
 progs_pass=""
 progs_fail=""
 progs_fail_peasoup=""
+
+if [ -d tmp_test_area.$config ]; then
+	rm -fr tmp_test_area.$config
+fi
+
+mkdir tmp_test_area.$config
+
+pushd .
+cd tmp_test_area.$config
 
 for prog in $orig_progs
 do
@@ -33,19 +34,32 @@ do
 		rm $protected
 	fi
 
-	echo "TEST ${prog}: Protecting..."
+	echo "TEST ($config) ${prog}: Protecting..."
 
-	#
-	# change options here for different kinds of protections
-	#
-#	$PEASOUP_HOME/tools/ps_analyze.sh `which $prog` $protected --backend zipr --tempdir $temp_dir > test_${prog}.ps.log 2>&1
+	case $config in
+		zipr)
+			$PEASOUP_HOME/tools/ps_analyze.sh `which $prog` $protected --backend zipr --tempdir $temp_dir > test_${prog}.ps.log 2>&1
+		;;
+		scfi.color)
+			FIX_CALLS_FIX_ALL_CALLS=1 $PEASOUP_HOME/tools/ps_analyze.sh `which $prog` $protected --backend zipr --step selective_cfi=on --step-option selective_cfi:--color --tempdir $temp_dir > test_${prog}.ps.log 2>&1
+		;;
+		scfi.color.nojmps)
+			FIX_CALLS_FIX_ALL_CALLS=1 $PEASOUP_HOME/tools/ps_analyze.sh `which $prog` $protected --backend zipr --step selective_cfi=on --step-option selective_cfi:--color --step-option selective_cfi:--no-protect-jumps --tempdir $temp_dir > test_${prog}.ps.log 2>&1
+		;;
+		scfi)
+			FIX_CALLS_FIX_ALL_CALLS=1 $PEASOUP_HOME/tools/ps_analyze.sh `which $prog` $protected --backend zipr --step selective_cfi=on --tempdir $temp_dir > test_${prog}.ps.log 2>&1
+		;;
+		kill_deads)
+			$PEASOUP_HOME/tools/ps_analyze.sh `which $prog` $protected --backend zipr --step kill_deads=on --tempdir $temp_dir > test_${prog}.ps.log 2>&1
+		;;
+		*)
+			echo "Unknown configuration requested"
+			continue
+		;;
+	esac
 
-#	FIX_CALLS_FIX_ALL_CALLS=1 $PEASOUP_HOME/tools/ps_analyze.sh `which $prog` $protected --backend zipr --step selective_cfi=on --tempdir $temp_dir > test_${prog}.ps.log 2>&1
-	FIX_CALLS_FIX_ALL_CALLS=1 $PEASOUP_HOME/tools/ps_analyze.sh `which $prog` $protected --backend zipr --step selective_cfi=on --step-option selective_cfi:--color --step-option selective_cfi:--no-protect-jumps --tempdir $temp_dir > test_${prog}.ps.log 2>&1
-#	FIX_CALLS_FIX_ALL_CALLS=1 $PEASOUP_HOME/tools/ps_analyze.sh `which $prog` $protected --backend zipr --step selective_cfi=on --step-option selective_cfi:--color --tempdir $temp_dir > test_${prog}.ps.log 2>&1
-#	timeout 900 $PEASOUP_HOME/tools/ps_analyze.sh `which $prog` $protected --backend zipr --step kill_deads=on --tempdir $temp_dir > test_${prog}.ps.log 2>&1
 	if [ ! $? -eq 0 ]; then
-		echo "TEST ${prog}: FAILED to peasoupify"
+		echo "TEST ($config) ${prog}: FAILED to peasoupify"
 		progs_fail="$progs_fail $prog"
 		progs_fail_peasoup="$progs_fail_peasoup $prog"
 	fi
@@ -54,20 +68,22 @@ do
 		continue
 	fi
 
-	echo "TEST ${prog}: Running tests..."
+	echo "TEST $config ${prog}: Running tests..."
 	timeout 300 ../$prog/test_script.sh `which $prog` ./$protected > test_${prog}.log 2>&1
 	if [ $? -eq 0 ]; then
-		echo "TEST ${prog}: PASS"
+		echo "TEST ($config) ${prog}: PASS"
 		progs_pass="$progs_pass $prog"
 	else
-		echo "TEST ${prog}: FAIL"
+		echo "TEST ($config) ${prog}: FAIL"
 		progs_fail="$progs_fail $prog"
 	fi
 done
 
 echo "================================================"
-echo "PASS: $progs_pass"
-echo "FAIL: $progs_fail"
-echo "FAIL (ps_analyze): $progs_fail_peasoup"
+echo "TEST SUMMARY PASS ($config): $progs_pass"
+echo "TEST SUMMARY FAIL ($config): $progs_fail"
+echo "TEST SUMMARY FAIL ($config) (ps_analyze): $progs_fail_peasoup"
 
 popd
+
+done
