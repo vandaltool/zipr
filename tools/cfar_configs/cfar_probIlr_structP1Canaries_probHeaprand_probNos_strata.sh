263c2708f034c886b6c19b593e84125b55e3911a
jdh8d <jdh8d@git.zephyr-software.com>
2016-03-17 13:05:08 +0000

    Former-commit-id: 19d48349e6f69064ba5e9f25f6862c66ebd8c5cd

---
+++ tools/cfar_configs/cfar_probIlr_structP1Canaries_probHeaprand_probNos_strata.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+$PEASOUP_HOME/tools/cfar.sh "$@" --backend strata --step ilr=on --structured_p1_canaries --step p1transform=on --config_name $(basename $0 .sh|sed "s/cfar_//") --diehard --step non_overlapping_stack=on
