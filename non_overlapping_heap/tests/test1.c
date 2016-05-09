a2dd86bc7f9ecc24b52abd2a87856a681a96f912
jdh8d <jdh8d@git.zephyr-software.com>
2016-05-09 15:57:35 +0000

    Former-commit-id: 10b6ed4ae0cac85dd21be4b1adcaf38613c2deb5

--- non_overlapping_heap/tests/test1.c
+++
@@ -1,8 +0,0 @@
-#include <sys/mman.h>
-#include <stdio.h>
-
-int main() {
-	void* foobar = mmap(NULL, 1024, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
-	printf("%p\n",foobar);
-	return 0;
-}
