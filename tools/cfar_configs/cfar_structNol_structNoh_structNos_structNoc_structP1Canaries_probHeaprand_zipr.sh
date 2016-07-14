bdaa9832960199e1738e2660c7b3cbdee47b79ad
dem7yw <dem7yw@git.zephyr-software.com>
2016-07-14 14:56:53 +0000

    Add set_interpreter to nol configs.
    Former-commit-id: 25a762b8e2c73595462227531fad7ac9575240b6

--- tools/cfar_configs/cfar_structNol_structNoh_structNos_structNoc_structP1Canaries_probHeaprand_zipr.sh
+++ tools/cfar_configs/cfar_structNol_structNoh_structNos_structNoc_structP1Canaries_probHeaprand_zipr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-$PEASOUP_HOME/tools/cfar.sh "$@" --backend zipr --diehard --step-option zipr:"--zipr:seed $$" --structured_noc --structured_nos --step-option zipr:"--large_only:on true" --structured_p1_canaries  --step p1transform=on --step non_overlapping_stack=on --config_name $(basename $0 .sh|sed "s/cfar_//")
+$PEASOUP_HOME/tools/cfar.sh "$@" --backend zipr --diehard --step-option zipr:"--zipr:seed $$" --structured_noc --structured_nos --step-option zipr:"--large_only:on true" --structured_p1_canaries  --step p1transform=on --step non_overlapping_stack=on ---step set_interpreter=on --step-option set_interpreter:"--interp /target_apps/ld-nol.so" -config_name $(basename $0 .sh|sed "s/cfar_//")
