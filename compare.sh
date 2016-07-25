637c8a57799a28f7e4a9f48130495d24a1363b4b
an7s <an7s@git.zephyr-software.com>
2016-07-25 19:14:48 +0000

    Add a script to compare two binaries using the simplest metric described.

---
+++ compare.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ORIGINAL_CB=$1
+REPLACEMENT_CB=$2
+
+INPUT=test
+
+BASE_OUTPUT=$(echo $INPUT | timeout 5s $1)
+BASE_RESULT=$?
+RCB_OUTPUT=$(echo $INPUT | timeout 5s $2)
+RCB_RESULT=$?
+
+echo $BASE_RESULT
+echo $RCB_RESULT
+
+if [ $BASE_RESULT -ne $RCB_RESULT ]; then
+	exit 1
+fi
+if [ $BASE_OUTPUT -ne $RCB_OUTPUT ]; then
+	exit 1
+fi
+exit 0
