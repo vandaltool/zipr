eeb24983b4190d7dbcbee8268b51a8b19439dcbf
an7s <an7s@git.zephyr-software.com>
2016-08-02 17:48:12 +0000

    Update script

--- update-all.sh
+++ update-all.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-for i in `ls`
+
+svn_top="daffy diablo_toolchain IdaProServer irdb_transforms irdb_vars peasoup_examples security_transforms strata stratafier zipr zipr_callbacks zipr_install zipr_relax_plugin zipr_scfi_plugin zipr_sdk"
+for i in $svn_top
 do
 	if [ -d "$i" ]; then
 		pushd "$i"
-		svn update
+		svn diff -r head
 		popd
 	fi
 done
+
