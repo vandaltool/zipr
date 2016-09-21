0d09aba0d4c261929d038e5d65999815bc32892a
an7s <an7s@git.zephyr-software.com>
2016-09-21 14:43:36 +0000

    No longer use sudo to setup database tables
    Former-commit-id: e55baa86e2ba5e60afbbde12f1f14414a7bce2a3

--- cfar_postgres_setup.sh
+++ cfar_postgres_setup.sh
@@ -6,10 +6,9 @@ echo \$USER is $USER
 source set_env_vars
 for i in $(seq 0 4)
 do
-	sudo su -c "dropdb peasoup_${USER}_v$i" postgres
-	sudo su -c "createdb -O ${USER} peasoup_${USER}_v$i" postgres
+	dropdb peasoup_${USER}_v$i
+	createdb -O ${USER} peasoup_${USER}_v$i
 
 	#Setup the Database to store PEASOUP info
 	PGDATABASE=peasoup_${USER}_v$i $PEASOUP_HOME/tools/db/pdb_setup.sh
-
 done
