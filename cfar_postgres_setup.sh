a687ca639d88e99690c9a4590dc03eb81ef62aa5
jdh8d <jdh8d@git.zephyr-software.com>
2016-09-29 17:32:36 +0000

    Former-commit-id: 1499844077e96cd3ed1ebb271d619073d16e7201

--- cfar_postgres_setup.sh
+++ cfar_postgres_setup.sh
@@ -2,13 +2,23 @@
 
 
 echo \$USER is $USER
+
+
+reset_db()
+{
+	dropdb $PGDATABASE || true
+	createdb -O ${USER} $PGDATABASE
+	$PEASOUP_HOME/tools/db/pdb_setup.sh
+}
+
+
+# reset default db
+reset_db
+
 #Create Database User and Table
 source set_env_vars
 for i in $(seq 0 4)
 do
-	dropdb peasoup_${USER}_v$i
-	createdb -O ${USER} peasoup_${USER}_v$i
-
 	#Setup the Database to store PEASOUP info
-	PGDATABASE=peasoup_${USER}_v$i $PEASOUP_HOME/tools/db/pdb_setup.sh
+	PGDATABASE=peasoup_${USER}_v$i reset_db
 done
