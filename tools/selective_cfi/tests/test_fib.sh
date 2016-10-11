2c945bf213255b39cc60672d69d5f49dde841806
an7s <an7s@git.zephyr-software.com>
2016-10-11 13:13:15 +0000

    Add different optimization levels to tests
    Former-commit-id: 472b61c5a927743376c969077962ddb66aba2336

--- tools/selective_cfi/tests/test_fib.sh
+++ tools/selective_cfi/tests/test_fib.sh
@@ -55,6 +55,24 @@ build()
 	gcc -o fib.exe.pie fib.c -fPIC -fpie -pie -w -L. -lfib -lfib2
 	mv libfib.so libfib.so.orig
 	mv libfib2.so libfib2.so.orig
+
+	gcc -o libfib2.O.so libfib2.c -O -w -shared -fPIC
+	gcc -o libfib2.O2.so libfib2.c -O2 -w -shared -fPIC
+	gcc -o libfib2.O3.so libfib2.c -O3 -w -shared -fPIC
+	gcc -o libfib2.Os.so libfib2.c -Os -w -shared -fPIC
+	mv libfib2.O.so libfib2.O.so.orig
+	mv libfib2.O2.so libfib2.O2.so.orig
+	mv libfib2.O3.so libfib2.O3.so.orig
+	mv libfib2.Os.so libfib2.Os.so.orig
+
+	gcc -o libfib.O.so libfib.c -O -w -shared -fPIC
+	gcc -o libfib.O2.so libfib.c -O2 -w -shared -fPIC
+	gcc -o libfib.O3.so libfib.c -O3 -w -shared -fPIC
+	gcc -o libfib.Os.so libfib.c -Os -w -shared -fPIC
+	mv libfib.O.so libfib.O.so.orig
+	mv libfib.O2.so libfib.O2.so.orig
+	mv libfib.O3.so libfib.O3.so.orig
+	mv libfib.Os.so libfib.Os.so.orig
 }
 
 
@@ -67,6 +85,26 @@ protect()
 	do_coloring_cfi ./fib.exe ./fib.exe.cfi.color
 	do_coloring_cfi ./libfib.so.orig ./libfib.so.cfi.color
 	do_coloring_cfi ./libfib2.so.orig ./libfib2.so.cfi.color
+
+	do_cfi ./libfib2.O.so.orig ./libfib2.O.so.cfi
+	do_cfi ./libfib2.O2.so.orig ./libfib2.O2.so.cfi
+	do_cfi ./libfib2.O3.so.orig ./libfib2.O3.so.cfi
+	do_cfi ./libfib2.Os.so.orig ./libfib2.Os.so.cfi
+
+	do_coloring_cfi ./libfib2.O.so.orig ./libfib2.O.so.cfi.color
+	do_coloring_cfi ./libfib2.O2.so.orig ./libfib2.O2.so.cfi.color
+	do_coloring_cfi ./libfib2.O3.so.orig ./libfib2.O3.so.cfi.color
+	do_coloring_cfi ./libfib2.Os.so.orig ./libfib2.Os.so.cfi.color
+
+	do_cfi ./libfib.O.so.orig ./libfib.O.so.cfi
+	do_cfi ./libfib.O2.so.orig ./libfib.O2.so.cfi
+	do_cfi ./libfib.O3.so.orig ./libfib.O3.so.cfi
+	do_cfi ./libfib.Os.so.orig ./libfib.Os.so.cfi
+
+	do_coloring_cfi ./libfib.O.so.orig ./libfib.O.so.cfi.color
+	do_coloring_cfi ./libfib.O2.so.orig ./libfib.O2.so.cfi.color
+	do_coloring_cfi ./libfib.O3.so.orig ./libfib.O3.so.cfi.color
+	do_coloring_cfi ./libfib.Os.so.orig ./libfib.Os.so.cfi.color
 }
 
 clean()
@@ -192,6 +230,27 @@ main()
 	test fib.exe.cfi.color 3 libfib.so.cfi.color libfib2.so.cfi.color
 	test fib.exe.cfi.color 4 libfib.so.cfi.color libfib2.so.cfi.color
 	test fib.exe.cfi.color 5 libfib.so.cfi.color libfib2.so.cfi.color
+
+	# some tests for different optimization levels
+	test fib.exe.cfi.color 5 libfib.so.orig libfib2.O.so.cfi
+	test fib.exe.cfi.color 5 libfib.so.orig libfib2.O2.so.cfi
+	test fib.exe.cfi.color 5 libfib.so.orig libfib2.O3.so.cfi
+	test fib.exe.cfi.color 5 libfib.so.orig libfib2.Os.so.cfi
+
+	test fib.exe.cfi.color 5 libfib.so.cfi.color libfib2.O.so.cfi.color
+	test fib.exe.cfi.color 5 libfib.so.cfi.color libfib2.O2.so.cfi.color
+	test fib.exe.cfi.color 5 libfib.so.cfi.color libfib2.O3.so.cfi.color
+	test fib.exe.cfi.color 5 libfib.so.cfi.color libfib2.Os.so.cfi.color
+
+	test fib.exe.cfi.color 5 libfib.O3.so.orig libfib2.O.so.cfi
+	test fib.exe.cfi.color 5 libfib.O2.so.orig libfib2.O2.so.cfi
+	test fib.exe.cfi.color 5 libfib.Os.so.orig libfib2.O3.so.cfi
+	test fib.exe.cfi.color 5 libfib.O.so.orig libfib2.Os.so.cfi
+
+	test fib.exe.cfi.color 5 libfib.O2.so.cfi.color libfib2.O.so.cfi.color
+	test fib.exe.cfi.color 5 libfib.O3.so.cfi.color libfib2.O2.so.cfi.color
+	test fib.exe.cfi.color 5 libfib.O.so.cfi.color libfib2.O3.so.cfi.color
+	test fib.exe.cfi.color 5 libfib.Os.so.cfi.color libfib2.Os.so.cfi.color
 
 	report
 	clean
