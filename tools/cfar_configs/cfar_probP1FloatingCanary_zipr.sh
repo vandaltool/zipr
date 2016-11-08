ea8c9258fc5a414d24666020c38c343f96f388de
an7s <an7s@git.zephyr-software.com>
2016-11-08 14:38:32 +0000

    Add floating canary cfar scripts
    Former-commit-id: 16dc3a4fdf99414349ab02bb23a3f7c99b0f714c

---
+++ tools/cfar_configs/cfar_probP1FloatingCanary_zipr.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+$PEASOUP_HOME/tools/cfar.sh "$@" --backend zipr --step p1transform=on --step-option p1transform:"--canaries=on --floating_canary" --step-option zipr:"--zipr:seed $$" --config_name $(basename $0 .sh|sed "s/cfar_//")
---
+++ tools/cfar_configs/cfar_structP1FloatingCanary_zipr.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+
+if [ ! -z $NO_FLOAT ];
+then
+	$PEASOUP_HOME/tools/cfar.sh "$@" --backend zipr --step-option zipr:"--zipr:seed $$" --structured_p1_canaries --step p1transform=on --config_name $(basename $0 .sh|sed "s/cfar_//")
+else
+	$PEASOUP_HOME/tools/cfar.sh "$@" --backend zipr --step-option zipr:"--zipr:seed $$" --structured_p1_canaries --step p1transform=on --step-option p1transform:"--floating_canary" --config_name $(basename $0 .sh|sed "s/cfar_//")
+fi
