4cf413cb518640fb15a9ad04410b09c518617a7d
jdh8d <jdh8d@git.zephyr-software.com>
2017-04-07 14:24:41 +0000

    Working version of eh-frame rewriting for non-pie executables.
    Former-commit-id: bebdc1ec856aa5f0972aa3fb68b7fd7d61c0f6e4

---
+++ c++_examples/derived3_throw.cpp
@@ -0,0 +1,57 @@
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+class Base
+{
+	public:
+		Base() : a(3) { } 
+
+	int a;
+};
+
+class Derived : public Base
+{
+	public:
+	Derived() { a=4;} 
+};
+
+int bar()
+{
+	if(getenv("THROW_CHAR")!=NULL)
+		throw Base();
+	else if(getenv("THROW_INT")!=NULL)
+		throw Derived();
+	else if(getenv("THROW_FLOAT")!=NULL)
+		throw float(3.14);
+	else 
+		return 0;
+}
+
+main()
+{
+
+	try	
+	{
+		int res= bar();
+		cout<<"No Throw!"<<endl;
+		return res;
+	}
+	catch(Base s)
+	{
+		cout<<"main caught Base with val=" << s.a << endl;
+	}
+	catch(Derived s)
+	{
+		cout<<"main caught Derived with val=" << s.a << endl;
+	}
+	catch(...)
+	{
+		cout<<"main caught unnamed"<<endl;
+	}
+
+	return 1;
+
+}
+
--- c++_examples/testit.sh
+++ c++_examples/testit.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+src_files="derived4_throw.cpp derived3_throw.cpp derived2_throw.cpp  derived_throw.cpp simple_throw.cpp throw.cpp "
+throws="THROW_INT THROW_CHAR THROW_FLOAT"
+
+#---------------
 
 compare()
 {
@@ -15,27 +19,49 @@ compare()
 	fi
 }
 
-src_files="simple_throw.cpp throw.cpp"
-options="-Os -O0 -O1 -O2 -O3"
-throws="THROW_INT THROW_CHAR THROW_FLOAT"
+doit()
+{
+	src=$1
+	shift
+	options=$*
 
-for src in $src_files
-do
-	echo "Trying $src"
+	echo "Trying $src with options: $options "
+	g++ -w $options $src 
+	rm -Rf peasoup_executable_direc*
+	EHIR_VERBOSE=1 $PSZ ./a.out ./xxx --step-option fill_in_indtargs:--split-eh-frame --step-option zipr:'--add-sections true' 	
 
-	for option in $options
+	compare
+
+	for throw in $throws
 	do
-		echo "With $option"
-		g++ $option $src
-		rm -Rf peasoup_executable_direc*
-		EHIR_VERBOSE=1 $PSZ ./a.out ./xxx --step-option fill_in_indtargs:--split-eh-frame --step-option zipr:'--add-sections true' 	
-
-		compare
-
-		for throw in $throws
-		do
-			compare $throw=1	
-		done
-		
+		compare $throw=1	
 	done
-done
+}
+
+
+doit_meta()
+{
+	src=$1
+	shift
+	option=$*
+
+			doit $src $option 
+			doit $src $option -fPIC 
+			doit $src $option -fPIC -fPIE 
+			doit $src $option -fPIC -fPIE -fomit-frame-pointer
+}
+
+main()
+{
+
+	for src in $src_files
+	do
+		doit_meta $src "-O0"
+		doit_meta $src "-O1"
+		doit_meta $src "-O2"
+		doit_meta $src "-O3"
+		doit_meta $src "-Os"
+	done
+}
+
+main
