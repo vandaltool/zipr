6fe8ef5072fef978bec530d8db848995256dd731
jdh8d <jdh8d@git.zephyr-software.com>
2017-06-07 15:21:53 +0000

    Former-commit-id: 99e5f81e3c7fe5567d08908bd6ca9b8cc227ed61

--- tools/selective_cfi/tests/test_spec.sh
+++ tools/selective_cfi/tests/test_spec.sh
@@ -1,37 +1,10 @@
 #!/bin/bash
 
-benchmarks="gcc"
+#benchmarks="gcc"
 # 447.dealII // broken build
-#benchmarks="
-#	400.perlbench
-#	401.bzip2
-#	403.gcc
-#	410.bwaves
-#	416.gamess
-#	429.mcf
-#	433.milc
-#	434.zeusmp
-#	435.gromacs
-#	436.cactusADM
-#	437.leslie3d
-#	444.namd
-#	445.gobmk
-#	450.soplex
-#	453.povray
-#	454.calculix
-#	456.hmmer
-#	458.sjeng
-#	459.GemsFDTD
-#	462.libquantum
-#	464.h264ref
-#	465.tonto
-#	470.lbm
-#	471.omnetpp
-#	473.astar
-#	481.wrf
-#	482.sphinx3
-#	483.xalancbmk
-#	"
+#benchmarks=" 400.perlbench 401.bzip2 403.gcc 410.bwaves 416.gamess 429.mcf 433.milc 434.zeusmp 435.gromacs 436.cactusADM 437.leslie3d 444.namd 445.gobmk 450.soplex 453.povray 454.calculix 456.hmmer 458.sjeng 459.GemsFDTD 462.libquantum 464.h264ref 465.tonto 470.lbm 471.omnetpp 473.astar 481.wrf 482.sphinx3 483.xalancbmk "
+
+benchmarks="400.perlbench 403.gcc 445.gobmk 453.povray 458.sjeng 464.h264ref 464.tonto 471.omnetpp 481.wrf 482.sphinx3 483.xalancbmk"
 number=1
 
 setup()
@@ -57,11 +30,17 @@ run_test()
 	config=$2
 	cd $SPEC
 	if [ ! -d result.$config_name ]; then
+		dropdb  $PGDATABASE 
+		createdb  $PGDATABASE 
+		$PEASOUP_HOME/tools/db/pdb_setup.sh
 		rm -Rf result/*
 		runspec  --action scrub --config $config $benchmarks
 		runspec  --action validate --config $config -n $number $benchmarks 
 		cp benchspec/CPU2006/*/exe/* result
 		mv result result.$config_name
+		dropdb  $PGDATABASE 
+		createdb  $PGDATABASE 
+		$PEASOUP_HOME/tools/db/pdb_setup.sh
 	fi
 
 }
@@ -69,8 +48,12 @@ run_test()
 get_size_result()
 {
 	bench=$1
-	size=$(stat --printf="%s" $bench)
-	echo -n $size
+	if [ -f $1 ]; then
+		size=$(stat --printf="%s" $bench)
+		echo -n $size
+	else
+		echo -n 0
+	fi
 }
 
 get_result()
@@ -145,11 +128,13 @@ main()
 	setup
 	run_test baseline $SPEC/config/ubuntu14.04lts-64bit.cfg
 	PSOPTS="--backend zipr"  run_test zipr     $SPEC/config/ubuntu14.04lts-64bit-withps.cfg
+	PSOPTS="--backend zipr --step-option zipr:--relax:on --step-option zipr:true"  run_test zipr-relax     $SPEC/config/ubuntu14.04lts-64bit-withps.cfg
 	PSOPTS="--backend zipr --step move_globals=on --step selective_cfi=on --step-option selective_cfi:--multimodule --step-option move_globals:--cfi  --step-option fix_calls:--fix-all" run_test mm-basic-cfi $SPEC/config/ubuntu14.04lts-64bit-withps.cfg
-
+	PSOPTS="--backend zipr --step move_globals=on --step selective_cfi=on --step-option selective_cfi:--multimodule --step-option move_globals:--cfi  --step-option fix_calls:--fix-all --step-option zipr:--relax:on --step-option zipr:true" run_test mm-basic-cfi-relax $SPEC/config/ubuntu14.04lts-64bit-withps.cfg
 	PSOPTS="--backend zipr --step move_globals=on --step selective_cfi=on --step-option selective_cfi:--multimodule --step-option move_globals:--cfi  --step-option fix_calls:--fix-all --step-option selective_cfi:--color" run_test mm-color-cfi $SPEC/config/ubuntu14.04lts-64bit-withps.cfg
+	PSOPTS="--backend zipr --step move_globals=on --step selective_cfi=on --step-option selective_cfi:--multimodule --step-option move_globals:--cfi  --step-option fix_calls:--fix-all --step-option selective_cfi:--color --step-option zipr:--relax:on --step-option zipr:true" run_test mm-color-cfi-relax $SPEC/config/ubuntu14.04lts-64bit-withps.cfg
 
-	get_raw_results baseline zipr mm-basic-cfi mm-color-cfi
+	get_raw_results baseline zipr mm-basic-cfi mm-color-cfi zipr-relax mm-basic-cfi-relax mm-color-cfi-relax 
 
 }
 
