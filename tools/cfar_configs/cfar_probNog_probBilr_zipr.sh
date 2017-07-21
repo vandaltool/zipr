8a846b91aaad7a6daf1758f8ec265fa74b411839
jdh8d <jdh8d@git.zephyr-software.com>
2017-07-21 15:35:30 +0000

    actually added the flag for probNog -- wasn't actually doing it
    Former-commit-id: acb78755f53658da57dd436c308d67f237a929f4

--- tools/cfar_configs/cfar_probNog_probBilr_zipr.sh
+++ tools/cfar_configs/cfar_probNog_probBilr_zipr.sh
@@ -2,5 +2,5 @@
 
 source $(dirname $0)/../ps_wrapper.source $0
 
-$PEASOUP_HOME/tools/cfar.sh "$@" --backend zipr --config_name $(basename $0 .sh|sed "s/cfar_//") --step move_globals=on --step-option move_globals:-d --step-option move_globals:.interp
+$PEASOUP_HOME/tools/cfar.sh "$@" --backend zipr --config_name $(basename $0 .sh|sed "s/cfar_//") --step move_globals=on --step-option move_globals:-d --step-option move_globals:.interp --step-option zipr:"--large_only:nog_on true"
 
