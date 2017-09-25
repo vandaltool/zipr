693cd1192757b52ef42f1ffb4695ae4f6fca43c4
jdh8d <jdh8d@git.zephyr-software.com>
2017-09-25 15:36:35 +0000

    Better EH verbose messages for zipr, stack stamping+EH support, a new cfar config (NOG+SS vrs NogOF+SS), as well as an IR refinement technique to do recursive-decent disassembly when a landing pad is not part of a function.
    Former-commit-id: 7fc665e949b9b78dce836b8d181faa509e2e7e8d

--- cpp-examples/testit.sh
+++ cpp-examples/testit.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-src_files="derived4_throw.cpp derived3_throw.cpp derived2_throw.cpp  derived_throw.cpp simple_throw.cpp throw.cpp "
+src_files=" simple_throw.cpp throw.cpp derived_throw.cpp derived2_throw.cpp  derived3_throw.cpp derived4_throw.cpp "
 throws="THROW_INT THROW_CHAR THROW_FLOAT"
 
 #---------------
 
 compare()
 {
+
+	rm -f good.txt xform.txt 
+
 	env $1 ./a.out > good.txt 2>&1 
+	exit1=$?
 	env $1 ./xxx > xform.txt 2>&1 
+	exit2=$?
 
 	cmp good.txt xform.txt > /dev/null 2>&1
 
-	if [ $? != 0 ]; then
+	if [[ $? != 0 ]] || [[ $exit1 != $exit2 ]]; then
 		echo "Failed test: $1"; 
 		diff good.txt xform.txt
 		exit 2
@@ -27,7 +32,7 @@ doit()
 
 	echo  "------------------------------------------------------"
 	echo "Trying $src with options: $options"
-	echo "And psflags=$psflags "
+	echo "And psflags=$psopts "
 	g++ -w $options $src 
 	rm -Rf peasoup_executable_direc*
 	EHIR_VERBOSE=1 $PSZ ./a.out ./xxx --step-option fill_in_indtargs:--split-eh-frame --step-option zipr:'--add-sections true' 	$psopts
@@ -38,6 +43,8 @@ doit()
 	do
 		compare $throw=1	
 	done
+	echo "Passed test!"
+	echo  "------------------------------------------------------"
 }
 
 
@@ -61,8 +68,9 @@ main()
 	do
 		for option in -O0 -O1 -O2 -O3 -Os -Og
 		do
-			doit_meta $src "$option" ""
-			doit_meta $src "$option" "--step p1transform=on"
+			#doit_meta $src "$option" ""
+			#doit_meta $src "$option" "--step p1transform=on"
+			doit_meta $src "$option" "--step stack_stamp=on"
 		done
 	done
 }
