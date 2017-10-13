f61641eba953199a7e3656fff5672f7fb203bf91
mc2zk <mc2zk@git.zephyr-software.com>
2017-10-13 18:40:56 +0000

    Correcting postgres version number from 9.1 to 9.3
    Former-commit-id: 12fb3054c429c0656f1ba5704e9a2c251047c00d

--- postgres_setup.sh
+++ postgres_setup.sh
@@ -5,8 +5,8 @@ printf ":5432::$USER:1qaz2wsx\nlocalhost:5432:*:$USER:1qaz2wsx\n127.0.0.1:5432:*
 chmod og-rw $HOME/.pgpass
 
 #Allow remote access to PostGres
-sudo su -c "printf \"\nhost \t all \t all \t 127.0.0.1/16 \t md5\nhostssl  all \t all \t 127.0.0.1/16 \t md5\n\" >> /etc/postgresql/9.1/main/pg_hba.conf"
-printf "\nlisten_addresses = '*'\n" | sudo tee -a /etc/postgresql/9.1/main/postgresql.conf > /dev/null
+sudo su -c "printf \"\nhost \t all \t all \t 127.0.0.1/16 \t md5\nhostssl  all \t all \t 127.0.0.1/16 \t md5\n\" >> /etc/postgresql/9.3/main/pg_hba.conf"
+printf "\nlisten_addresses = '*'\n" | sudo tee -a /etc/postgresql/9.3/main/postgresql.conf > /dev/null
 
 #Restart PostGres
 sudo service postgresql restart
