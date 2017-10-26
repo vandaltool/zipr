dc4369cacca26419089801050a6f52179160e866
jdh8d <jdh8d@git.zephyr-software.com>
2017-10-26 20:57:01 +0000

    Updates to make double helix preloads and unm/asu  crispr preload compatible.  added mvp settings to vgt, updated pntrasnform drive to not spri validate unless requested.  ubuntu linker patch updated to fix linker bug i found related to non-contiguous libraries. vgt testing harness patch for output clarity.
    Former-commit-id: 888612f4931a773076b9e6ff5b7bb1dec42f49ad

---
+++ tools/cfar_configs/cfar_structNoc_structP1Canaries_structNog_probHeaprand_zipr.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+source $(dirname $0)/../ps_wrapper.source $0
+
+$PEASOUP_HOME/tools/cfar.sh "$@" --backend zipr --step diehard=on --step-option zipr:"--zipr:seed $$" --structured_noc --step-option zipr:"--large_only:on true" --structured_p1_canaries  --step p1transform=on --config_name $(basename $0 .sh|sed "s/cfar_//") --step move_globals=on --structured_nog --step-option zipr:"--large_only:nog_on true" --step-option move_globals:-d --step-option move_globals:.interp
