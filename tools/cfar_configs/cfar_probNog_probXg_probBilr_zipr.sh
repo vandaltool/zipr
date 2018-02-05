3f6a9b2b4747d94a14671ccd75a89a28dc367511
clc5q <clc5q@git.zephyr-software.com>
2018-02-05 16:21:54 +0000

    Remove scripts woth older probXg naming convention.
    Former-commit-id: 02b3769192d99971bfe96ba431aa349010193049

--- tools/cfar_configs/cfar_probNog_probXg_probBilr_zipr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-source $(dirname $0)/../ps_wrapper.source $0
-
-$PEASOUP_HOME/tools/cfar.sh "$@" --backend zipr --config_name $(basename $0 .sh|sed "s/cfar_//") --step move_globals=on --step xor_globals=on --step-option move_globals:"--aggressive" --step-option move_globals:-d --step-option move_globals:.interp --step-option zipr:"--large_only:nog_on true"
-
