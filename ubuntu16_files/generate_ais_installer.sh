10ff1c845bbb0b4ed221ba7702ede4fdca72f66d
jdh8d <jdh8d@git.zephyr-software.com>
2018-02-09 20:27:08 +0000

    fixed so that build isn't broken after exporting package
    Former-commit-id: dad711e5de498d6eef68edb0b9d29d3682932087

--- ubuntu16_files/generate_ais_installer.sh
+++ ubuntu16_files/generate_ais_installer.sh
@@ -4,5 +4,9 @@ cd $PEASOUP_UMBRELLA_DIR
 
 ./regen_install.sh ida ps zipr ubuntu16 stars
 cp -r installed zipr_toolchain
+
+echo "Creating zipr_toolchain.tgz" 
 tar czf ubuntu16_files/zipr_toolchain.tgz zipr_toolchain
 rm -Rf zipr_toolchain
+
+$PEDI_HOME/pedi -c -m manifest.txt
