4b25a16150c04edc461381218bc1ad73074f12cb
jdh8d <jdh8d@git.zephyr-software.com>
2018-02-16 18:21:22 +0000

    Former-commit-id: 8c5a2b2bf03c5a079ddb3cc5c6334f7bf9299cc7

---
+++ libIRDB/include/decode/decode.hpp
@@ -0,0 +1,20 @@
+#ifndef libdecode_decode_hpp
+#define libdecode_decode_hpp
+
+
+#include <decode/decode_meta.hpp>
+#include <decode/operand_meta.hpp>
+
+
+namespace libIRDB
+{
+using namespace std;
+using namespace libIRDB;
+
+
+typedef DecodedInstructionMeta_t DecodedInstruction_t;
+typedef DecodedOperandMeta_t DecodedOperand_t;
+typedef DecodedOperandMetaVector_t DecodedOperandVector_t;
+}
+
+#endif
