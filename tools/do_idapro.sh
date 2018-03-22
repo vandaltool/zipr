09981764da13f2dbc0e203a408babb9caa724cbd
jdh8d <jdh8d@git.zephyr-software.com>
2018-03-22 12:49:09 +0000

    STARS interface scripts cleanup
    Former-commit-id: ccb0c2e34cb24a4e46ae2c6838e8bbfbaf74ab4f

--- tools/do_idapro.sh
+++ tools/do_idapro.sh
@@ -22,22 +22,7 @@ analyze_file()
 
 	if [ ! $rc -eq 0 ]; then
 		echo "STARS (Local) Analyzing $file target is: $target_name"
-
-		case "$IDAROOT" in
-    		*idapro5* )
-        		echo "IDA 5.* detected."
-        		$SMPSA_HOME/SMP-analyze.sh $file $stars_options
-        		;;
-    		*idapro6*|*idaproCur*)
-        		# only works on IDA 6.0+
-        		echo "IDA 6.* detected."
-        		#screen -D -L -ln -m -a -T xterm sh -x $SMPSA_HOME/SMP-analyze.sh $file $stars_options
-        		TVHEADLESS=1 $SMPSA_HOME/SMP-analyze.sh $file $stars_options
-        		;;
-		*)
-			echo Cannot detect IDA version: $IDAROOT
-			exit 1
-		esac
+        	TVHEADLESS=1 $SMPSA_HOME/SMP-analyze.sh $file $stars_options
 	fi
 
 	lines=`cat $file.annot | wc -l`
@@ -58,7 +43,7 @@ analyze_file()
 
 }
 
-if [ ! -z $1 ]; then
+if [[ ! -z $1 ]]; then
 	TARGET_NAME=$1	
 else
 	TARGET_NAME=a.ncexe
@@ -68,7 +53,7 @@ shift
 
 analyze_file a.ncexe $TARGET_NAME $@
 
-if [ -d shared_objects ]; then
+if [[ -d shared_objects ]]; then
 	cd shared_objects
 
 	for i in `cat ../shared_libs`; do
