912518538b9d30cd1a736eb70fca9cbc3c7f3105
Anh <an7s@virginia.edu>
2018-06-13 20:30:58 +0000

    Forgot shift in default case
    Former-commit-id: 9e9d93f7df0d65d71128fbbce66e5f2498547d3f

--- plugins_install/diehard.sh
+++ plugins_install/diehard.sh
@@ -6,5 +6,40 @@ if [ ! -f $CFAR_HOME/DieHard/src/libdiehard.so ]; then
 fi
 
 $PEASOUP_HOME/tools/update_env_var.sh DO_DIEHARD 1
-cp $CFAR_HOME/DieHard/src/libdiehard.so libheaprand.so
+
+
+seq=""
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+	--structured_heap)
+		seq="$2"
+		shift
+	;;
+	*)
+		shift;
+	;;
+esac
+done
+
+if [ -z "$seq" ]; then
+	cp $CFAR_HOME/DieHard/src/libdiehard.so libheaprand.so
+else
+	if [ ! -f $CFAR_HOME/DieHard/src/libdiehard-4k.so ]; then
+		echo "ERROR: DieHard library not built/found" | tee warning.txt
+		exit 1
+	fi
+
+	if [ ! -f $CFAR_HOME/DieHard/src/libdiehard-32k.so ]; then
+		echo "ERROR: DieHard library not built/found" | tee warning.txt
+		exit 1
+	fi
+
+	if [ $(expr ${seq} % 2) = 0 ]; then
+		cp $CFAR_HOME/DieHard/src/libdiehard-32k.so libheaprand.so
+	else
+		cp $CFAR_HOME/DieHard/src/libdiehard-4k.so libheaprand.so
+	fi
+fi
 
