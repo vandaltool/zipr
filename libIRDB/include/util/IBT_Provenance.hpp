b31985d40794484569d9d9a58b06542b7935e181
Matthew McGill <mm8bx@mega-techx41.maas>
2018-09-04 16:53:08 +0000

    Speedup for STARS changes required to support unpinned IBTS
    Former-commit-id: 44779ccb49b255f457aae62a66afcbf4fbbaa016

---
+++ libIRDB/include/util/IBT_Provenance.hpp
@@ -0,0 +1,43 @@
+#ifndef IBT_Provenance_h
+#define IBT_Provenance_h
+
+#include <bitset>
+
+class IBT_Provenance_t
+{
+	private:
+        enum class IB_Type { IndJmp = 0, IndCall = 1, Ret = 2 };
+	typedef std::map<const Instruction_t*, std::bitset<3>> ProvMap_t;
+
+	public:
+	IBT_Provenance_t(const FileIR_t* f=NULL) {Init(); if(f) AddFile(f);}
+        virtual ~IBT_Provenance_t() {;}
+	virtual void AddFile(const FileIR_t* );
+        
+        bool IsInsnRetTarg(const Instruction_t* i) const
+        {
+            return (prov_map.at(i)).test((size_t) IB_Type::Ret);
+        }
+
+	bool IsInsnIndJmpTarg(const Instruction_t* i) const
+        {
+            return (prov_map.at(i)).test((size_t) IB_Type::IndJmp);
+        }
+        
+        bool IsInsnIndCallTarg(const Instruction_t* i) const
+        {
+            return (prov_map.at(i)).test((size_t) IB_Type::IndCall);
+        }
+
+	protected:
+	virtual void Init() {};
+
+	private:
+
+	virtual void AddProvs(const Instruction_t* before, const InstructionSet_t& after);
+
+	ProvMap_t prov_map;
+};
+
+#endif
+
---
+++ libIRDB/src/util/IBT_Provenance.cpp
@@ -0,0 +1,58 @@
+#include <map>
+#include <bitset>
+#include <libIRDB-core.hpp>
+#include <libIRDB-util.hpp>
+#include <utils.hpp>
+
+using namespace libIRDB;
+using namespace std;
+
+
+
+void IBT_Provenance_t::AddProvs(const Instruction_t* before, const InstructionSet_t& afterset)
+{
+        // Determine type of IB
+        const auto IndBranchAsm=DecodedInstruction_t(before);
+        
+        bool isIndJmp = IndBranchAsm.isUnconditionalBranch() && !IndBranchAsm.getOperand(0).isConstant();
+        bool isIndCall = IndBranchAsm.isCall() && !IndBranchAsm.getOperand(0).isConstant();
+        bool isRet = IndBranchAsm.isReturn();
+        
+        // Set the provenance info of targets depending on the type of IB
+        IB_Type this_IB_type;
+        
+        if(isIndJmp)
+        {
+                this_IB_type = IB_Type::IndJmp;
+        }
+        else if(isIndCall)
+        {
+                this_IB_type = IB_Type::IndCall;
+        }
+        else if(isRet)
+        {
+                this_IB_type = IB_Type::Ret;
+        }
+        else
+        {
+                assert(0);
+        }
+        
+	for(auto insn : afterset)
+	{
+                prov_map[insn].set((size_t) this_IB_type);
+	}
+}
+
+void IBT_Provenance_t::AddFile(const FileIR_t* firp2)
+{
+	FileIR_t* firp=(FileIR_t*)firp2; // discarding const qualifier because we know we won't change the set
+        firp->AssembleRegistry(); // Takes time but I'm paranoid
+	for(auto insn : firp->GetInstructions())
+	{
+		// If insn is an IB, add the type of IB to the targets' provenance info
+	        if(insn->GetIBTargets())
+        	      	AddProvs(insn, *insn->GetIBTargets());		
+	}
+}
+
