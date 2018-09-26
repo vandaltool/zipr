4f85cf24f825bdf9e89be9a2ac301cd29e28ed55
Jason Hiser <jdhiser@gmail.com>
2018-09-26 14:22:02 +0000

    added --aggressive to nog
    Former-commit-id: 2c4fda6e63b786e557d6c3ce2790df7828af86f6

--- tools/cfar_configs/cfar_probBilr_probP1_probNog_probHeaprand_zipr.sh
+++ tools/cfar_configs/cfar_probBilr_probP1_probNog_probHeaprand_zipr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 source $(dirname $0)/../ps_wrapper.source $0
 
-$PEASOUP_HOME/tools/cfar.sh "$@" --backend zipr --step diehard=on --step p1transform=on --config_name $(basename $0 .sh|sed "s/cfar_//") --step move_globals=on --step-option zipr:"--large_only:nog_on true" --step-option move_globals:-d --step-option move_globals:.interp
+$PEASOUP_HOME/tools/cfar.sh "$@" --backend zipr --step diehard=on --step p1transform=on --config_name $(basename $0 .sh|sed "s/cfar_//") --step move_globals=on --step-option zipr:"--large_only:nog_on true" --step-option move_globals:-d --step-option move_globals:.interp --step-option move_globals:--aggressive
 
