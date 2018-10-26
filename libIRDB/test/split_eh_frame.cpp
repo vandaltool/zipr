524151c8ff3ab89f7a479fc2c50217d09ac9cd99
Jason Hiser <jdh8d@virginia.edu>
2018-10-26 17:17:24 -0400

    performance optimizations for building the eh-frame ir.  mostly avoiding copying structures that dont need to be copied, but also caching/re-using a few results from libehp
    Former-commit-id: d919cd681a4c1f8ba1464c50f8c6408da899e377

--- libIRDB/test/split_eh_frame.cpp
+++ libIRDB/test/split_eh_frame.cpp
@@ -205,9 +205,9 @@ void split_eh_frame_impl_t<ptrsize>::build_ir() const
 			EhProgram_t* getProgram() const { return pgm; }
 			uint64_t getPersonality() const { return personality; }
 		private:
-			uint64_t hashPgm(const EhProgram_t* vec)
+			static uint64_t hashPgm(const EhProgram_t* vec)
 			{
-				assert(pgm);
+				assert(vec);
 				auto seed = vec->GetCIEProgram().size() + vec->GetFDEProgram().size();
 				auto hash_fn=hash<string>();
 
@@ -279,7 +279,23 @@ void split_eh_frame_impl_t<ptrsize>::build_ir() const
 			{ return fde->getStartAddress() <= find_addr && find_addr < fde->getEndAddress(); };
 		const auto fie_ptr_it=find_if ( ALLOF(*fdes), finder);
 		*/
-		const auto fie_ptr=eh_frame_parser->findFDE(find_addr);
+		static auto fie_ptr=shared_ptr<FDEContents_t>(); 
+		static auto cie_instructions=shared_ptr<EHProgramInstructionVector_t>();
+		static auto fde_instructions=shared_ptr<EHProgramInstructionVector_t>();
+
+		if (fie_ptr && fie_ptr->getStartAddress() <= find_addr && find_addr < fie_ptr->getEndAddress())
+		{
+			// fie_ptr already points at right thing, do nothing
+		}
+		else 
+		{
+			fie_ptr=eh_frame_parser->findFDE(find_addr);
+			if(fie_ptr)
+			{
+				cie_instructions=fie_ptr->getCIE().getProgram().getInstructions();
+				fde_instructions=fie_ptr->getProgram().getInstructions();
+			}
+		}
 
 		if(fie_ptr != nullptr ) 
 		{
@@ -300,11 +316,10 @@ void split_eh_frame_impl_t<ptrsize>::build_ir() const
 			const auto personality=fie_ptr->getCIE().getPersonality(); 
 			const auto insn_addr=insn->GetAddress()->GetVirtualOffset();
 
-			auto import_pgm = [&](EhProgramListing_t& out_pgm_final, const EHProgram_t& in_pgm) -> void
+			auto import_pgm = [&](EhProgramListing_t& out_pgm_final, const shared_ptr<EHProgramInstructionVector_t> &in_pgm_instructions_ptr) -> void
 			{
 				auto out_pgm=vector<shared_ptr<EHProgramInstruction_t> >();
 				auto cur_addr=fde_addr;
-				const auto in_pgm_instructions_ptr=in_pgm.getInstructions();
 				const auto in_pgm_instructions=*in_pgm_instructions_ptr;
 				for(const auto & insn_ptr : in_pgm_instructions)
 				{
@@ -360,8 +375,8 @@ void split_eh_frame_impl_t<ptrsize>::build_ir() const
 			// build an eh program on the stack;
 
 			auto ehpgm=EhProgram_t(BaseObj_t::NOT_IN_DATABASE,caf,daf,return_reg, ptrsize);
-			import_pgm(ehpgm.GetCIEProgram(), fie_ptr->getCIE().getProgram());
-			import_pgm(ehpgm.GetFDEProgram(), fie_ptr->getProgram());
+			import_pgm(ehpgm.GetCIEProgram(), cie_instructions);
+			import_pgm(ehpgm.GetFDEProgram(), fde_instructions);
 
 
 			if(getenv("EHIR_VERBOSE")!=NULL)
