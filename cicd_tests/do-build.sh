3680f687b2c1dc4ad71e3f8c245ebde4b0765756
Jason Hiser <jdh8d@virginia.edu>
2018-11-09 11:10:19 -0800

    updated to try to set build results as a gitlab -artifact-

--- cicd_tests/do-build.sh
+++ cicd_tests/do-build.sh
@@ -3,23 +3,33 @@
 set -e
 set -x
 
-# gather info for debugging later, probably not necessary 
-pwd
-hostname
-whoami
-env|grep CICD
+main()
+{
 
-# puts peasoup_umbrella (and all submodules) in CICD_MODULE_WORK_DIR
-cicd_setup_module_dependency allnp/peasoup_umbrella.git
+	# gather info for debugging later, probably not necessary 
+	pwd
+	hostname
+	whoami
+	env|grep CICD
+
+	local orig_dir=$(pwd)
+
+	# puts peasoup_umbrella (and all submodules) in CICD_MODULE_WORK_DIR
+	cicd_setup_module_dependency allnp/peasoup_umbrella.git
 
 
-# puts the version of zipr to test in peasoup_umbrella/zipr.
-cicd_put_module_in_tree peasoup_umbrella/zipr
+	# puts the version of zipr to test in peasoup_umbrella/zipr.
+	cicd_put_module_in_tree peasoup_umbrella/zipr
 
-# Build/run $PSZ, test result
-cd $CICD_MODULE_WORK_DIR/peasoup_umbrella
-source set_env_vars
-sudo ./get-peasoup-packages.sh all
-./build-all.sh
-dropdb $PGDATABASE 2>/dev/null || true ; ./postgres_setup.sh
+	# Build/run $PSZ, test result
+	cd $CICD_MODULE_WORK_DIR/peasoup_umbrella
+	source set_env_vars
+	sudo ./get-peasoup-packages.sh all
+	./build-all.sh
+	dropdb $PGDATABASE 2>/dev/null || true ; ./postgres_setup.sh
 
+	cd $orig_dir
+	cp $CICD_MODULE_WORK_DIR/install ps-install-$OS
+}
+
+main "$@"
