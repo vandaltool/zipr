bb85f2a03940b67956f4c48586ef2418de3edf6c
Jason Hiser <jdhiser@gmail.com>
2018-11-12 17:01:55 +0000

    initial tests of p1 in sec-trans
    Former-commit-id: 979387cf75b4b6c7ab6e69c924d98d41eb32a71f

---
+++ cicd_tests/do-build.sh
@@ -0,0 +1,34 @@
+#/bin/bash 
+
+set -e
+set -x
+
+main()
+{
+
+	# gather info for debugging later, probably not necessary 
+	pwd
+	hostname
+	whoami
+	env|grep "^CICD"
+
+	local orig_dir=$(pwd)
+
+	# puts peasoup_umbrella (and all submodules) in CICD_MODULE_WORK_DIR
+	cicd_setup_module_dependency allnp/peasoup_umbrella.git
+
+
+	# puts the version of security_transforms to test in peasoup_umbrella/security_transforms
+	cicd_put_module_in_tree peasoup_umbrella/security_transforms
+
+	# Build/run $PSZ, test result
+	cd $CICD_MODULE_WORK_DIR/peasoup_umbrella
+	source set_env_vars
+	sudo ./get-peasoup-packages.sh all
+	./build-all.sh
+	dropdb $PGDATABASE 2>/dev/null || true ; ./postgres_setup.sh
+
+	cd $orig_dir
+}
+
+main "$@"
