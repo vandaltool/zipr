a543a65c289c6e409a1950590e01dc8571e585f4
Jason Hiser <jdhiser@gmail.com>
2018-11-13 19:35:04 +0000

    added orig to test of basic pgms to verify testing harness
    Former-commit-id: 9b127ee86e698270a1320412e081c75a2563ff4d

--- cicd_testing/basic-pgms-rida.sh
+++ cicd_testing/basic-pgms-rida.sh
@@ -5,5 +5,7 @@ set -x
 cd /tmp/peasoup_test
 source set_env_vars
 cd $CICD_TO_TEST_DIR/peasoup_examples/tests
-cd $PEASOUP_HOME/tests; make clean; ./test_cmds.sh rida
+cd $PEASOUP_HOME/tests
+make clean; ./test_cmds.sh orig
+make clean; ./test_cmds.sh rida
 
