da58829ecc5fa674c3354d4e3c6c574ea0b7bff6
Jason Hiser <jdhiser@gmail.com>
2018-11-29 16:25:57 +0000

    avoid excess work
    Former-commit-id: 7e1fb1d17c0e3f071ff58cafbecb1831e8772ef9

--- libIRDB/src/util/IBT_Provenance.cpp
+++ libIRDB/src/util/IBT_Provenance.cpp
@@ -13,10 +13,9 @@ void IBTProvenance_t::AddProvs(const Instruction_t* before, const InstructionSet
 {
         // Determine type of IB
         const auto IndBranchAsm=DecodedInstruction_t(before);
-        
-        bool isIndJmp = IndBranchAsm.isUnconditionalBranch() && !IndBranchAsm.getOperand(0).isConstant();
-        bool isIndCall = IndBranchAsm.isCall() && !IndBranchAsm.getOperand(0).isConstant();
-        bool isRet = IndBranchAsm.isReturn();
+        const auto isIndJmp = IndBranchAsm.isUnconditionalBranch() && !IndBranchAsm.getOperand(0).isConstant();
+        const auto isIndCall = IndBranchAsm.isCall() && !IndBranchAsm.getOperand(0).isConstant();
+        const auto isRet = IndBranchAsm.isReturn();
         
         // Set the provenance info of targets depending on the type of IB 
 	for(auto insn : afterset)
@@ -42,13 +41,32 @@ void IBTProvenance_t::AddProvs(const Instruction_t* before, const InstructionSet
 
 void IBTProvenance_t::AddFile(const FileIR_t* firp2)
 {
+	using TypeTuple_t=tuple<bool,bool,bool>;
+	using Seen_t = pair<TypeTuple_t,ICFS_t*>;
+	using SeenSet_t=set<Seen_t>;
+	auto already_seen=SeenSet_t();
 	FileIR_t* firp=(FileIR_t*)firp2; // discarding const qualifier because we know we won't change the set
         firp->AssembleRegistry(); // Takes time but I'm paranoid
 	for(auto insn : firp->GetInstructions())
 	{
+		const auto &ibTargets=insn->GetIBTargets();
+		if(!ibTargets)
+			continue;
+
+		const auto IndBranchAsm=DecodedInstruction_t(insn);
+		const auto isIndJmp = IndBranchAsm.isUnconditionalBranch() && !IndBranchAsm.getOperand(0).isConstant();
+		const auto isIndCall = IndBranchAsm.isCall() && !IndBranchAsm.getOperand(0).isConstant();
+		const auto isRet = IndBranchAsm.isReturn();
+		const auto the_tup=TypeTuple_t(isIndJmp,isIndCall,isRet);
+		const auto to_be_seen=Seen_t(the_tup,ibTargets);
+		
+
 		// If insn is an IB, add the type of IB to the targets' provenance info
-	        if(insn->GetIBTargets())
-        	      	AddProvs(insn, *insn->GetIBTargets());		
+
+		const auto find_it=already_seen.find(to_be_seen);
+	        if(find_it==already_seen.end()) 
+        	      	AddProvs(insn, *ibTargets);
+		already_seen.insert(to_be_seen);
 	}
 }
 
