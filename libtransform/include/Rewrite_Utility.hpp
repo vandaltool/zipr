af94ef04fde7cfbf882448aee23b69454bdcc211
Jason Hiser <jdhiser@gmail.com>
2018-12-22 20:41:45 -0500

    removed commented code
    Former-commit-id: 49b68c5311222daf510a36ca098dbb792457d17c

--- libtransform/include/Rewrite_Utility.hpp
+++ libtransform/include/Rewrite_Utility.hpp
@@ -24,16 +24,6 @@ using namespace libIRDB;
 using namespace std;
 
 namespace IRDBUtility {
-// make sure these match values in detector_handlers.h in the strata library
-/*enum mitigation_policy 
-{
-	P_NONE=0, 
-	P_CONTINUE_EXECUTION, 
-	P_CONTROLLED_EXIT, 
-	P_CONTINUE_EXECUTION_SATURATING_ARITHMETIC, 
-	P_CONTINUE_EXECUTION_WARNONLY
-};*/
-
 
 //The "first" instruction will have its contents replaced and a duplicate of "first" will be in the follow of first. 
 //This duplicate is returned since the user already has a pointer to first. 
@@ -63,7 +53,6 @@ void copyInstruction(Instruction_t* src, Instruction_t* dest);
 Instruction_t* allocateNewInstruction(FileIR_t* virp, db_id_t p_fileID,Function_t* func);
 Instruction_t* allocateNewInstruction(FileIR_t* virp, Instruction_t *template_instr);
 void setInstructionAssembly(FileIR_t* virp,Instruction_t *p_instr, string p_assembly, Instruction_t *p_fallThrough, Instruction_t *p_target);
-//Instruction_t* getHandlerCode(FileIR_t* virp, Instruction_t* fallthrough, mitigation_policy policy );
 
 
 }
