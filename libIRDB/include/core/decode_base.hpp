9063ab6e56d096c78621762598ebae8c12137830
Jason Hiser <jdhiser@gmail.com>
2018-12-26 15:50:04 +0000

    fixed memory leak from missing destructor
    Former-commit-id: 8a9ace85f86675e2a823a762d1d2732d41bdf0c4

--- libIRDB/include/core/decode_base.hpp
+++ libIRDB/include/core/decode_base.hpp
@@ -13,11 +13,12 @@ using namespace std;
 
 class DecodedOperandCapstone_t;
 class DecodedInstructionDispatcher_t;
-typedef std::vector<shared_ptr<DecodedOperandCapstone_t> > DecodedOperandCapstoneVector_t;
+typedef vector<shared_ptr<DecodedOperandCapstone_t> > DecodedOperandCapstoneVector_t;
 
 class DecodedInstructionCapstone_t
 {
 	public:
+		virtual ~DecodedInstructionCapstone_t(){}
 		virtual string getDisassembly() const =0;
 		virtual bool valid() const =0;
 		virtual uint32_t length() const =0;
@@ -40,14 +41,14 @@ class DecodedInstructionCapstone_t
 
 		// 0-based.  first operand is numbered 0.
 		virtual bool hasOperand(const int op_num) const =0;
-		virtual std::shared_ptr<DecodedOperandCapstone_t> getOperand(const int op_num) const =0;
+		virtual shared_ptr<DecodedOperandCapstone_t> getOperand(const int op_num) const =0;
 		virtual DecodedOperandCapstoneVector_t getOperands() const =0;
 
 	private:
 
-		static std::shared_ptr<DecodedInstructionCapstone_t> factory(const libIRDB::Instruction_t* i);
-		static std::shared_ptr<DecodedInstructionCapstone_t> factory(const virtual_offset_t start_addr, const void *data, uint32_t max_len);
-		static std::shared_ptr<DecodedInstructionCapstone_t> factory(const virtual_offset_t start_addr, const void *data, const void* endptr);
+		static unique_ptr<DecodedInstructionCapstone_t> factory(const libIRDB::Instruction_t* i);
+		static unique_ptr<DecodedInstructionCapstone_t> factory(const virtual_offset_t start_addr, const void *data, uint32_t max_len);
+		static unique_ptr<DecodedInstructionCapstone_t> factory(const virtual_offset_t start_addr, const void *data, const void* endptr);
 
 		friend class DecodedInstructionDispatcher_t;
 
