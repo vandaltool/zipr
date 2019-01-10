d87222cdc20b538deb517b39a77fe64c927900b4
Anh <zenpoems@gmail.com>
2019-01-10 13:22:52 -0800

    Add zuntracer config
    Former-commit-id: 8f29588095ab526684760d0c32c9181fe92cdd0e

--- tests/test_cmds.sh
+++ tests/test_cmds.sh
@@ -69,6 +69,9 @@ do_tests()
 				zafl0)
 					ZAFL_LIMIT_END=0 zafl.sh $progpath $protected --tempdir $temp_dir > test_${prog}.ps.log 2>&1
 				;;
+				zafl_untracer)
+					zafl.sh $progpath $protected --rida --tempdir $temp_dir --untracer > test_${prog}.ps.log 2>&1
+				;;
 				zafl_rida)
 					zafl.sh $progpath $protected --rida --tempdir $temp_dir > test_${prog}.ps.log 2>&1
 				;;
