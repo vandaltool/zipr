0e4891c8ea9ca77a02ac17cb828e22fb1497c39d
Anh <zenpoems@gmail.com>
2019-01-11 06:49:16 -0800

    Add fix_calls test
    Former-commit-id: c1497aabdfd754c910c14922a735c1603836a180

---
+++ cicd_testing/basic-pgms-fixcalls.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+set -x
+
+cd /tmp/peasoup_test
+source set_env_vars
+cd $CICD_TO_TEST_DIR/peasoup_examples/tests
+cd $PEASOUP_HOME/tests
+make clean; ./test_cmds.sh -c fix_calls -l
+
