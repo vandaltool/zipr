1eee75bd841fd8d4875fda274857a053dec9c460
Anh Nguyen <zenpoems@gmail.com>
2019-01-12 12:55:15 -0800

    Modified self-validation test
    Former-commit-id: d187761fd14df54a8593076508ff0b12950fab6c

--- cicd_testing/basic-pgms-fixcalls.sh
+++ cicd_testing/basic-pgms-fixcalls.sh
@@ -6,5 +6,7 @@ cd /tmp/peasoup_test
 source set_env_vars
 cd $CICD_TO_TEST_DIR/peasoup_examples/tests
 cd $PEASOUP_HOME/tests
-make clean; ./test_cmds.sh -c fix_calls -l
+make clean; 
+./test_cmds.sh -c fix_calls_rida -l
+./test_cmds.sh -c fix_calls_ida -l
 
