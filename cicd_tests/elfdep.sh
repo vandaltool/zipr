49a89768d0db53e61e4ea08e26043e672f1eb55b
Jason Hiser <jdhiser@gmail.com>
2019-02-01 12:52:43 -0500

    adjusted test to reflect new path
    Former-commit-id: fe6c68a5d0af24d53fd8c49339a9a5e582c16c45

--- cicd_tests/elfdep.sh
+++ cicd_tests/elfdep.sh
@@ -5,5 +5,5 @@ set -x
 cd $CICD_MODULE_WORK_DIR/peasoup_umbrella
 source set_env_vars
 
-cd $SECURITY_TRANSFORMS_HOME/libElfDep/test/
+cd $SECURITY_TRANSFORMS_HOME/libIRDB-elfdep/test/
 ./test-elfdep.sh
