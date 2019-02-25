68f550e465f2a77441097078e0b927d227e62e14
Jason Hiser <jdhiser@gmail.com>
2019-02-25 16:32:59 +0000

    added no-pie and no-PIC flags for newer machines that default to something othr than what zipr actually wants

--- peasoup_examples/tools/eh_frame_tools/eh_to_bin.sh
+++ peasoup_examples/tools/eh_frame_tools/eh_to_bin.sh
@@ -4,7 +4,7 @@ infile=$1
 addr=$2
 outfile=$3
 
-gcc $infile -nostdlib -Wl,--section-start -Wl,eh_frame_hdr=$addr -Wl,-e -Wl,0x1000  -Wl,--build-id=none -Wl,-T -Wl,${PEASOUP_HOME}/tools/eh_frame_tools/eh_frame.ls -o $outfile -Wl,-Map,$outfile.map || exit
+gcc $infile -nostdlib -Wl,--section-start -Wl,eh_frame_hdr=$addr -Wl,-e -Wl,0x1000  -Wl,--build-id=none -Wl,-T -Wl,${PEASOUP_HOME}/tools/eh_frame_tools/eh_frame.ls -o $outfile -Wl,-Map,$outfile.map -static -no-pie -fno-PIC || exit
 #eu-readelf -S ./a.out
 objcopy --rename-section eh_frame_hdr=.eh_frame_hdr --rename-section eh_frame=.eh_frame --rename-section gcc_except_table=.gcc_except_table $outfile
 #eu-readelf -S -w ./b.out
