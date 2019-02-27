2cf27d2e091aeb11f289f306a90228b41bcec012
Jason Hiser <jdhiser@gmail.com>
2019-02-27 10:37:14 -0500

    cleanup for older versions of gcc

--- peasoup_examples/tools/eh_frame_tools/eh_to_bin.sh
+++ peasoup_examples/tools/eh_frame_tools/eh_to_bin.sh
@@ -4,7 +4,13 @@ infile=$1
 addr=$2
 outfile=$3
 
-gcc $infile -nostdlib -Wl,--section-start -Wl,eh_frame_hdr=$addr -Wl,-e -Wl,0x1000  -Wl,--build-id=none -Wl,-T -Wl,${PEASOUP_HOME}/tools/eh_frame_tools/eh_frame.ls -o $outfile -Wl,-Map,$outfile.map -static -no-pie -fno-PIC || exit
+# Try gcc with -no-pie option, but some don't support it
+# so then try normal gcc
+# and if both fail, then we exit with an error code
+
+gcc $infile -nostdlib -Wl,--section-start -Wl,eh_frame_hdr=$addr -Wl,-e -Wl,0x1000  -Wl,--build-id=none -Wl,-T -Wl,${PEASOUP_HOME}/tools/eh_frame_tools/eh_frame.ls -o $outfile -Wl,-Map,$outfile.map -static -no-pie -fno-PIC  || \
+gcc $infile -nostdlib -Wl,--section-start -Wl,eh_frame_hdr=$addr -Wl,-e -Wl,0x1000  -Wl,--build-id=none -Wl,-T -Wl,${PEASOUP_HOME}/tools/eh_frame_tools/eh_frame.ls -o $outfile -Wl,-Map,$outfile.map -static  || \
+exit 1
 #eu-readelf -S ./a.out
 objcopy --rename-section eh_frame_hdr=.eh_frame_hdr --rename-section eh_frame=.eh_frame --rename-section gcc_except_table=.gcc_except_table $outfile
 #eu-readelf -S -w ./b.out
