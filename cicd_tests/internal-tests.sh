109225aa38dd19db5fb0974850068162c267c3f6
Jason Hiser <jdhiser@gmail.com>
2019-03-09 10:53:27 -0500

    updated tests to use irdb-lib path

--- cicd_tests/internal-tests.sh
+++ cicd_tests/internal-tests.sh
@@ -7,6 +7,8 @@ cd $CICD_MODULE_WORK_DIR/zipr_umbrella
 source set_env_vars
 # run zipr internal tests
 cd $ZIPR_HOME/test; scons
+
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PEASOUP_HOME/irdb-libs/lib
 for i in *.exe; do ./$i; done
 
 
