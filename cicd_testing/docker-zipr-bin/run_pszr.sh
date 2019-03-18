46da93e70509af2e5fb743111eb566f1646eb1c1
Jason Hiser <jdhiser@gmail.com>
2019-03-18 11:01:12 -0400

    cleanup of run-pszr

--- cicd_testing/docker-zipr-bin/run_pszr.sh
+++ cicd_testing/docker-zipr-bin/run_pszr.sh
@@ -17,44 +17,23 @@ print_usage()
 	echo ""
 }
 
-function is_in_activation 
-{
-	service "$1" status
-   activation=$(service "$1" status | grep "Active: activation" )
-   if [ -z "$activation" ]; then
-      true;
-   else
-      false;
-   fi
-
-   return $?;
-}
 
 main()
 {
 	local res=0
-	export USER=root;
-	cd /opt/ps_zipr 
-	source ./set_env_vars 
-	cd /home/zuser
+	local subcommand=$1
 
-	subcommand=$1
 	shift
 	
-	echo "Arguments are: $@"
 	case "$subcommand" in
 		iagree)
 			echo
 			echo Welcome to the IRDB toolchain docker image!
 			echo
-			echo "Setting up postgres..."
-			echo
-			service postgresql start 
-			echo
 			echo 'The IRDB toolchain is setup and ready to run.'
 			echo 'You could start your first experiment with:'
 			echo
-			echo 'zuser@a3fc1666aaa4:~$ pszr /bin/ls ./ls.p1 -c p1transform'
+			echo 'zuser@a3fc1666aaa4:~$ pszr /bin/ls ./ls.bilr'
 			echo 'Using Zipr backend.'
 			echo 'Detected ELF shared object.'
 			echo 'Performing step rida [dependencies=mandatory] ...Done.  Successful.'
@@ -62,9 +41,8 @@ main()
 			echo 'Performing step fill_in_cfg [dependencies=unknown] ...Done.  Successful.'
 			echo 'Performing step fill_in_indtargs [dependencies=unknown] ...Done.  Successful.'
 			echo 'Performing step fix_calls [dependencies=unknown] ...Done.  Successful.'
-			echo 'Performing step p1transform [dependencies=unknown] ...Done.  Successful.'
 			echo 'Performing step zipr [dependencies=none] ...Done.  Successful.'
-			echo 'zuser@a3fc1666aaa4:~$ ./ls.p1 -l ' 
+			echo 'zuser@a3fc1666aaa4:~$ ./ls.bilr -l ' 
 			echo ' < ls output >  '
 			echo 'zuser@a3fc1666aaa4:~$ readelf -l /bin/ls ./ls.p1 ' 
 			echo
