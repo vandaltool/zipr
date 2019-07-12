a07685ac296f3e1bc8ec664efa71281ab00193d3
Jason Hiser <jdhiser@gmail.com>
2019-07-11 21:58:22 -0400

    added arm tests

--- cicd_testing/builtin-xforms.sh
+++ cicd_testing/builtin-xforms.sh
@@ -5,5 +5,11 @@ set -x
 cd /tmp/peasoup_test
 source set_env_vars
 cd $PEASOUP_HOME/tests
+
 make clean; 
-./test_cmds.sh -c "fix_calls_rida fix_calls_ida" -l -a "bzip2 tcpdump"
+if [[ $(uname -m) == 'armv7l' ]] || [[ $(uname -m) == 'aarch64' ]]; then
+	./test_cmds.sh -c "zipr_rida" -l -a "bzip2 ls"
+else
+	./test_cmds.sh -c "zipr_rida fix_calls_rida fix_calls_ida" -l -a "bzip2 tcpdump"
+fi
+
