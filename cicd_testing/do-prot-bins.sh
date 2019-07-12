0fd8057140c0cc3575fb463889f6b13656a64253
Jason Hiser <jdhiser@gmail.com>
2019-07-12 17:58:43 -0400

    updated do-prot to use arm readelf/objdump

--- cicd_testing/do-prot-bins.sh
+++ cicd_testing/do-prot-bins.sh
@@ -8,10 +8,21 @@ source set_env_vars
 popd
 
 
-mkdir -p artifacts/protected_binaries
+install_deps()
+{
+sudo 
+apt-get install -y binutils-aarch64-linux-gnu
+
+}
 
 main()
 {
+	install_deps
+	export PS_OBJDUMP=$(which aarch64-linux-gnu-objdump)
+	export PS_READELF=$(which aarch64-linux-gnu-readelf)
+
+
+	mkdir -p artifacts/protected_binaries
 	for i in $(ls artifacts/test_binaries/); do
 		$PSZ  artifacts/test_binaries/$i artifacts/protected_binaries/$(basename $i.$(hostname))
 	done
