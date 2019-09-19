ab989ac0c17b0b122016099f9674032a22b90917
Anh <zenpoems@gmail.com>
2019-09-19 14:05:27 -0700

    Remove ida test

--- cicd_testing/builtin-xforms.sh
+++ cicd_testing/builtin-xforms.sh
@@ -10,6 +10,6 @@ make clean;
 if [[ $(uname -m) == 'armv7l' ]] || [[ $(uname -m) == 'aarch64' ]]; then
 	./test_cmds.sh -c "rida" -l -a "bzip2 ls"
 else
-	./test_cmds.sh -c "rida fix_calls_rida fix_calls_ida" -l -a "bzip2 tcpdump"
+	./test_cmds.sh -c "rida fix_calls_rida" -l -a "bzip2 tcpdump"
 fi
 
