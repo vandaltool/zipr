7a20428d351a17dea84fa6b5da272195217644a8
Jason Hiser <jdhiser@gmail.com>
2019-10-16 11:29:48 -0400

    updated test-cmds to be more specific about what edges to use for critical edge breaker -- required for zafl.sh changes

--- tests/test_cmds.sh
+++ tests/test_cmds.sh
@@ -88,10 +88,10 @@ do_tests()
 					zafl.sh $progpath $protected --rida -m --tempdir $temp_dir --untracer > test_${prog}.ps.log 2>&1
 				;;
 				zafl_untracer_critical_edges)
-					zafl.sh $progpath $protected --rida --tempdir $temp_dir --untracer -c > test_${prog}.ps.log 2>&1
+					zafl.sh $progpath $protected --rida --tempdir $temp_dir --untracer -c all > test_${prog}.ps.log 2>&1
 				;;
 				zafl_untracer_critical_edges_fix_map)
-					zafl.sh $progpath $protected -m --rida --tempdir $temp_dir --untracer -c > test_${prog}.ps.log 2>&1
+					zafl.sh $progpath $protected -m --rida --tempdir $temp_dir --untracer -c all > test_${prog}.ps.log 2>&1
 				;;
 				zafl_rida)
 					zafl.sh $progpath $protected --rida --tempdir $temp_dir > test_${prog}.ps.log 2>&1
