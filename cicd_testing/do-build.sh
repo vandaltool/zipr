e736a6a0799a7ac52a2d44e0843b555a6d2eb9c8
Jason Hiser <jdhiser@gmail.com>
2021-02-10 16:58:26 -0500

    Change path to ida for sharing with other projects.

--- cicd_testing/do-build.sh
+++ cicd_testing/do-build.sh
@@ -15,9 +15,9 @@ env|grep CICD
 time rsync -a --exclude='.git'  $CICD_TO_TEST_DIR/ /tmp/peasoup_test
 cd /tmp/peasoup_test
 source set_env_vars
-cicd_setup_module_dependency allzp/idapro71.git zipr_umbrella_ida
+cicd_setup_module_dependency allzp/idapro71.git ida71
 export IDAROOT=$CICD_MODULE_WORK_DIR/zipr_umbrella_ida
-cicd_setup_module_dependency allzp/idapro71_sdk.git zipr_umbrella_idasdk
+cicd_setup_module_dependency allzp/idapro71_sdk.git ida71_sdk
 export IDASDK=$CICD_MODULE_WORK_DIR/zipr_umbrella_idasdk
 
 sudo ./get-peasoup-packages.sh all
--- cicd_testing/do-prot-bins.sh
+++ cicd_testing/do-prot-bins.sh
@@ -4,8 +4,8 @@ set -e
 set -x
 
 pushd /tmp/peasoup_test
-export IDAROOT=$CICD_MODULE_WORK_DIR/zipr_umbrella_ida
-export IDASDK=$CICD_MODULE_WORK_DIR/zipr_umbrella_idasdk
+export IDAROOT=$CICD_MODULE_WORK_DIR/ida71
+export IDASDK=$CICD_MODULE_WORK_DIR/ida71_sdk
 source set_env_vars
 popd
 
