10d3bce9afaf0d55dce4e6283da5861fd20931dc
Jason Hiser <jdhiser@gmail.com>
2021-02-10 16:19:41 -0500

    Fix path to IDA on gitlab.

--- cicd_testing/do-build.sh
+++ cicd_testing/do-build.sh
@@ -15,9 +15,9 @@ env|grep CICD
 time rsync -a --exclude='.git'  $CICD_TO_TEST_DIR/ /tmp/peasoup_test
 cd /tmp/peasoup_test
 source set_env_vars
-cicd_setup_module_dependency allnp/idaproCur.git zipr_umbrella_ida
+cicd_setup_module_dependency allzp/idaproCur.git zipr_umbrella_ida
 export IDAROOT=$CICD_MODULE_WORK_DIR/zipr_umbrella_ida
-cicd_setup_module_dependency allnp/idaproCur_sdk.git zipr_umbrella_idasdk
+cicd_setup_module_dependency allzp/idaproCur_sdk.git zipr_umbrella_idasdk
 export IDASDK=$CICD_MODULE_WORK_DIR/zipr_umbrella_idasdk
 
 sudo ./get-peasoup-packages.sh all
