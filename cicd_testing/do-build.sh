fbff5e78f5b1ffcaee02a0900f43dc5abe80c5f9
Jason Hiser <jdhiser@gmail.com>
2021-02-10 16:17:27 -0500

    Update cicd to grab ida and build that way for arm testing.

--- cicd_testing/do-build.sh
+++ cicd_testing/do-build.sh
@@ -15,6 +15,11 @@ env|grep CICD
 time rsync -a --exclude='.git'  $CICD_TO_TEST_DIR/ /tmp/peasoup_test
 cd /tmp/peasoup_test
 source set_env_vars
+cicd_setup_module_dependency allnp/idaproCur.git zipr_umbrella_ida
+export IDAROOT=$CICD_MODULE_WORK_DIR/zipr_umbrella_ida
+cicd_setup_module_dependency allnp/idaproCur_sdk.git zipr_umbrella_idasdk
+export IDASDK=$CICD_MODULE_WORK_DIR/zipr_umbrella_idasdk
+
 sudo ./get-peasoup-packages.sh all
 bash -x ./build-all.sh 
 bash -x ./postgres_setup.sh
