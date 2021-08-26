b8c81f039453501a9f9b8a8a5c2771ae084e80f7
Anh <anh@zephyr-software.com>
2021-08-26 18:58:22 +0000

    Sanity check that Keystone assembles exactly 1 instrution

--- irdb-libs/libIRDB-core/src/assemblestr.hpp
+++ irdb-libs/libIRDB-core/src/assemblestr.hpp
@@ -5,7 +5,7 @@
 
 static void assemblestr(ks_engine * &ks, IRDB_SDK::Instruction_t *ins, const char * instruct, char * &encode, size_t &size, size_t &count)
 {
-        if(ks_asm(ks, instruct, 0, (unsigned char **)&encode, &size, &count) != KS_ERR_OK) { //string or cstr
+        if(ks_asm(ks, instruct, 0, (unsigned char **)&encode, &size, &count) != KS_ERR_OK || count != 1) { //string or cstr
                 ks_free((unsigned char*)encode);
                 ks_close(ks);
                 throw std::runtime_error("ERROR: ks_asm() failed during instrunction assembly.");
