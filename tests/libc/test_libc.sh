97e31a07436b18ded1f5b36e841992bbe4a27fd2
Jason Hiser <jdhiser@gmail.com>
2023-03-27 19:43:21 -0400

    update test to get the real path for libc, instead of the symlink

--- tests/libc/test_libc.sh
+++ tests/libc/test_libc.sh
@@ -6,13 +6,16 @@ main()
 	set -e
 	set -x
 
+	#  make sure no prior libcs are hanging out.
+	rm -rf libc.* || true
+
 	# calc some names
-	local libc=$(ldd $(which ls)|grep libc|cut -d'>' -f2|cut -d'(' -f1)
+	local libc=$(ldd $(which ls)|grep 'libc\.'|cut -d'>' -f2|cut -d'(' -f1)
 	local libc_short=$(basename $libc)
 	local libc_zipr=$(basename $libc).zipr
 
 	# run zipr
-	$PSZ $libc $libc_zipr
+	$PSZ $(realpath $libc) $libc_zipr
 
 	# move the zir'd libc to the right name.  All subsequent commands
 	# now run with the zipr'd libc
@@ -24,7 +27,8 @@ main()
 
 	# invoke zipr on ls -- using zipr'd libc.
 	$PSZ $(which ls) ls.zipr || exit 1
-	ls.zipr -lhrSR || exit 1
+	./ls.zipr -lhrSR || exit 1
+	ldd ./ls.zipr
 }
 
 main "$@"
